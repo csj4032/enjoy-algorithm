40ebb7962be618d8e5c12198b00aa7c31f598e62
genius.choi <csj4032@gmail.com>
2017-08-10 02:09:21 +0900

    백준 1065 문제 시작

---
+++ baekjoon/src/main/java/p1065/Main.java
@@ -0,0 +1,14 @@
+package p1065;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			Arrays.stream(String.valueOf(i).split(""));
+		}
+	}
+}
