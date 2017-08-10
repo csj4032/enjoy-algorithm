1b9bdcc74d09aeda10926f262949dcba280c2d05
genius.choi <genius.choi@meshkorea.net>
2017-08-10 12:39:43 +0900

    백준 1065 문제 완

--- baekjoon/src/main/java/p1065/Main.java
+++ baekjoon/src/main/java/p1065/Main.java
@@ -1,18 +1,30 @@
 package p1065;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int count = 0;
 		for (int i = 1; i <= n; i++) {
-			Arrays.stream(String.valueOf(i).split(""));
+			count += arithmetical(String.valueOf(i).split(""));
 		}
+		System.out.println(count);
 	}
 
-	private static void arithmetical() {
-
+	private static int arithmetical(String[] str) {
+		Set<Integer> numbers = new HashSet<>();
+		Arrays.stream(str).mapToInt(s -> Integer.valueOf(s)).reduce((a, b) -> {
+			numbers.add(a - b);
+			return b;
+		});
+		if (numbers.size() < 2) {
+			return 1;
+		}
+		return 0;
 	}
 }
