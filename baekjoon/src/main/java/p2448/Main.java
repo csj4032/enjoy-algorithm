3cf23685cc7702d4ad959076897ce3124cd8ed72
genius.choi <genius.choi@meshkorea.net>
2017-08-12 00:37:43 +0900

    백준 2448 문재 완료

--- baekjoon/src/main/java/p2448/Main.java
+++ baekjoon/src/main/java/p2448/Main.java
@@ -1,21 +1,34 @@
 package p2448;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static java.lang.System.in;
 
 public class Main {
 
-	private static List<String> list = new ArrayList<>();
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(in);
-		solved(sc.nextInt());
-	}
+		String[] arr;
+		int copyHeight = 3;
+		int n = sc.nextInt();
+		int m = (int) (Math.log(n / 3) / Math.log(2)) + 1;
+		arr = new String[n];
 
-	private static void solved(int n) {
+		arr[0] = "  *  ";
+		arr[1] = " * * ";
+		arr[2] = "*****";
 
+		for (int i = 1; i < m; i++) {
+			for (int j = 0; j < copyHeight; j++) {
+				arr[copyHeight + j] = arr[j] + " " + arr[j];
+				for (int k = 0; k < copyHeight / 3; k++) {
+					arr[j] = "   " + arr[j] + "   ";
+				}
+			}
+			copyHeight *= 2;
+		}
+
+		Stream.of(arr).forEach(System.out::println);
 	}
 }
