a634e141f5ae4c1505bce9df4b7f56df414af28a
genius.choi <genius.choi@meshkorea.net>
2017-08-14 01:33:40 +0900

    백준 10039, 11654 문제 완료

---
+++ baekjoon/src/main/java/p10039/Main.java
@@ -0,0 +1,15 @@
+package p10039;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			int score = sc.nextInt();
+			sum += score < 40 ? 40 : score;
+		}
+		System.out.println(sum / 5);
+	}
+}
