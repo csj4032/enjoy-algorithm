e3779ec96fea7648e08661d6c115968f48dc6273
genius.choi <csj4032@gmail.com>
2017-08-14 01:33:40 +0900

    백준 10039, 11654 문제 완료

---
+++ baekjoon/src/main/java/p11654/Main.java
@@ -0,0 +1,9 @@
+package p11654;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new Scanner(System.in).next().chars().sum());
+	}
+}
