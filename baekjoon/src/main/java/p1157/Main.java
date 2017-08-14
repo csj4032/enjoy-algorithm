fbb3b201795921de0da845db835c0025965ba65d
genius.choi <genius.choi@meshkorea.net>
2017-08-14 19:02:12 +0900

    백준 문제 1157 시작

---
+++ baekjoon/src/main/java/p1157/Main.java
@@ -0,0 +1,10 @@
+package p1157;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] str = sc.next().toLowerCase().split("");
+	}
+}
