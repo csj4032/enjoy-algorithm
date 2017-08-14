7a5b79301bd18b9ffb9a4d44fc65306d3ddf8146
genius.choi <genius.choi@meshkorea.net>
2017-08-15 02:23:31 +0900

    백준 문제 1316 완료

--- baekjoon/src/main/java/p1316/Main.java
+++ baekjoon/src/main/java/p1316/Main.java
@@ -1,4 +1,30 @@
 package p1316;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = 0;
+		for (int i = 0; i < n; i++) {
+			String[] str = sc.next().split("");
+			List<String> list = new ArrayList<>();
+			list.add(str[0]);
+			boolean flag = true;
+			for (int j = 1; j < str.length; j++) {
+				String now = str[j];
+				String prev = str[j - 1];
+				if (list.contains(now) && !now.equals(prev)) {
+					flag = false;
+				} else {
+					list.add(now);
+				}
+			}
+			if (flag) m++;
+		}
+		System.out.println(m);
+	}
 }
