b9b2fd3ed8aeba488e6f079ddc8534177409afe1
genius.choi <csj4032@gmail.com>
2017-08-18 01:14:11 +0900

    백준 문제 2292 완료

--- baekjoon/src/main/java/p2292/Main.java
+++ baekjoon/src/main/java/p2292/Main.java
@@ -1,7 +1,21 @@
 package p2292;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = 1, j = 1, i = 1;
+		while (true) {
+			if (n > j) {
+				m++;
+				j = j + 6 * i;
+				i++;
+			} else {
+				break;
+			}
+		}
+		System.out.println(m);
 	}
 }
