5fd6d964df4952c444407a71e2f3dd0d7ad4e3b8
genius.choi <genius.choi@meshkorea.net>
2017-08-22 19:07:18 +0900

    백준 문제 10989 진행

--- baekjoon/src/main/java/p10989/Main.java
+++ baekjoon/src/main/java/p10989/Main.java
@@ -7,15 +7,26 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] m2 = new int[10000];
+		int max = 0;
+		int[] m1 = new int[n];
+
 		for (int i = 0; i < n; i++) {
-			m2[sc.nextInt()-1] += 1;
+			int min = sc.nextInt();
+			m1[i] = min;
+			if (max < min) {
+				max = min;
+			}
 		}
 
-		for (int i = 0; i < 10000; i++) {
+		int[] m2 = new int[max+1];
+		for (int i = 0; i < n; i++) {
+			m2[m1[i]]++;
+		}
+
+		for (int i = 0; i < m2.length; i++) {
 			int k = m2[i];
 			for (int j = 0; j < k; j++) {
-				System.out.println(i + 1);
+				System.out.println(i);
 			}
 		}
 	}
