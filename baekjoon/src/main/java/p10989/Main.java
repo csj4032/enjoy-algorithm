8015481400ad0245b166a9977270299709c3406f
genius.choi <genius.choi@meshkorea.net>
2017-08-22 23:56:38 +0900

    백준 문제 10989 완료

--- baekjoon/src/main/java/p10989/Main.java
+++ baekjoon/src/main/java/p10989/Main.java
@@ -1,33 +1,39 @@
 package p10989;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+
+		int[] n;
+		int[] m;
 		int max = 0;
-		int[] m1 = new int[n];
 
-		for (int i = 0; i < n; i++) {
-			int min = sc.nextInt();
-			m1[i] = min;
-			if (max < min) {
-				max = min;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = new int[Integer.parseInt(br.readLine().trim())];
+
+		for (int i = 0; i < n.length; i++) {
+			int num = Integer.parseInt(br.readLine().trim());
+			n[i] = num;
+			if (max < num) {
+				max = num;
 			}
 		}
 
-		int[] m2 = new int[max+1];
-		for (int i = 0; i < n; i++) {
-			m2[m1[i]]++;
+		m = new int[max + 1];
+
+		for (int i = 0; i < n.length; i++) {
+			m[n[i]]++;
 		}
 
-		for (int i = 0; i < m2.length; i++) {
-			int k = m2[i];
-			for (int j = 0; j < k; j++) {
-				System.out.println(i);
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i]; j++) {
+				bw.write(i + "\n");
 			}
 		}
+		bw.close();
 	}
 }
