669c78513a107784926d35f9a4159f9a4055e8cc
genius.choi <csj4032@gmail.com>
2017-08-30 13:53:15 +0900

    백준 문제 522088 스칼라

---
+++ baekjoon/src/main/python3/2741.py
@@ -0,0 +1,5 @@
+n = int(input())
+buf = []
+for i in range(1, n + 1):
+    buf.append(str(i))
+print('\n'.join(buf))
---
+++ baekjoon/src/main/scala/s522088.scala
@@ -0,0 +1,8 @@
+object s522088 {
+
+  def main(args: Array[String]): Unit = {
+    val n = readInt;
+    (1 to n).foreach(println)
+    for( i <- 1 to n) println(i)
+  }
+}
