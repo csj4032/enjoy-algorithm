98af423e36617179035a02b7bbc8b339bff499eb
genius.choi <csj4032@gmail.com>
2017-09-06 14:39:37 +0900

    백준 문제 2579 진

--- baekjoon/src/main/java/p2579/Main.java
+++ baekjoon/src/main/java/p2579/Main.java
@@ -5,22 +5,24 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		int n = sc.nextInt();
-//		int[] m = new int[n];
-//		for (int i = 0; i < n; i++) {
-//			m[i] = sc.nextInt();
-//		}
-		int n = 6;
-		int[] m = {10, 20, 15, 25, 10, 20};
-		recursive(n, m, 1);
-	}
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] m = new int[n];
+		int[] s = new int[n];
 
-	private static void recursive(int n, int[] m, int s) {
-		if (s > n) {
-			return;
+		for (int i = 0; i < n; i++) {
+			m[i] = sc.nextInt();
 		}
-		System.out.println(m[s]);
-		recursive(n, m, s + 1);
+
+		s[0] = m[0];
+		s[1] = m[0] + m[1];
+		s[2] = s[1] + m[2];
+
+		for (int i = 3; i < n; i++) {
+			s[i] = Math.max(s[i - 3] + m[i - 1] + m[i], s[i - 2]) + m[i];
+			System.out.println(s[i]);
+		}
+
+		System.out.println(s[n - 1]);
 	}
 }
