58fde4115c474956cc07b4d8d9d3e49ecde8ec6c
genius.choi <csj4032@gmail.com>
2017-09-06 23:54:35 +0900

    백준 문제 1149 완료

--- baekjoon/src/main/java/p1149/Main.java
+++ baekjoon/src/main/java/p1149/Main.java
@@ -7,12 +7,24 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[][] m = new int[n][3];
-		int[][] l = new int[n][3];
-		for (int i = 0; i < n; i++) {
+		int[][] m = new int[n + 1][3];
+		int[][] l = new int[n + 1][3];
+
+		m[0][0] = m[0][1] = m[0][2] = 0;
+		l[0][0] = l[0][1] = l[0][2] = 0;
+
+		for (int i = 1; i <= n; i++) {
 			for (int j = 0; j < 3; j++) {
 				m[i][j] = sc.nextInt();
 			}
 		}
+
+		for (int i = 1; i <= n; i++) {
+			l[i][0] = Math.min(l[i - 1][1], l[i - 1][2]) + m[i][0];
+			l[i][1] = Math.min(l[i - 1][0], l[i - 1][2]) + m[i][1];
+			l[i][2] = Math.min(l[i - 1][0], l[i - 1][1]) + m[i][2];
+		}
+
+		System.out.println(Math.min(Math.min(l[n][0], l[n][1]), l[n][2]));
 	}
 }
