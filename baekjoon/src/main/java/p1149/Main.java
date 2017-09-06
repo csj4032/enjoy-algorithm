5f041e454845ff05593ade0dcbe29a3419a10d56
genius.choi <csj4032@gmail.com>
2017-09-06 19:01:51 +0900

    백준 문제 1149 진행

--- baekjoon/src/main/java/p1149/Main.java
+++ baekjoon/src/main/java/p1149/Main.java
@@ -8,21 +8,11 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[][] m = new int[n][3];
+		int[][] l = new int[n][3];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < 3; j++) {
 				m[i][j] = sc.nextInt();
 			}
-		}
-		recursive(m, 3, 0, 0, 0, "");
-	}
-
-	private static void recursive(int[][] m, int n, int d, int k, int p, String r) {
-		if (d == n) {
-			System.out.println(p + " " + r);
-			return;
-		}
-		for (int i = 0; i < n; i++) {
-			recursive(m, n, d + 1, i, p + m[d][i], r + "" + i);
 		}
 	}
 }
