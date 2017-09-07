e29c7da6f44b4416cc00604ed616694473799986
genius.choi <csj4032@gmail.com>
2017-09-07 15:41:27 +0900

    백준 문제 1463 완료

--- baekjoon/src/main/java/p1463/Main.java
+++ baekjoon/src/main/java/p1463/Main.java
@@ -9,12 +9,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int m = recursive(new int[]{n}, 0);
-		System.out.println(m);
+		recursive(n, 0);
 		System.out.println(k);
 	}
 
-	private static int recursive(int[] n, int m) {
-		return 0;
+	private static void recursive(int n, int m) {
+		if (n == 1 && k > m) k = m;
+		if (k <= m) return;
+		if (n % 3 == 0) recursive(n / 3, m + 1);
+		if (n % 2 == 0) recursive(n / 2, m + 1);
+		recursive(n - 1, m + 1);
 	}
 }
