9c4213d0b7acbe4433cb09d753323aa4c46c1ce7
genius.choi <genius.choi@meshkorea.net>
2017-09-08 03:29:24 +0900

    백준 문제 2293 진행

--- baekjoon/src/main/kotlin/2557.kt
+++ baekjoon/src/main/kotlin/2557.kt
@@ -1,8 +1,8 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    val input = Scanner(System.`in`);
-    val t1 = input.next();
-    val t2 = input.next();
-    println(t1.toInt() + t2.toInt());
+    val input = Scanner(System.`in`)
+    val t1 = input.next()
+    val t2 = input.next()
+    println(t1.toInt() + t2.toInt())
 }
