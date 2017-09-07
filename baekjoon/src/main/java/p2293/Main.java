8bb12dbd12d1803cdf3cca982b926b4db197c5ae
genius.choi <csj4032@gmail.com>
2017-09-08 03:29:24 +0900

    백준 문제 2293 진행

--- baekjoon/src/main/java/p2293/Main.java
+++ baekjoon/src/main/java/p2293/Main.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 public class Main {
 
 	static int[] m;
+	static int n;
+	static int k;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k = sc.nextInt();
+		n = sc.nextInt();
+		k = sc.nextInt();
 		m = new int[n];
 		for (int i = 0; i < n; i++) m[i] = sc.nextInt();
 		System.out.println(recursive(k, 0));
 	}
 
 	private static int recursive(int k, int s) {
-		if (k == 0) return 1;
-		else if (k < 0 || m.length <= s) return 0;
+		if (k < 0 || n <= s) {
+			System.out.println(k + " : " +s);
+			return 0;
+		} else if (k == 0) {
+			System.out.println(k + " " +s);
+			return 1;
+		}
 		return recursive(k - m[s], s) + recursive(k, s + 1);
 	}
 }
