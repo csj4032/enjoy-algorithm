f07851b70282078264f833bdf05e7a48e51fb1a7
genius.choi <csj4032@gmail.com>
2017-09-09 23:36:01 +0900

    백준 문제 11050 코틀린

--- baekjoon/src/main/java/p11050/Main.java
+++ baekjoon/src/main/java/p11050/Main.java
@@ -1,4 +1,26 @@
 package p11050;
 
+import java.util.Scanner;
+
+/**
+ * 제목 : 이항 계수 1
+ * 단계 : 이항 계수
+ * 힌트 : http://destiny738.tistory.com/194
+ */
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int s = +recursive(n, k);
+		System.out.println(s);
+	}
+
+	private static int recursive(int n, int k) {
+		if (k == 0 || k == n) {
+			return 1;
+		}
+		return recursive(n - 1, k - 1) + recursive(n - 1, k);
+	}
 }
