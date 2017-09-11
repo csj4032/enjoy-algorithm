61b4f4fd26ee8c683865f8dab8743f09590af986
genius.choi <csj4032@gmail.com>
2017-09-11 17:05:00 +0900

    백준 문제 1991 완료

--- baekjoon/src/main/java/p1991/Main.java
+++ baekjoon/src/main/java/p1991/Main.java
@@ -4,23 +4,56 @@ import java.util.Scanner;
 
 public class Main {
 
+	static String[] node = new String[1000];
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		Node root;
+		node[1] = "A";
 		for (int i = 0; i < n; i++) {
-			Node node = new Node(sc.next());
-			node.right = new Node(sc.next());
-			node.left = new Node(sc.next());
+			String a = sc.next();
+			String b = sc.next();
+			String c = sc.next();
+			int parent = search(a);
+			node[parent * 2] = b;
+			node[parent * 2 + 1] = c;
 		}
+		preOrder(1);
+		System.out.println("");
+		inOrder(1);
+		System.out.println("");
+		postOrder(1);
 	}
-}
 
-class Node {
-	public Node left;
-	public Node right;
-	public String value;
-	public Node(String value) {
-		this.value = value;
+	private static void preOrder(int i) {
+		if (node[i].equals(".")) return;
+		System.out.print(node[i]);
+		preOrder(i * 2);
+		preOrder(i * 2 + 1);
+	}
+
+	private static void inOrder(int i) {
+		if (node[i].equals(".")) return;
+		inOrder(i * 2);
+		System.out.print(node[i]);
+		inOrder(i * 2 + 1);
+	}
+
+	private static void postOrder(int i) {
+		if (node[i].equals(".")) return;
+		postOrder(i * 2);
+		postOrder(i * 2 + 1);
+		System.out.print(node[i]);
+	}
+
+	private static int search(String a) {
+		int i = 1;
+		while (i < node.length) {
+			if (a.equals(node[i])) {
+				break;
+			}
+			i++;
+		}
+		return i;
 	}
 }
