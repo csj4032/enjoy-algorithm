bc2a788bc7c2f64341c0bef9aeaadc7da9a54485
genius.choi <csj4032@gmail.com>
2017-09-12 19:10:28 +0900

    백준 문제 2504 진행

--- baekjoon/src/main/java/p2504/Main.java
+++ baekjoon/src/main/java/p2504/Main.java
@@ -1,11 +1,28 @@
 package p2504;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] s = sc.next().split("");
+		Stack<String> stack1 = new Stack<>();
+		Stack<String> stack2 = new Stack<>();
+		Stack<String> stack3 = new Stack<>();
+		String prev = "";
+		String cur = "";
+		for (int i = 0; i < s.length; i++) {
+			cur = s[i];
+			if (cur.equals("(")) {
+				stack1.push(cur);
+			} else {
+				stack2.push("2");
+				prev = cur;
+			}
+		}
+
+		System.out.println(stack2);
 	}
 }
