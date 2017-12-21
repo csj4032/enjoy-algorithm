218c9c2fb30ad8803886c47a9c3ebe9e5ef071ab
genius.choi <csj4032@gmail.com>
2017-12-22 02:13:00 +0900

    OddOccurrencesInArray

--- codility/src/main/java/arrays/OddOccurrencesInArray.java
+++ codility/src/main/java/arrays/OddOccurrencesInArray.java
@@ -1,12 +1,33 @@
 package arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class OddOccurrencesInArray {
 
-	public int solution(int[] N) {
+	public int solution(int[] A) {
 		int temp = 0;
-		for (int i = 0; i < N.length; i++) {
-			temp = temp ^ N[i];
+		for (int i = 0; i < A.length; i++) {
+			temp = temp ^ A[i];
 		}
 		return temp;
+	}
+
+	public int solution2(int[] A) {
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < A.length; i++) {
+			if (!set.contains(A[i])) {
+				set.add(A[i]);
+			} else {
+				set.remove(A[i]);
+			}
+		}
+		return (int) set.toArray()[0];
+	}
+
+	public static void main(String[] args) {
+		OddOccurrencesInArray oddOccurrencesInArray = new OddOccurrencesInArray();
+		System.out.println(oddOccurrencesInArray.solution(new int[]{9, 3, 9, 3, 9, 7, 9}));
+		System.out.println(oddOccurrencesInArray.solution2(new int[]{9, 3, 9, 3, 9, 7, 9}));
 	}
 }
