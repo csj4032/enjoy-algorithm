c082523fc78078bc42abf73bf813fe7d07abb786
genius.choi <csj4032@gmail.com>
2017-12-31 16:42:05 +0900

    Solution

---
+++ codility/src/main/java/sktechx/Solution.java
@@ -0,0 +1,12 @@
+package sktechx;
+
+public class Solution {
+
+	public static int solution(String S) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution("))"));
+	}
+}
