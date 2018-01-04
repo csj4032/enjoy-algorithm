5fd3b7816ca8457c68e4677c11e00e77e5a5054c
genius.choi <csj4032@gmail.com>
2018-01-04 20:10:00 +0900

    백준 문제 11399 완료

--- baekjoon/src/main/java/p11399/Main.java
+++ baekjoon/src/main/java/p11399/Main.java
@@ -1,4 +1,30 @@
 package p11399;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			list.add(sc.nextInt());
+		}
+
+		list.sort(Comparator.naturalOrder());
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				sum = sum + list.get(j);
+			}
+		}
+
+		System.out.println(sum);
+	}
 }
