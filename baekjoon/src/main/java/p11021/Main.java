6302aa9b1aeaef02c1791cf7975964898538e894
genius.choi <csj4032@gmail.com>
2018-01-04 10:01:07 +0900

    백준 문제 10953, 11021 완료

--- baekjoon/src/main/java/p11021/Main.java
+++ baekjoon/src/main/java/p11021/Main.java
@@ -1,4 +1,16 @@
 package p11021;
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.printf("Case #%d: %d\n", (i+1), (a+b));
+		}
+	}
 }
