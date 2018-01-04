eeb91e1697b4949f5f8af8774c3a7cfc56c3c526
genius.choi <genius.choi@wemakeprice.com>
2018-01-04 10:01:07 +0900

    백준 문제 10953, 11021 완료

--- baekjoon/src/main/java/p10953/Main.java
+++ baekjoon/src/main/java/p10953/Main.java
@@ -1,4 +1,17 @@
 package p10953;
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			String[] s = sc.next().split(",");
+			int a = Integer.valueOf(s[0]);
+			int b = Integer.valueOf(s[1]);
+			System.out.printf("%d\n", (a+b));
+		}
+	}
 }
