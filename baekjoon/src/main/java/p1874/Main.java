e96a0d86cf20745d391c117144ea20690d5b6b47
genius.choi <csj4032@gmail.com>
2018-01-06 21:49:45 +0900

    백준문제 1874 진행

--- baekjoon/src/main/java/p1874/Main.java
+++ baekjoon/src/main/java/p1874/Main.java
@@ -1,8 +1,16 @@
 package p1874;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		Stack<Integer> stack = new Stack();
+		Integer n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			stack.add(i);
+		}
 	}
 }
