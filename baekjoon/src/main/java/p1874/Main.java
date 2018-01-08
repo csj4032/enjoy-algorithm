d1f88c5b6505a7c1466fd467073c709aedb140e8
genius.choi <csj4032@gmail.com>
2018-01-08 09:34:59 +0900

    백준 문제 1874 수정

--- baekjoon/src/main/java/p1874/Main.java
+++ baekjoon/src/main/java/p1874/Main.java
@@ -1,5 +1,7 @@
 package p1874;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,10 +9,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Stack<Integer> stack = new Stack();
-		Integer n = sc.nextInt();
-		for (int i = 1; i <= n; i++) {
-			stack.add(i);
+		Stack<Integer> stack = new Stack<>();
+		List<String> string = new ArrayList<>();
+		int n = sc.nextInt();
+		int index;
+		int max = 0;
+		int k;
+		while (0 < n--) {
+			k = sc.nextInt();
+			if (max < k) {
+				for (index = max; index < k; index++) {
+					stack.push(index + 1);
+					string.add("+");
+				}
+			} else {
+				if (stack.peek() > k) {
+					System.out.println("NO");
+					return;
+				}
+			}
+			stack.pop();
+			string.add("-");
+			if (max < k) max = k;
 		}
+		string.stream().forEach(System.out::println);
 	}
 }
