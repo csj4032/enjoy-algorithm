627f1d3fe038ef1ce93af60bca2885f3a7bec3ac
genius.choi <genius.choi@wemakeprice.com>
2018-01-09 09:37:54 +0900

    백준 문제 2445 완료

--- baekjoon/src/main/java/p2445/Main.java
+++ baekjoon/src/main/java/p2445/Main.java
@@ -7,5 +7,25 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int h = n * 2 - 1;
+		int w = n * 2;
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if(i < n) {
+					if (j <= i || (w - i - 1) <= j) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				} else {
+					if (j > i || (w - i - 1) > j) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println("");
+		}
 	}
 }
--- baekjoon/src/main/java/p2579/Main.java
+++ baekjoon/src/main/java/p2579/Main.java
@@ -19,8 +19,8 @@ public class Main {
 		s[2] = Math.max(m[0] + m[2], m[1] + m[2]);
 
 		for (int i = 3; i < n; i++) {
-			s[i] = Math.max(s[i - 2] + m[i], m[i - 1] + m[i]+s[i - 3] );
+			s[i] = Math.max(s[i - 2] + m[i], m[i - 1] + m[i] + s[i - 3]);
 		}
-		System.out.println(s[n-1]);
+		System.out.println(s[n - 1]);
 	}
 }
