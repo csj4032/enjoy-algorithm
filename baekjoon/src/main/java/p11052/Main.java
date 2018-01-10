092615acdcbcc73d923409687bbe4b8340f8e6a4
genius.choi <genius.choi@wemakeprice.com>
2018-01-10 16:56:36 +0900

    백준 문제 11052 완료

--- baekjoon/src/main/java/p11052/Main.java
+++ baekjoon/src/main/java/p11052/Main.java
@@ -1,4 +1,27 @@
 package p11052;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] p = new int[n + 1];
+		int[] d = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			p[i] = sc.nextInt();
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j <= n; j++) {
+				if (d[j] < d[j - i] + p[i]) {
+					d[j] = d[j - i] + p[i];
+				}
+			}
+		}
+		System.out.println(Arrays.stream(d).max().orElse(0));
+	}
 }
