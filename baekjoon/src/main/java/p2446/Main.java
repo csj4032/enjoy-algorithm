724b5bd2852e20016accadb93cd8a81a91b060d5
genius.choi <csj4032@gmail.com>
2018-01-16 02:27:14 +0900

    백준 문제 2446 완료

--- baekjoon/src/main/java/p2446/Main.java
+++ baekjoon/src/main/java/p2446/Main.java
@@ -1,4 +1,22 @@
 package p2446;
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		int m = (2 * new Scanner(System.in).nextInt()) - 1;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < m; j++) {
+				if (j >= i && j < m - i) {
+					System.out.print("*");
+				} else if (j >= (m - i - 1) && j < i + 1) {
+					System.out.print("*");
+				} else {
+					if (i > j) System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+	}
 }
