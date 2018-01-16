15016345fc7713e99688f07ae0bd800341b37de4
genius.choi <genius.choi@wemakeprice.com>
2018-01-16 18:47:16 +0900

    백준 문제 2555, 1037

--- baekjoon/src/main/java/p1037/Main.java
+++ baekjoon/src/main/java/p1037/Main.java
@@ -1,4 +1,20 @@
 package p1037;
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int min = 1000000;
+		int max = 2;
+		for (int i = 0; i < n; i++) {
+			int k = sc.nextInt();
+			if (k < min) min = k;
+			if (k > max) max = k;
+		}
+
+		System.out.println(max * min);
+	}
 }
