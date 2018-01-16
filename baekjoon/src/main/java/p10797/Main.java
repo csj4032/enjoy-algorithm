a4d6574b198f0540ee0772654787184a03f9eb5e
genius.choi <csj4032@gmail.com>
2018-01-16 12:39:34 +0900

    백준 문제 1085 완료

---
+++ baekjoon/src/main/java/p10797/Main.java
@@ -0,0 +1,18 @@
+package p10797;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = 0;
+		for (int i = 0; i < 5; i++) {
+			if (n == sc.nextInt()) {
+				m++;
+			}
+		}
+		System.out.print(m);
+	}
+}
