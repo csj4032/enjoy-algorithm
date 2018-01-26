8dfcaa343ccaa49b47691c60b55397107f611996
genius.choi <csj4032@gmail.com>
2018-01-27 00:00:52 +0900

    백준 문제 2293 재도전

--- baekjoon/src/main/java/p2293/Main.java
+++ baekjoon/src/main/java/p2293/Main.java
@@ -1,28 +1,8 @@
 package p2293;
 
-import java.util.Scanner;
-
 public class Main {
 
-	static int[] m;
-	static int k;
-	static int n;
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		k = sc.nextInt();
-		m = new int[n];
-		for (int i = 0; i < n; i++) m[i] = sc.nextInt();
-		System.out.println(recursive(k, 0));
-	}
 
-	private static int recursive(int q, int s) {
-		if (q == 0) {
-			return 1;
-		} else if (q < 0 || n <= s) {
-			return 0;
-		}
-		return recursive(q - m[s], s) + recursive(q, s + 1);
 	}
 }
