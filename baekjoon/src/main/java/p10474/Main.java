96a95b7f514e203e487522edeab1c87162e94aea
genius.choi <csj4032@gmail.com>
2018-01-29 23:22:37 +0900

    백준 문제 10474 완료

---
+++ baekjoon/src/main/java/p10474/Main.java
@@ -0,0 +1,19 @@
+package p10474;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+
+			if (n == 0 && m == 0) break;
+
+			System.out.println(n / m + " " + n % m + " / " + m);
+		}
+	}
+}
