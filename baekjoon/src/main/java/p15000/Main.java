73185b7b3585ce47d847b1d7ba0100e58428c33a
genius.choi <csj4032@gmail.com>
2018-01-30 12:31:32 +0900

    백준 문제 15000 완료

---
+++ baekjoon/src/main/java/p15000/Main.java
@@ -0,0 +1,12 @@
+package p15000;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String message = sc.nextLine();
+		System.out.println(message.toUpperCase());
+	}
+}
