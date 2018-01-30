46b3d3e415d66176a7b8bd0d08bfa3dcdae8a01a
genius.choi <csj4032@gmail.com>
2018-01-30 12:26:34 +0900

    백준 문제 2156 복습 완료

--- baekjoon/src/main/java/p2156/Main.java
+++ baekjoon/src/main/java/p2156/Main.java
@@ -5,27 +5,26 @@ import java.util.Scanner;
 
 public class Main {
 
-	static int dp[] = new int[10001];
-	static int n;
-	static int m[];
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		m = new int[n];
+		int n = sc.nextInt();
+		int[] m = new int[10001];
+		int[] dp = new int[10001];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			m[i] = sc.nextInt();
 		}
 
 		dp[0] = 0;
-		dp[1] = m[0];
-		dp[2] = m[0] + m[1];
+		dp[1] = m[1];
+		dp[2] = dp[1] + m[2];
 
 		for (int i = 3; i <= n; i++) {
-			dp[i] = Math.max(dp[i - 3] + m[i - 2] + m[i - 1], dp[i - 2] + m[i - 1]);
-			System.out.println(Arrays.toString(dp));
+			int k = Math.max(dp[i - 2] + m[i], dp[i - 3] + m[i] + m[i - 1]);
+			dp[i] = Math.max(k, dp[i - 1]);
 		}
-		System.out.println(Arrays.stream(dp).max().orElse(0));
+
+		//System.out.println(Arrays.toString(dp));
+		System.out.println(dp[n]);
 	}
 }
