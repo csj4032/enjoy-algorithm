e9c6d1eadbf98a049440a3c350072dbd3328e271
genius.choi <csj4032@gmail.com>
2018-01-30 09:36:49 +0900

    백준 문제 11651 수정

--- baekjoon/src/main/java/p11651/Main.java
+++ baekjoon/src/main/java/p11651/Main.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class Main {
 
+	static int l = 100000;
 	static int m[] = new int[200001];
 	static Map<Integer, List<Integer>> map = new HashMap<>();
 
@@ -15,20 +16,20 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			m[y + 100000] = 1;
-			if (map.get(y + 100000) == null) {
+			m[y + l] = 1;
+			if (map.get(y + l) == null) {
 				List<Integer> list = new ArrayList<>();
 				list.add(x);
-				map.put(y + 100000, list);
+				map.put(y + l, list);
 			} else {
-				map.get(y + 100000).add(x);
+				map.get(y + l).add(x);
 			}
 		}
 
 		map.keySet().stream().sorted().forEach(k -> {
 			List list = map.get(k).stream().sorted().collect(Collectors.toList());
 			list.stream().forEach(e -> {
-				System.out.println(e + " " + (k - 100000));
+				System.out.println(e + " " + (k - l));
 			});
 		});
 	}
