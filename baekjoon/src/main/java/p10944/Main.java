f7b37029624b165d74e97c53a6aa87e2193b5cfa
genius.choi <csj4032@gmail.com>
2018-02-01 00:46:34 +0900

    백준 문제 10944 랜덤문제 5분에 1번 -_-;;

---
+++ baekjoon/src/main/java/p10944/Main.java
@@ -0,0 +1,11 @@
+package p10944;
+
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int random = new Random().nextInt(10000);
+		System.out.print(random);
+	}
+}
