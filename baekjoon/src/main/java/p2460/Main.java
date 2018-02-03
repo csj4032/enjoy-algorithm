6723e8c099a41be051f251f472182e1c5d90208c
genius.choi <csj4032@gmail.com>
2018-02-03 17:33:19 +0900

    백준 문제 2460 완료

---
+++ baekjoon/src/main/java/p2460/Main.java
@@ -0,0 +1,21 @@
+package p2460;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int max = 0;
+		int k = 0;
+		for (int i = 0; i < 10; i++) {
+			k = k - sc.nextInt();
+			k = k + sc.nextInt();
+			if (max < k) {
+				max = k;
+			}
+		}
+
+		System.out.println(max);
+	}
+}
