259f0fc74028e0d63cb1e075545e226ea598f256
genius.choi <csj4032@gmail.com>
2018-02-05 09:59:23 +0900

    백준 문제 10451 진행

--- baekjoon/src/main/java/p10451/Main.java
+++ baekjoon/src/main/java/p10451/Main.java
@@ -1,5 +1,6 @@
 package p10451;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,10 +12,20 @@ public class Main {
 		for (int i = 0; i < t; i++) {
 			int n = sc.nextInt();
 			int[] m = new int[n];
+			int s = 0;
 			for (int j = 0; j < n; j++) {
-				m[i] = sc.nextInt();
+				m[j] = sc.nextInt();
 			}
-
+			for (int j = 0; j < n; j++) {
+				int k = m[j];
+				while (m[j] != k) {
+					k = m[k] - 1;
+					if (k == m[j]) {
+						s++;
+					}
+				}
+			}
+			System.out.println(s);
 		}
 	}
 }
