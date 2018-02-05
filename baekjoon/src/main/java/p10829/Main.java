36fa49829d5a23aafd2ac85609df8c0d20611775
genius.choi <genius.choi@wemakeprice.com>
2018-02-05 10:47:16 +0900

    백준 문제 10829 완료

---
+++ baekjoon/src/main/java/p10829/Main.java
@@ -0,0 +1,10 @@
+package p10829;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Long.toBinaryString(new Scanner(System.in).nextLong()));
+	}
+}
