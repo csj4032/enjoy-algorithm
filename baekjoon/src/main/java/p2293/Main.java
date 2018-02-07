b07051d7df9deaedb20f3b1e07db73b12015d082
genius.choi <csj4032@gmail.com>
2018-02-07 15:51:30 +0900

    백준 문제 2294 완료

--- baekjoon/src/main/java/p2293/Main.java
+++ baekjoon/src/main/java/p2293/Main.java
@@ -1,5 +1,6 @@
 package p2293;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 // 동전 1
@@ -11,7 +12,11 @@ public class Main {
 		int m = sc.nextInt();
 		int[] l = new int[n + 1];
 		int[] dp = new int[m + 1];
+		int[] dp2 = new int[m + 1];
 		dp[0] = 1;
+
+		Arrays.fill(dp2, 100000);
+		dp2[0] = 0;
 
 		for (int i = 0; i < n; i++) {
 			l[i] = sc.nextInt();
@@ -20,10 +25,14 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			for (int j = l[i]; j <= m; j++) {
 				dp[j] += dp[j - l[i]];
+				dp2[j] = Math.min(dp2[j], dp2[j - l[i]]+1);
 			}
+			System.out.println();
 		}
 
-		//System.out.println(Arrays.toString(dp));
+		System.out.println(Arrays.toString(dp));
+		System.out.println(Arrays.toString(dp2));
 		System.out.println(dp[m]);
+		System.out.println(dp2[m]);
 	}
 }
