7f74ee499ffbfa772f4af2d72e56fd612afbc445
genius.choi <csj4032@gmail.com>
2018-02-10 02:22:31 +0900

    백준 문제 1002 완료

--- baekjoon/src/main/java/p1002/Main.java
+++ baekjoon/src/main/java/p1002/Main.java
@@ -21,38 +21,25 @@ public class Main {
 	}
 
 	private static int turret(int x1, int y1, int r1, int x2, int y2, int r2) {
-
-
-		if (Math.abs(x1 - x2) == r1 + r2 && y1 != y2) {
-			return 0;
-		}
-
-		if (Math.abs(y1 - y2) == r1 + r2 && x1 != x2) {
-			return 0;
-		}
-
-		if (Math.abs(x1 - x2) == r1 + r2 && y1 == y2) {
-			return 1;
-		}
-
-		if (Math.abs(y1 - y2) == r1 + r2 && x1 == x2) {
-			return 1;
-		}
-
-		if (Math.abs(x1 - x2) < r1 + r2 || Math.abs(y1 - y2) < r1 + r2) {
-			if (x1 == x2 && y1 == y2 && r1 != r2) {
-				return 0;
+		int result = 0;
+		if (x1 == x2 && y1 == y2) {
+			if (r1 == r2) {
+				result = -1;
+			} else {
+				result = 0;
 			}
-			if (x1 == x2 && y1 == y2 && r1 == r2) {
-				return -1;
+		} else {
+			if (r1 + r2 == dist(x1, x2, y1, y2) || Math.abs(r1 - r2) == dist(x1, x2, y1, y2)) {
+				result = 1;
 			}
-			return 2;
+			if (r1 + r2 > dist(x1, x2, y1, y2) && Math.abs(r1 - r2) < dist(x1, x2, y1, y2)) {
+				result = 2;
+			}
 		}
+		return result;
+	}
 
-		if (Math.abs(x1 - x2) > r1 + r2 || Math.abs(y1 - y2) > r1 + r2) {
-			return 0;
-		}
-
-		return 0;
+	private static double dist(int x1, int x2, int y1, int y2) {
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 	}
 }
