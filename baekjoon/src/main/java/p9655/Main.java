dd9d930f459c841d20b5d129afce917df364d407
genius.choi <csj4032@gmail.com>
2018-02-10 12:13:10 +0900

    백준 문제 9655 완료

---
+++ baekjoon/src/main/java/p9655/Main.java
@@ -0,0 +1,15 @@
+package p9655;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if (new Scanner(System.in).nextInt() % 2 == 0) {
+			System.out.println("CY");
+		} else {
+			System.out.println("SK");
+
+		}
+	}
+}
