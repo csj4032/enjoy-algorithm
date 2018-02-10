3247c549b4fbad5e10f36ea7ee412a8ce73f358b
genius.choi <genius.choi@wemakeprice.com>
2018-02-10 11:25:54 +0900

    백준 문제 13410 완료

--- baekjoon/src/main/java/p13410/Main.java
+++ baekjoon/src/main/java/p13410/Main.java
@@ -1,24 +1,36 @@
 package p13410;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] ins = br.readLine().split(" ");
+		int n = Integer.parseInt(ins[0]);
+		int k = Integer.parseInt(ins[1]);
 		int max = 0;
 
-		for (int i = n; i <= n; i++) {
-			for (int j = 1; j <= k; j++) {
-				StringBuilder sb = new StringBuilder();
-				int t = Integer.valueOf(sb.append(String.valueOf(i*j)).reverse().toString());
-				if(max < t) {
-					max = t;
-				}
+		while (k > 0) {
+			int t = 0;
+			int j = k * n;
+			t = getReverse(j, t);
+			if (max < t) {
+				max = t;
 			}
+			k--;
 		}
 		System.out.println(max);
+	}
+
+	private static int getReverse(int j, int t) {
+		while (j > 0) {
+			t *= 10;
+			t += j % 10;
+			j /= 10;
+		}
+		return t;
 	}
 }
