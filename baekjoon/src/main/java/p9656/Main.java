410bfb5dc064b51efe9c008153312b9af0ff955e
genius.choi <csj4032@gmail.com>
2018-02-10 12:37:45 +0900

    백준 문제 9656 완료

---
+++ baekjoon/src/main/java/p9656/Main.java
@@ -0,0 +1,14 @@
+package p9656;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if (new Scanner(System.in).nextInt() % 2 == 0) {
+			System.out.println("SK");
+		} else {
+			System.out.println("CY");
+		}
+	}
+}
