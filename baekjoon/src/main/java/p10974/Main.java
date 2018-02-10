df0d3a50d38eb5205cb8a403512e045d61ec0ec8
genius.choi <csj4032@gmail.com>
2018-02-10 17:30:50 +0900

    백준 문제 10974 순열 생각보다 어렵다.

--- baekjoon/src/main/java/p10974/Main.java
+++ baekjoon/src/main/java/p10974/Main.java
@@ -1,24 +1,51 @@
 package p10974;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
+import static java.util.stream.Collectors.joining;
+
+// 순열
 public class Main {
+
+	static List<String> list = new ArrayList<>();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-
-		//for (int i = 0; i < n; i++) {
-			recursive(0, n);
-		//}
+		int[] m = new int[n];
+		for (int i = 1; i <= n; i++) {
+			m[i - 1] = i;
+		}
+		recursive(0, n, m);
+		display(list);
 	}
 
-	private static void recursive(int k, int n) {
-		if (n <= 0) return;
-		System.out.print(k);
-		recursive(k + 1, n - 1);
-		System.out.print(k);
-		recursive(k + 1, n - 1);
-		System.out.println();
+	private static void recursive(int k, int n, int[] m) {
+		if (k == n) {
+			list.add(Arrays.stream(m).mapToObj(String::valueOf).collect(joining()));
+			return;
+		}
+
+		for (int i = k; i < n; i++) {
+			swap(m, i, k);
+			recursive(k + 1, n, m);
+			swap(m, i, k);
+		}
+	}
+
+	private static void display(List<String> list) {
+		list.stream().sorted().forEach(e -> {
+			String[] s = e.split("");
+			System.out.println(Arrays.stream(s).collect(joining(" ")));
+		});
+	}
+
+	private static void swap(int[] m, int i, int k) {
+		int a = m[i];
+		m[i] = m[k];
+		m[k] = a;
 	}
 }
