f9a0ee0784810ac1d6c64fd335915b203bb95ecc
genius.choi <genius.choi@wemakeprice.com>
2018-02-12 17:07:23 +0900

    백준 문제 2447 완료

--- baekjoon/src/main/java/p2447/Main.java
+++ baekjoon/src/main/java/p2447/Main.java
@@ -6,31 +6,45 @@ public class Main {
 
 	static String a = "***";
 	static String b = "* *";
-	static String c = "***";
+	static String c = "   ";
+	static int nn;
+	static int mm;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt() / 3;
+		mm = sc.nextInt();
+		nn = mm / 3;
 
-		for (int j = 0; j < n; j++) {
+		if (mm == 1) {
+			System.out.print("*");
+			return;
+		}
 
-			for (int i = 0; i < n; i++) {
-				System.out.print(a);
-			}
+		for (int i = 0; i < nn; i++) {
+			display(nn, i, a);
+			display(nn, i, b);
+			display(nn, i, a);
+		}
+	}
 
-			System.out.println();
-
-			for (int i = 0; i < n; i++) {
-				System.out.print(b);
-			}
-
-			System.out.println();
-
-			for (int i = 0; i < n; i++) {
+	private static void display(int n, int i, String s) {
+		for (int k = 0; k < n; k++) {
+			if (recursive(n, i, k)) {
+				System.out.print(s);
+			} else {
 				System.out.print(c);
 			}
-
-			System.out.println();
 		}
+		System.out.println();
+	}
+
+	private static boolean recursive(int n, int i, int k) {
+		while (n > 1) {
+			if (((i / (nn / n)) % 3 == 1) && ((k / (nn / n)) % 3 == 1)) {
+				return false;
+			}
+			n = n / 3;
+		}
+		return true;
 	}
 }
