c503cd269f807492891a2981292dc245d53167bd
genius.choi <csj4032@gmail.com>
2018-02-13 03:32:56 +0900

    백준 문제 1182 완료

---
+++ baekjoon/src/main/java/p1182/Main3.java
@@ -0,0 +1,27 @@
+package p1182;
+
+import java.util.Scanner;
+
+public class Main3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] m = new int[n];
+		int t = 0;
+
+		for (int i = 0; i < n; i++) m[i] = sc.nextInt();
+
+		for (int i = 1; i < (1 << (n)); i++) {
+			int sum = 0;
+			for (int j = 0; j < n; j++) {
+				if (0 < (i & (1 << j))) {
+					sum += m[j];
+				}
+			}
+			if (sum == k) t++;
+		}
+
+		//System.out.print(t);
+	}
+}
