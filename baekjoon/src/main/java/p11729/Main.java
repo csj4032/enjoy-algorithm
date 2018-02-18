2f644cea22082c853a824b1c406a9471983e72e5
genius.choi <csj4032@gmail.com>
2018-02-19 02:48:27 +0900

    백준 문제 11729 진행

---
+++ baekjoon/src/main/java/p11729/Main.java
@@ -0,0 +1,8 @@
+package p11729;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
--- baekjoon/src/main/java/p3049/Main.java
+++ baekjoon/src/main/java/p3049/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int n = new Scanner(System.in).nextInt();
 		System.out.println(n * (n - 1) * (n - 2) * (n - 3) / 24);
 	}
 }
