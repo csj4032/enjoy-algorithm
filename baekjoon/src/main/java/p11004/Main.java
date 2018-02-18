3a22775da1c74f90a537ffe8bc342bdb5d2488c3
genius.choi <csj4032@gmail.com>
2018-02-18 15:07:41 +0900

    백준 문제 11004 수정

--- baekjoon/src/main/java/p11004/Main.java
+++ baekjoon/src/main/java/p11004/Main.java
@@ -1,6 +1,5 @@
 package p11004;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +14,45 @@ public class Main {
 			m[i] = sc.nextInt();
 		}
 
-		Arrays.sort(m);
-		System.out.println(m[k-1]);
+		System.out.println(quickSelect(m, m.length, k - 1));
+	}
+
+	private static int quickSelect(int[] array, int length, int k) {
+
+		if (array == null || length <= k) return -1;
+
+		int from = 0;
+		int to = length - 1;
+
+		while (from < to) {
+
+			int i = from;
+			int j = to;
+			int mid = array[(i + j) / 2];
+
+			while (i < j) {
+				if (array[i] >= mid) {
+					swap(array, i, j);
+					j--;
+				} else {
+					i++;
+				}
+			}
+
+			if (array[i] > mid) i--;
+
+			if (k <= i) {
+				to = i;
+			} else {
+				from = i + 1;
+			}
+		}
+		return array[k];
+	}
+
+	private static void swap(int[] array, int i, int j) {
+		int tmp = array[j];
+		array[j] = array[i];
+		array[i] = tmp;
 	}
 }
