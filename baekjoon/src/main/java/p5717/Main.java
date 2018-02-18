d46d179d2db2ebaf30897cbccb16d0c6e8b43b5e
genius.choi <csj4032@gmail.com>
2018-02-18 21:01:20 +0900

    백준 문제 5717 완료

---
+++ baekjoon/src/main/java/p5717/Main.java
@@ -0,0 +1,16 @@
+package p5717;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int m = sc.nextInt();
+			int f = sc.nextInt();
+			if (m == 0 && f == 0) break;
+			System.out.println(m + f);
+		}
+	}
+}
