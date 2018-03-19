d0e87298f2d2fa033286def7af2c8d9ab8a2f01e
genius.choi <csj4032@gmail.com>
2018-03-19 20:21:21 +0900

    Question5 완료

--- mailprogramming/src/main/java/Question8.java
+++ mailprogramming/src/main/java/Question8.java
@@ -11,9 +11,14 @@ public class Question8 {
 		solve(new int[]{1, 1, 3});
 		solve(new int[]{0, 1, 3});
 		solve(new int[]{-1, -10, 2, -2});
+		solve(new int[]{-1});
 	}
 
 	private static void solve(int[] array) {
+		if (array.length < 2) {
+			System.out.println("Does not exist");
+			return;
+		}
 		int first = array[0];
 		int second = array[1];
 		for (int i = 0; i < array.length; i++) {
