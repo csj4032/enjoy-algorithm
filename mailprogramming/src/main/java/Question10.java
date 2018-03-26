87a4fa343259608a9d479f7868eb381e5ccf6d9a
genius.choi <genius.choi@wemakeprice.com>
2018-03-26 16:24:35 +0900

    Question10 완료

---
+++ mailprogramming/src/main/java/Question10.java
@@ -0,0 +1,39 @@
+public class Question10 {
+
+	public static void main(String[] args) {
+		solve("");
+		solve("a");
+		solve("ab");
+		solve("aabcbcbc");
+		solve("aaaaaaaa");
+		solve("abbbcedd");
+		solve("abcdefghijk");
+		solve("aaaabbbfffadakdef");
+	}
+
+	private static void solve(String str) {
+		int length = str.length();
+		String max = "";
+		String temp = "";
+
+		for (int j = 0; j < length; j++) {
+			boolean overlap = false;
+			int t = 0;
+			for (int k = 0; k < temp.length(); k++) {
+				if (temp.substring(k, k + 1).equals(str.substring(j, j + 1))) {
+					overlap = true;
+					t = k;
+				}
+			}
+			if (!overlap) {
+				temp += str.substring(j, j + 1);
+				if (max.length() < temp.length()) max = temp;
+			} else {
+				if (max.length() < temp.length()) max = temp;
+				j = j - temp.length() + t + 1;
+				temp = str.substring(j, j + 1);
+			}
+		}
+		System.out.println(max);
+	}
+}
