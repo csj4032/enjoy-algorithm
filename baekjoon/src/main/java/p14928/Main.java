10c67ff0bebaa634e1f385c2e158a1d91ca6e36d
genius.choi <csj4032@gmail.com>
2018-03-30 01:53:14 +0900

    백준 문제 14928 완료

--- baekjoon/src/main/java/p14928/Main.java
+++ baekjoon/src/main/java/p14928/Main.java
@@ -6,35 +6,39 @@ import java.util.Scanner;
 
 /**
  * https://ko.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-exponentiation
+ * System.out.println(130000000L % 20000303);
+ * System.out.println((100000000L % 20000303 + (10000000L % 20000303) * 3) % 20000303);
  */
 public class Main {
 
-	static String m = "20000303";
+	static BigInteger modular = new BigInteger("20000303");
+	static BigInteger result = BigInteger.ZERO;
+	static BigInteger decimal = BigInteger.TEN;
 
 	public static void main(String[] args) throws IOException {
-//		Scanner sc = new Scanner(System.in);
-//		BigInteger n = new BigInteger(sc.next());
-//		BigInteger m = BigInteger.TEN;
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //
-//		int k = 0;
-//		while (n.compareTo(new BigInteger("20000303")) > 0) {
-//			n = n.divide(BigInteger.TEN);
-//			k++;
+//		String numbers = br.readLine();
+//		int length = numbers.length();
+//
+//		for (int i = length - 1; i >= 0; i--) {
+//			BigInteger exponent = new BigInteger(String.valueOf(i));
+//			BigInteger first = new BigInteger(String.valueOf(numbers.charAt(length - i - 1)));
+//			result = result.add(decimal.modPow(exponent, modular).mod(modular).multiply(first));
 //		}
-//		System.out.println(k);
-//		System.out.println(n.mod(new BigInteger("20000303")));
-		//System.out.println(100000000000000L%2000303);
-		//System.out.println((10000000L%2000303) * (10000000L%2000303)%2000303);
+//
+//		bw.write(result.mod(modular).toString());
+//		bw.flush();
 
+		char[] N = new Scanner(System.in).next().toCharArray();
 
-		System.out.println(21100000000000000L % 20000303);
-		System.out.println(10000000000000000L % 20000303 + 10000000000000000L % 20000303 + 1000000000000000L % 20000303 + 100000000000000L % 20000303);
+		int remainder = 0;
+		for (int i = 0; i < N.length; i++) {
+			remainder = (remainder * 10 + N[i] - '0') % 20000303;
+			System.out.println(remainder);
+		}
 
-		System.out.println(20000000000000000L % 20000303);
-		System.out.println(10000000000000000L % 20000303 + 10000000000000000L % 20000303);
-
-
-		System.out.println((100000000L % 20000303) * (100000000L % 20000303) % 20000303);
-		System.out.println(10000000000000000L % 20000303);
+		System.out.print(remainder);
 	}
 }
