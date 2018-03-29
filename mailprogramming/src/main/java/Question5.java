39082d0033ac2ebcbf58c726fc97f5ad6c14e01a
genius.choi <genius.choi@wemakeprice.com>
2018-03-29 18:26:42 +0900

    Question5 재풀이

--- mailprogramming/src/main/java/Question5.java
+++ mailprogramming/src/main/java/Question5.java
@@ -1,10 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Question5 {
 
 	static int arrayLength = 200000001;
 	static int arrayHaftLength = 100000000;
 
 	public static void main(String[] args) {
-		solve(new int[]{2, 5, 6, 1, 10}, 8);
+		solve2(new Integer[]{2, 5, 6, 1, 10}, 8);
+	}
+
+	private static void solve2(Integer[] array, int target) {
+		int len = array.length;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < len; i++) {
+			map.put(array[i], i);
+			if (null != map.get(target - array[i])) {
+				Integer a = target - array[i];
+				System.out.println(map.get(a) + " " + i);
+			}
+		}
+		System.out.println(map);
 	}
 
 	private static int[][] solve(int[] array, int target) {
@@ -21,8 +37,6 @@ public class Question5 {
 			if (bool[array[i] + arrayHaftLength] && bool[target - array[i] + arrayHaftLength]) {
 				bool[array[i] + arrayHaftLength] = false;
 				System.out.println("[" + i + ", " + idx[target - array[i] + arrayHaftLength] + "]");
-				//result[i][0] = i;
-				//result[i][1] = idx[target - array[i] + arrayHaftLength];
 			}
 		}
 		return result;
