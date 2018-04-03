553f6e07232aa9c653af3dd3ac3046b2c04755dc
genius.choi <csj4032@gmail.com>
2018-04-03 11:11:29 +0900

    백준 9251 문제 진행

---
+++ baekjoon/src/main/java/p9251/Main.java
@@ -0,0 +1,19 @@
+package p9251;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static int[] m = new int[1001];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		String b = sc.nextLine();
+		longestCommonSubsequence(a, b);
+	}
+
+	private static int longestCommonSubsequence(String a, String b) {
+		return 0;
+	}
+}
