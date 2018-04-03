f7bab77bf48127f05ee5eec6fe049a2739392ad0
genius.choi <genius.choi@wemakeprice.com>
2018-04-03 11:21:17 +0900

    백준 9251 문제 진행

--- baekjoon/src/main/java/p9251/Main.java
+++ baekjoon/src/main/java/p9251/Main.java
@@ -2,6 +2,9 @@ package p9251;
 
 import java.util.Scanner;
 
+/**
+ * https://ko.wikipedia.org/wiki/%EC%B5%9C%EC%9E%A5_%EA%B3%B5%ED%86%B5_%EB%B6%80%EB%B6%84_%EC%88%98%EC%97%B4
+ */
 public class Main {
 
 	static int[] m = new int[1001];
