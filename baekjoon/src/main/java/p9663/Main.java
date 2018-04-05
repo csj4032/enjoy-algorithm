5931aa4565fb4e07f223e68bd68c08e4598b3c57
genius.choi <csj4032@gmail.com>
2018-04-05 17:36:05 +0900

    백준 9663 문제 진행

--- baekjoon/src/main/java/p9663/Main.java
+++ baekjoon/src/main/java/p9663/Main.java
@@ -1,44 +1,40 @@
 package p9663;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	static int n;
-	static int[] m;
+	static int[] cols;
+	static int k;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		m = new int[n];
+		cols = new int[n+1];
 		recursive(0);
-		System.out.println(Arrays.toString(m));
+		System.out.println(k);
 	}
 
-	private static void recursive(int l) {
-
-		if (l == n) {
-			System.out.println(Arrays.toString(m));
-			return;
-		}
-
-		for (int i = 0; i < n; i++) {
-			if (promise(l, i)) {
-				m[l] = i;
-				recursive(l + 1);
-			}
-		}
-	}
-
-	private static boolean promise(int l, int t) {
-		for (int i = 0; i < l; i++) {
-//			if (m[i] ==l || (Math.abs(m[i] - m[l]) == Math.abs(l - i))) {
-//				return false;
-//			}
-			if (m[i] == l || (Math.abs(l - m[i]) == t - i))
+	private static boolean recursive(int row) {
+		if (!promising(row)) {
 			return false;
+		} else if (row == n) {
+			k++;
+			return true;
+		}
 
+		for (int i = 1; i <= n; i++) {
+			cols[row + 1] = i;
+			if (recursive(row + 1)) return false;
+		}
+		return false;
+	}
+
+	private static boolean promising(int row) {
+		for (int i = 1; i < row; i++) {
+			if (cols[i] == cols[row]) return false;
+			if ((row - i) == Math.abs(cols[row] - cols[i])) return false;
 		}
 		return true;
 	}
