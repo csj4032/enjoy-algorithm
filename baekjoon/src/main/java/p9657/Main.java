07b9c0a5e0c85cd2fce8626ca3e77c01bee0c4dd
genius.choi <csj4032@gmail.com>
2018-04-06 17:44:12 +0900

    백준 9657 문제 진행

---
+++ baekjoon/src/main/java/p9657/Main.java
@@ -0,0 +1,16 @@
+package p9657;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n % 7 == 0 || n % 7 == 2) {
+			System.out.println("CY");
+		} else {
+			System.out.println("SK");
+		}
+	}
+}
