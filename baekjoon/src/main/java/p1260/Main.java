5cffad064f75a6c6258acfc36245c180a1f941ae
genius.choi <genius.choi@wemakeprice.com>
2018-04-13 11:31:03 +0900

    백준 13458 문제 완료

--- baekjoon/src/main/java/p1260/Main.java
+++ baekjoon/src/main/java/p1260/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
 	private static StringBuilder sf1;
 	private static StringBuilder sf2;
-	private static int graph[][];
-	private static int visit1[];
-	private static int visit2[];
+	private static boolean graph[][];
+	private static boolean visit[];
+	private static boolean flag;
 	private static int m;
 	private static int n;
 	private static int v;
@@ -21,47 +21,64 @@ public class Main {
 
 		sf1 = new StringBuilder();
 		sf2 = new StringBuilder();
-		graph = new int[n + 1][n + 1];
-		visit1 = new int[n + 1];
-		visit2 = new int[n + 1];
+		graph = new boolean[1001][1001];
+		visit = new boolean[1001];
 
-		for (int i = 1; i < m; i++) {
+		for (int i = 1; i <= m; i++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			graph[x][y] = graph[y][x] = 1;
+			graph[x][y] = graph[y][x] = true;
 		}
 
-		dfs(v);
-		System.out.println(sf1.toString().trim());
-
-
+		dfss(v);
+		visit = new boolean[1001];
 		bfs(v);
+		System.out.println(sf1.toString().trim());
 		System.out.print(sf2.toString().trim());
 	}
 
-	private static void dfs(int v) {
-		visit1[v] = 1;
+	private static void dfsr(int v) {
+		visit[v] = true;
 		sf1.append(v + " ");
 		for (int i = 1; i <= n; i++) {
-			if (visit1[i] != 1 && graph[v][i] == 1) {
-				dfs(i);
+			if (graph[v][i] == true && visit[i] == false) {
+				dfsr(i);
 			}
+		}
+	}
+
+	private static void dfss(int v) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(v);
+		visit[v] = true;
+		sf1.append(v + " ");
+		while (!stack.isEmpty()) {
+			flag = false;
+			int x = stack.peek();
+			for (int i = 1; i <= n; i++) {
+				if (graph[x][i] == true && visit[i] == false) {
+					stack.push(i);
+					visit[i] = true;
+					flag = true;
+					sf1.append(i + " ");
+					break;
+				}
+			}
+			if (!flag) stack.pop();
 		}
 	}
 
 	private static void bfs(int v) {
 		Queue<Integer> queue = new LinkedList();
-		queue.add(v);
-		visit2[v] = 1;
-		//sf2.append(v + " ");
+		queue.offer(v);
+		visit[v] = true;
 		while (!queue.isEmpty()) {
-			int x = queue.peek();
-			queue.poll();
+			int x = queue.poll();
 			sf2.append(x + " ");
 			for (int i = 1; i <= n; i++) {
-				if (graph[x][i] == 1 && visit2[i] != 1) {
-					queue.add(i);
-					visit2[i] = 1;
+				if (graph[x][i] == true && visit[i] == false) {
+					queue.offer(i);
+					visit[i] = true;
 				}
 			}
 		}
