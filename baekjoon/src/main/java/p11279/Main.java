62a7efa30a885b85f17ec5ee3e79491fc3d365c2
genius.choi <csj4032@gmail.com>
2018-04-19 21:40:37 +0900

    백준 문제 9325 완료

---
+++ baekjoon/src/main/java/p11279/Main.java
@@ -0,0 +1,17 @@
+package p11279;
+
+import java.util.Scanner;
+
+public class Main {
+
+	int[] m = new int[100001];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+
+		}
+	}
+}
