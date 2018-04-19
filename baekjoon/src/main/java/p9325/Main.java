1e52ad526ef88059416f0d32ae2297e1daee2a2a
genius.choi <csj4032@gmail.com>
2018-04-19 21:40:37 +0900

    백준 문제 9325 완료

--- baekjoon/src/main/java/p9325/Main.java
+++ baekjoon/src/main/java/p9325/Main.java
@@ -1,4 +1,19 @@
 package p9325;
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			long s = sc.nextLong();
+			int n = sc.nextInt();
+			for (int j = 0; j < n; j++) {
+				s += sc.nextLong() * sc.nextLong();
+			}
+			System.out.println(s);
+		}
+	}
 }
