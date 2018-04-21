75392bd18140fe63b96114e024f1c6e9fa82a4ea
genius.choi <csj4032@gmail.com>
2018-04-21 21:44:37 +0900

    백준 문제 14888 진행

---
+++ baekjoon/src/main/java/p14888/Main.java
@@ -0,0 +1,22 @@
+package p14888;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] m = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			m[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int cnt = sc.nextInt();
+			for (int j = 0; j < cnt; j++) {
+			}
+		}
+	}
+}
