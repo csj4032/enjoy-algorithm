e5ff889d765549aa0649a11fe54c0e38679b20c9
genius.choi <csj4032@gmail.com>
2018-04-28 13:18:31 +0900

    백준 문제 9251 완료

--- baekjoon/src/main/java/p9251/Main.java
+++ baekjoon/src/main/java/p9251/Main.java
@@ -1,22 +1,43 @@
 package p9251;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * https://ko.wikipedia.org/wiki/%EC%B5%9C%EC%9E%A5_%EA%B3%B5%ED%86%B5_%EB%B6%80%EB%B6%84_%EC%88%98%EC%97%B4
+ * https://ko.wikipedia.org/wiki/최장_공통_부분_수열
  */
 public class Main {
-
-	static int[] m = new int[1001];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String a = sc.nextLine();
 		String b = sc.nextLine();
-		longestCommonSubsequence(a, b);
+		System.out.println(longestCommonSubsequence(a, b));
 	}
 
-	private static int longestCommonSubsequence(String a, String b) {
-		return 0;
+	private static int longestCommonSubsequence(String str1, String str2) {
+		String[] arr1 = str1.split("");
+		String[] arr2 = str2.split("");
+
+		int n = arr1.length;
+		int m = arr2.length;
+
+		int dp[][] = new int[n + 1][m + 1];
+		Arrays.fill(dp[0], 0);
+
+		for (int i = 0; i < n; i++) {
+			dp[i][0] = 0;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (arr1[i-1].equals(arr2[j-1])) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+				}
+			}
+		}
+		return dp[n][m];
 	}
 }
