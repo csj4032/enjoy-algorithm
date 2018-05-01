6bc568891f99fa478a1001146255ac0ef1c68a6b
genius.choi <csj4032@gmail.com>
2018-05-01 14:27:07 +0900

    백준 문제 2744 완료

--- baekjoon/src/main/java/p9251/Main.java
+++ baekjoon/src/main/java/p9251/Main.java
@@ -1,6 +1,5 @@
 package p9251;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -23,15 +22,13 @@ public class Main {
 		int m = arr2.length;
 
 		int dp[][] = new int[n + 1][m + 1];
-		Arrays.fill(dp[0], 0);
 
-		for (int i = 0; i < n; i++) {
-			dp[i][0] = 0;
-		}
+		for (int i = 0; i < m; i++) dp[0][i] = 0;
+		for (int i = 0; i < n; i++) dp[i][0] = 0;
 
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
-				if (arr1[i-1].equals(arr2[j-1])) {
+				if (arr1[i - 1].equals(arr2[j - 1])) {
 					dp[i][j] = dp[i - 1][j - 1] + 1;
 				} else {
 					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
