32b543d8ef77f8379844184459d60d0b0730bc8e
genius.choi <csj4032@gmail.com>
2018-05-07 22:46:06 +0900

    백준 문제 9506 진행

---
+++ baekjoon/src/main/java/p9506/Main.java
@@ -0,0 +1,15 @@
+package p9506;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			int n = sc.nextInt();
+			if (n == -1) break;
+			System.out.println(n);
+		}
+	}
+}
