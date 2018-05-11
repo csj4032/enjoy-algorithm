9d367bdde4ea772ad0c622b9a212a17a78dfea44
genius.choi <csj4032@gmail.com>
2018-05-11 22:51:54 +0900

    백준 문제 1000  C  연습

---
+++ cmake-build-debug/CMakeFiles/enjoy_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enjoy_algorithm.dir/baekjoon/src/main/c/1000.c.o"
+  "enjoy_algorithm.pdb"
+  "enjoy_algorithm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/enjoy_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
