5a5e7b02ca5cb27f0355764c4feac0b320c33805
genius.choi <csj4032@gmail.com>
2018-05-29 12:59:01 +0900

    백준 9316 문제 완료

---
+++ baekjoon/src/main/java/p9316/Main.java
@@ -0,0 +1,13 @@
+package p9316;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n = new Scanner(System.in).nextInt();
+		for (int i = 1; i <= n; i++) {
+			System.out.printf("Hello World, Judge %d!\n", i);
+		}
+	}
+}
