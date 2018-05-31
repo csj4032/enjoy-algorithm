eeddc98735efbac745ee1117d2f806122dd5d231
genius.choi <csj4032@gmail.com>
2018-05-31 10:32:11 +0900

    백준 5597 문제 완료

--- baekjoon/src/main/java/p5597/Main.java
+++ baekjoon/src/main/java/p5597/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	static boolean[] b = new boolean[30];
+	static boolean[] b = new boolean[31];
 
 	public static void main(String[] args) {
 
@@ -12,6 +12,12 @@ public class Main {
 
 		for (int i = 0; i < 28; i++) {
 			b[sc.nextInt()] = true;
+		}
+
+		for (int i = 1; i < 31; i++) {
+			if (b[i] == false) {
+				System.out.println(i);
+			}
 		}
 	}
 }
