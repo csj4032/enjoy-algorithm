29076991aa1cfea087104c7cb8ec1435b548c199
genius.choi <genius.choi@wemakeprice.com>
2018-09-20 12:21:24 +0900

    백준 2877 문제 진행

---
+++ baekjoon/src/main/java/p2877/Main.java
@@ -0,0 +1,16 @@
+package p2877;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777, 4444
+
+	static long l[] = new long[999999999];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
+		System.out.println(l.length);
+	}
+}
