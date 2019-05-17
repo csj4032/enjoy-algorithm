e3edf4fd0755b5498e0b53d1f070505f9400841b
genius.choi <csj4032@gmail.com>
2019-05-17 19:12:18 +0900

    Codility StrSymmetryPoint

---
+++ codility/src/main/java/binarySearchAlgorithm/MinMaxDivision.java
@@ -0,0 +1,14 @@
+package binarySearchAlgorithm;
+
+public class MinMaxDivision {
+
+    public static void main(String[] args) {
+        int[] A = {2, 1, 5, 1, 2, 2, 2};
+        int result = new MinMaxDivision().solution(3, 5, A);
+        System.out.println(result);
+    }
+
+    public int solution(int K, int M, int[] A) {
+        return 0;
+    }
+}
