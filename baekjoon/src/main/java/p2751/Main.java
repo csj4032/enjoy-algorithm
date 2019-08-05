0778016a8b5fcf1340dfa83a5db1827422004bc9
genius.choi <csj4032@gmail.com>
2019-08-05 19:00:31 +0900

    백준 문제 2751 진행

--- baekjoon/src/main/java/p2751/Main.java
+++ baekjoon/src/main/java/p2751/Main.java
@@ -1,17 +1,24 @@
 package p2751;
 
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		Integer[] m = new Integer[n];
-		for (int i = 0; i < n; i++) {
-			m[i] = sc.nextInt();
-		}
-		Stream.of(m).sorted().forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] m = new Integer[1_000_001];
+        Arrays.fill(m, 0);
+        for (int i = 0; i < n; i++) {
+            int k = sc.nextInt();
+            m[k] = 1;
+        }
+
+        for (int i = 0; i < 1_000_001; i++) {
+            if (m[i] == 1) {
+                System.out.println(i);
+            }
+        }
+    }
 }
