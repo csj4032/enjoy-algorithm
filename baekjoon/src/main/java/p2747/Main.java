786ae1c8be303301ede970711dfc06dca68dc76d
genius.choi <csj4032@gmail.com>
2019-08-09 03:23:11 +0900

    백준 1330, 2747 등 문제 완료

--- baekjoon/src/main/java/p2747/Main.java
+++ baekjoon/src/main/java/p2747/Main.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class Main {
 
+	private static int[] fibonacci;
+
 	public static void main(String[] args) {
-		System.out.println(fibonacci(new Scanner(System.in).nextInt()));
+		int n = new Scanner(System.in).nextInt();
+		fibonacci = new int[n + 1];
+		fibonacci[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		}
+		System.out.println(fibonacci[n]);
 	}
 
 	private static int fibonacci(int i) {
