477176df469ca384c28082781a7b290f61391137
genius.choi <csj4032@gmail.com>
2019-08-25 02:16:05 +0900

    백준 문제 1918 수정

---
+++ basic/src/test/java/recursion/backtracking/BinaryTest.java
@@ -0,0 +1,12 @@
+package recursion.backtracking;
+
+import org.junit.jupiter.api.Test;
+
+public class BinaryTest {
+
+	@Test
+	public void binaryTest() {
+		Binary binary = new Binary();
+		binary.binary(2, new int[2]);
+	}
+}
