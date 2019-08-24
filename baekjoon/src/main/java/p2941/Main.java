1ef2965cea900d5eda66ab6c2f71b904a78d79de
genius.choi <csj4032@gmail.com>
2019-08-24 16:01:16 +0900

    백준 문제 2941 완료

--- baekjoon/src/main/java/p2941/Main.java
+++ baekjoon/src/main/java/p2941/Main.java
@@ -2,36 +2,15 @@ package p2941;
 
 import java.util.*;
 
+/**
+ * 제목 : 크로아티아 알파벳
+ * 링크 : https://www.acmicpc.net/problem/2941
+ */
 public class Main {
-
-	private static List<String> words = new ArrayList<>();
-
-	static {
-		words.add("c=");
-		words.add("c-");
-		words.add("dz=");
-		words.add("d-");
-		words.add("lj");
-		words.add("nj");
-		words.add("s=");
-		words.add("z=");
-	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
-		Integer len = str.length();
-		for (int i = 0; i < words.size(); i++) {
-			String word = words.get(i);
-			for (int j = 0; j < str.length() - (word.length() - 1); j++) {
-				if (str.substring(j, j + word.length()).equals(word)) {
-					len = len - (word.length() - 1);
-					if (word.equals("z=") && str.substring(j - 1, j + word.length()).equals("dz="))
-						len = len + 1;
-				}
-			}
-		}
-		System.out.println(len);
 		System.out.println(str.replaceAll("c=|c-|dz=|d-|lj|nj|s=|z="," ").length());
 	}
 }
