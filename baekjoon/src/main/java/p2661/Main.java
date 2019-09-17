63c00ae9c988e5b475b57b05c09ebc57f38d656e
genius.choi <csj4032@gmail.com>
2019-09-18 03:51:44 +0900

    백준 문제 1339 완료

--- baekjoon/src/main/java/p2661/Main.java
+++ baekjoon/src/main/java/p2661/Main.java
@@ -4,27 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int[] nn;
-    private static int k;
+	private static int k;
+	private static boolean flag;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        k = sc.nextInt();
-        nn = new int[k];
-        good(0, "");
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		k = sc.nextInt();
+		good(0, "");
+	}
 
-    private static void good(int n, String m) {
-        if (n == k) {
-            return;
-        }
-        for (int i = 1; i <= 3; i++) {
-            if (promise(m + i)) ;
-            good(n + 1, m + i);
-        }
-    }
+	private static void good(int n, String m) {
+		if (n == k) {
+			System.out.println(m);
+			flag = true;
+			return;
+		}
+		for (int i = 1; i <= 3; i++) {
+			if (promise(m + i) && !flag) good(n + 1, m + i);
+		}
+	}
 
-    private static boolean promise(String m) {
-        return true;
-    }
+	private static boolean promise(String m) {
+		int l = m.length();
+		for (int i = 0; i < l / 2; i++) {
+			String a = m.substring(l - ((i + 1) * 2), l - (i + 1));
+			String b = m.substring(l - (i + 1), l);
+			if (a.equals(b)) return false;
+		}
+		return true;
+	}
 }
