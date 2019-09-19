71255d52116be6a91ba5ecd859763dbc113380f3
genius.choi <csj4032@gmail.com>
2019-09-19 15:24:49 +0900

    백준 2480 문제 완료

---
+++ baekjoon/src/main/java/p3486/Main.java
@@ -0,0 +1,22 @@
+package p3486;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            StringBuilder a = new StringBuilder(sc.next());
+            StringBuilder b = new StringBuilder(sc.next());
+            StringBuilder c = a.reverse();
+            StringBuilder d = b.reverse();
+            int e = Integer.parseInt(c.toString());
+            int f = Integer.parseInt(d.toString());
+            sb.append(Integer.parseInt(new StringBuilder(String.valueOf(e + f)).reverse().toString())).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
