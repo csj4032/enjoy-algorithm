2d5df374240c1a789e3bda998ef6649838721707
genius.choi <csj4032@gmail.com>
2019-09-23 16:09:46 +0900

    프로그래머스 스킬 체크 레벨 1

---
+++ programmers/src/main/java/skillChecks/Push.java
@@ -0,0 +1,19 @@
+package skillChecks;
+
+public class Push {
+
+    public String solution(String s, int n) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            int k = s.charAt(i);
+            int c = ' ';
+            if (k >= 65 && k <= 90) {
+                c = 65 + ((s.charAt(i) + n - 65) % 26);
+            } else if (k >= 97 && k <= 122) {
+                c = 97 + ((s.charAt(i) + n - 97) % 26);
+            }
+            answer += Character.toString((c));
+        }
+        return answer;
+    }
+}
