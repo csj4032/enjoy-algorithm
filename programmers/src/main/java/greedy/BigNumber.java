58cd457e699fcc6fdfa97a5f5fb48490065e18fe
genius.choi <csj4032@gmail.com>
2019-09-25 19:01:40 +0900

    프로그래머스 큰 수 만들기

--- programmers/src/main/java/greedy/BigNumber.java
+++ programmers/src/main/java/greedy/BigNumber.java
@@ -1,41 +1,28 @@
 package greedy;
 
-import java.util.Arrays;
-
 public class BigNumber {
 
     public String solution(String number, int k) {
-        String answer = "";
-        char[] numbers1 = new char[number.length()];
-        char[] numbers2 = new char[number.length()];
-
-        for (int i = 0; i < number.length(); i++) {
-            numbers1[i] = number.charAt(i);
-            numbers2[i] = number.charAt(i);
-        }
-
-        Arrays.sort(numbers1);
-
-        System.out.println(Arrays.toString(numbers1));
-        System.out.println(Arrays.toString(numbers2));
-
-        int s = 0;
-        outer:
-        for (int i = 0; i < numbers1.length; i++) {
-            char t = numbers1[i];
-            for (int j = 0; j < numbers2.length; j++) {
-                if (numbers2[j] == t) {
-                    System.out.println(t + " " + numbers2[j]);
-                    numbers2[j] = ' ' ;
-                    s++;
-                    if (k == s) break outer;
-                }
+        StringBuilder sb = new StringBuilder();
+        int l = number.length();
+        int r = l - k;
+        Integer[] numbers = new Integer[l];
+        for (int i = 0; i < l; i++) numbers[i] = Integer.parseInt(String.valueOf(number.charAt(i)));
+        int p = 0;
+        int q = l - r;
+        for (int i = 0; i <= k; i++) {
+            int m = -1;
+            for (int j = p; j <= q; j++) {
+                int a = numbers[j];
+                if (m < a) m = a;
             }
+            sb.append(m);
+            p = number.indexOf(String.valueOf(m), p) + 1;
+            r--;
+            q = l - r;
+            if (p == q) break;
         }
-        System.out.println(Arrays.toString(numbers2));
-
-        for (int i = 0; i < numbers2.length; i++) if (numbers2[i] != ' ') answer += numbers2[i];
-
-        return answer;
+        sb.append(number.substring(q));
+        return sb.toString();
     }
 }
