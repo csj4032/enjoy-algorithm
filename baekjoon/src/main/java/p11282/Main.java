0774b1dd263de93f0b0858a2d4f9c7841ecf4862
genius.choi <csj4032@gmail.com>
2020-04-21 14:09:34 +0900

    백준 문제 11282, 11283 풀이

---
+++ baekjoon/src/main/java/p11282/Main.java
@@ -0,0 +1,12 @@
+package p11282;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var s = (char) (sc.nextInt() + 44031);
+		System.out.println(s);
+	}
+}
---
+++ baekjoon/src/main/java/p11283/Main.java
@@ -0,0 +1,11 @@
+package p11283;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var s = (sc.next().charAt(0) - 44031);
+		System.out.println(s);
+	}
+}
