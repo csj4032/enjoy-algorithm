3f95d2a4dbfeda8d828d097f82479b5e77f42374
genius.choi <csj4032@gmail.com>
2020-05-12 02:56:56 +0900

    백준 문제 14889 진행

---
+++ baekjoon/src/main/java/p14889/Main.java
@@ -0,0 +1,37 @@
+package p14889;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int n;
+	static int[][] m;
+	static boolean[] v;
+	static int[] d;
+
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = new int[n][n];
+		v = new boolean[n];
+		d = new int[n];
+
+		dfs(0, 0);
+	}
+
+	private static void dfs(int s, int l) {
+		if (l >= n / 2) {
+			System.out.println(Arrays.toString(v));
+		} else {
+			for (int i = s; i < n; i++) {
+				if (!v[i]) {
+					v[i] = true;
+					d[l] = i + 1;
+					dfs(i, l + 1);
+				}
+			}
+		}
+		v[s] = false;
+		System.out.println(s + " : " + l);
+	}
+}
