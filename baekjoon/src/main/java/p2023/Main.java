aa08a8fc25c18113b0fa800f371f4dc5777e42d2
genius.choi <csj4032@gmail.com>
2020-05-12 02:54:42 +0900

    백준 문제 14889 문제 진행

--- baekjoon/src/main/java/p2023/Main.java
+++ baekjoon/src/main/java/p2023/Main.java
@@ -9,69 +9,60 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
-    static boolean[] map = new boolean[10];
+	static boolean[] map = new boolean[10];
 
-    static {
-        map[2] = true;
-        map[3] = true;
-        map[5] = true;
-        map[7] = true;
-    }
+	static {
+		map[2] = true;
+		map[3] = true;
+		map[5] = true;
+		map[7] = true;
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int from = 1;
-        int to = 10;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int from = 1;
+		int to = 10;
 
-        for (int i = 0; i < n - 1; i++) {
-            from *= 10;
-            to *= 10;
-        }
-        to -= 1;
+		for (int i = 0; i < n - 1; i++) {
+			from *= 10;
+			to *= 10;
+		}
+		to -= 1;
 
-        for (int i = from; i <= to; i++) {
-            boolean f = true;
-            int t = from;
-            for (int j = 0; j < n; j++) {
-                if (!isPrimeNumber(i / t)) {
-                    f = false;
-                    break;
-                }
-                t = t / 10;
-            }
-            if (f) System.out.println(i);
-        }
+		for (int i = from; i <= to; i++) {
+			boolean f = true;
+			int t = from;
+			for (int j = 0; j < n; j++) {
+				if (!isPrimeNumber(i / t)) {
+					f = false;
+					break;
+				}
+				t = t / 10;
+			}
+			if (f) System.out.println(i);
+		}
 
-        var aa = """
-        SELECT
-            A.NO AS STUDENT_NO,
-            B.NO AS TEACHER_NO
-        FROM
-            STUDENT A
-            INNER JOIN TEACHER B ON A.CLASS_NO = B.CLASS_NO
-        """;
+		var errorCode = 1;
+		String message = switch (errorCode) {
+			case 1, 2, 3, 4:
+				yield "Genius Message";
+			case 404:
+				yield "Not found!";
+			case 500:
+				yield "Internal server error!";
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + errorCode);
+		};
+	}
 
-        var errorCode = 1;
-        String message = switch (errorCode) {
-            case 1, 2, 3, 4:
-                yield "Genius Message";
-            case 404:
-                yield "Not found!";
-            case 500:
-                yield "Internal server error!";
-            default:
-                throw new IllegalArgumentException("Unexpected value: " + errorCode);
-        };
-    }
-
-    private static boolean isPrimeNumber(int t) {
-        if (t < 2) return false;
-        if (t < 10 && !map[t]) return false;
-        int p = (int) Math.sqrt(t);
-        for (int i = 2; i <= p; i++) {
-            if (t % i == 0) return false;
-        }
-        return true;
-    }
+	private static boolean isPrimeNumber(int t) {
+		if (t < 2) return false;
+		if (t < 10 && !map[t]) return false;
+		int p = (int) Math.sqrt(t);
+		for (int i = 2; i <= p; i++) {
+			if (t % i == 0) return false;
+		}
+		return true;
+	}
 }
