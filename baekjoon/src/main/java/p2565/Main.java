e7ba5f11b8b5da65d8b95b197b4d022aa0b40d6c
genius.choi <genius.choi@wemakeprice.com>
2020-06-09 12:24:47 +0900

    백문 문제 2565 완료

--- baekjoon/src/main/java/p2565/Main.java
+++ baekjoon/src/main/java/p2565/Main.java
@@ -8,14 +8,15 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = 0;
-		int[] dp = new int[501];
-		int[] line = new int[501];
+		int[] dp = new int[510];
+		int[] line = new int[510];
 
 		for (int i = 0; i < n; i++) line[sc.nextInt()] = sc.nextInt();
 
-		Arrays.fill(dp, 1);
-		for (int i = 1; i < 500; i++) {
+		Arrays.fill(dp, 0);
+		for (int i = 1; i < 510; i++) {
 			if (line[i] == 0) continue;
+			dp[i] = 1;
 			for (int j = 1; j < i; j++) {
 				if (line[i] > line[j] && dp[j] + 1 > dp[i]) {
 					dp[i] = dp[j] + 1;
