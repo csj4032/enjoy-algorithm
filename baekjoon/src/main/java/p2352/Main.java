7549bfb31f31b5f736981ecb70ec55542a719f2c
genius.choi <csj4032@gmail.com>
2020-06-12 16:54:31 +0900

    백문 문제 2352 완료

---
+++ baekjoon/src/main/java/p2352/Main.java
@@ -0,0 +1,32 @@
+package p2352;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine().trim());
+		String nn[] = br.readLine().split(" ");
+		int[] m = new int[n];
+		int[] dp = new int[n];
+		int k=0;
+
+		for (int i = 0; i < n; i++) m[i] = Integer.parseInt(nn[i]);
+
+		Arrays.fill(dp, 1);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (m[i] > m[j] && dp[i] < dp[j] + 1) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+			k = Math.max(k, dp[i]);
+		}
+
+		System.out.println(k);
+	}
+}
