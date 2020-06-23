dfc6e42feee15d1be47cbd958b63ad31b4059788
genius.choi <csj4032@gmail.com>
2020-06-23 16:58:36 +0900

    동적기획법

--- baekjoon/src/main/java/p1067/Main.java
+++ baekjoon/src/main/java/p1067/Main.java
@@ -1,32 +1,42 @@
 package p1067;
 
-import java.util.ArrayDeque;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class Main {
 
 	private static int n;
+	private static Integer[] l;
+	private static long m = 0l;
 
-	public static void main(String[] args) {
-		var sc = new Scanner(System.in);
-		n = sc.nextInt();
-		ArrayDeque deque = new ArrayDeque();
-		int[] l = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine().trim());
+		LinkedList<Integer> deque = new LinkedList();
+		l = new Integer[n];
 
-		for (int i = 0; i < n; i++) deque.add(sc.nextInt());
-		for (int i = 0; i < n; i++) l[i] = sc.nextInt();
+		String[] x = br.readLine().split(" ");
+		String[] y = br.readLine().split(" ");
 
-
-		int m = 0;
 		for (int i = 0; i < n; i++) {
+			Integer x1 = Integer.valueOf(x[i]);
+			Integer y1 = Integer.valueOf(y[i]);
+			deque.add(x1);
+			l[i] = y1;
+		}
+
+		for (int i = 0; i < n; i++) {
+			deque.addLast(deque.poll());
 			m = Math.max(m, cal(deque, l));
 		}
+		System.out.println(m);
 	}
 
-	private static int cal(ArrayDeque deque, int[] l) {
-		for (int i = 0; i < n; i++) {
-		}
-		return 0;
-
+	private static long cal(LinkedList<Integer> deque, Integer[] l) {
+		long sum = 0;
+		for (int i = 0; i < n; i++) sum += l[i] * deque.get(i);
+		return sum;
 	}
 }
