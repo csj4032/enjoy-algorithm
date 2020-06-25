769bf485017869ace2ba3e5c8ca34c775ee07d5b
genius.choi <csj4032@gmail.com>
2020-06-25 14:40:01 +0900

    백문 문제 1365 완료

--- baekjoon/src/main/java/p1365/Main.java
+++ baekjoon/src/main/java/p1365/Main.java
@@ -1,32 +1,55 @@
 package p1365;
 
 import java.io.*;
-import java.util.Arrays;
 
-// https://yhwan.tistory.com/18
+/**
+ * 제목 : 꼬인 전깃줄
+ * 링크 : https://www.acmicpc.net/problem/1365
+ * 분류 : LIS, Lower Bound
+ * 참고 : https://mygumi.tistory.com/303, https://yhwan.tistory.com/18
+ */
 public class Main {
+
+	private static int dp[];
+	private static int m[];
+	private static int k;
+	private static int n;
+	private static String nn[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine().trim());
-		String nn[] = br.readLine().split(" ");
-		int m[] = new int[n];
-		int dp[] = new int[n];
-		int k = 0;
+		n = Integer.parseInt(br.readLine().trim());
+		nn = br.readLine().split(" ");
+		m = new int[n];
+		dp = new int[n];
 
 		for (int i = 0; i < n; i++) m[i] = Integer.parseInt(nn[i]);
 
-		Arrays.fill(dp, 1);
-
+		dp[0] = m[0];
+		int idx = 0;
 		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < i; j++) {
-				if (m[i] > m[j] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;
-				}
+			if (dp[idx] < m[i]) {
+				dp[++idx] = m[i];
+			} else {
+				dp[lowerBound(0, idx, m[i], dp) - 1] = m[i];
 			}
-			k = Math.max(k, dp[i]);
 		}
+
+		for (int i = 0; i < n; i++) if (dp[i] > 0) k++;
 		System.out.println(n - k);
+	}
+
+	private static int lowerBound(int start, int end, int target, int[] list) {
+		int mid = 0;
+		while (start < end) {
+			mid = (start + end) / 2;
+			if (list[mid] < target) {
+				start = mid + 1;
+			} else {
+				end = mid;
+			}
+		}
+		return end + 1;
 	}
 }
