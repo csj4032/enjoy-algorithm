fa9b01d3d4822706802e132cd212a9ebe59846a9
genius.choi <csj4032@gmail.com>
2020-06-27 03:23:50 +0900

    백준 문제 1021 완료

--- baekjoon/src/main/java/p1021/Main.java
+++ baekjoon/src/main/java/p1021/Main.java
@@ -1,28 +1,36 @@
 package p1021;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int k = 0;
-		int current = 1;
-		int target = 0;
-		int l[] = new int[10];
-
-		for (int i = 1; i <= 10; i++) l[i - 1] = i;
-
+		var n = sc.nextInt();
+		var m = sc.nextInt();
+		var k = 0;
+		LinkedList<String> queue = new LinkedList<>();
+		for (int i = 1; i <= n; i++) queue.add(String.valueOf(i));
 		for (int i = 0; i < m; i++) {
-			target = sc.nextInt();
-			int index = Arrays.binarySearch(l, target);
-			if (current == target) continue;
-
-			n = n - 1;
+			String t = sc.next();
+			var idx = queue.indexOf(t);
+			if (idx != 0) {
+				var q = idx;
+				var p = queue.size() - idx;
+				if (q < p) {
+					for (int j = 0; j < q; j++) {
+						k++;
+						queue.addLast(queue.pop());
+					}
+				} else {
+					for (int j = 0; j < p; j++) {
+						k++;
+						queue.addFirst(queue.removeLast());
+					}
+				}
+			}
+			queue.pop();
 		}
-
+		System.out.println(k);
 	}
 }
