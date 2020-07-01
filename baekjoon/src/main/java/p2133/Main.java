b2064140d244d38b89fe9151f0e42173634062e5
genius.choi <csj4032@gmail.com>
2020-07-02 03:27:13 +0900

    백준 문제 2133 진행

--- baekjoon/src/main/java/p2133/Main.java
+++ baekjoon/src/main/java/p2133/Main.java
@@ -1,26 +1,27 @@
 package p2133;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * 제목 : 타일 채우기
+ * 링크 : https://www.acmicpc.net/problem/2133
+ * 분류 : Dynamic Programming
+ */
 public class Main {
 
 	static long[] dp = new long[31];
 
 	static {
 		dp[0] = 0;
-		dp[1] = 2;
+		dp[1] = 0;
 		dp[2] = 3;
-		dp[3] = 6;
+		dp[3] = 0;
 		dp[4] = 11;
-
-		for (int i = 3; i < 31; i++) {
-			dp[i] = dp[i - 2] * 2 + 2;
-		}
+		dp[5] = 0;
+		dp[6] = 41;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(dp));
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println(dp[n]);
