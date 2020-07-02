9d9580a19c9edba752c9132b4417e75e5ddc7894
genius.choi <csj4032@gmail.com>
2020-07-02 13:44:01 +0900

    백문 문제 10811 완료

--- baekjoon/src/main/java/p18883/Main.java
+++ baekjoon/src/main/java/p18883/Main.java
@@ -5,18 +5,55 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		var sc = new Scanner(System.in);
-		var n = sc.nextInt();
-		var m = sc.nextInt();
-		var sb = new StringBuilder();
-		for (int i = 1; i <= n * m; i++) {
-			sb.append(i);
-			if (i % m == 0) {
-				sb.append("\n");
+//		var sc = new Scanner(System.in);
+//		var n = sc.nextInt();
+//		var m = sc.nextInt();
+//		var sb = new StringBuilder();
+//		for (int i = 1; i <= n * m; i++) {
+//			sb.append(i);
+//			if (i % m == 0) {
+//				sb.append("\n");
+//			} else {
+//				sb.append(" ");
+//			}
+//		}
+//		System.out.println(sb.toString());
+
+		int[] data = {4, 7, 5, 9, 6};
+		int[] data2 = {6 ,9 ,5 ,7 ,4};
+		System.out.println(lowerBound(data, 1,4));
+		System.out.println(lowerBound(data, 2,7));
+		System.out.println(lowerBound(data, 3,5));
+		System.out.println(lowerBound(data, 4,9));
+		System.out.println(lowerBound(data, 5,6));
+	}
+
+	public static int lowerBound(int data[], int low, int value) {
+		int high = data.length;
+		int mid = 0;
+		while (low < high) {
+			mid = (low + high) / 2;
+			if (value <= data[mid]) {
+				high = mid;
 			} else {
-				sb.append(" ");
+				low = mid + 1;
 			}
 		}
-		System.out.println(sb.toString());
+		return low;
+	}
+
+	public static int upperBound(int data[], int value) {
+		int low = 0;
+		int high = data.length;
+		int mid = 0;
+		while (low < high) {
+			mid = (low + high) / 2;
+			if (value >= data[mid]) {
+				low = mid + 1;
+			} else {
+				high = mid;
+			}
+		}
+		return low;
 	}
 }
