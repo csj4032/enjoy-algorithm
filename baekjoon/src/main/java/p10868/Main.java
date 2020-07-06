af572eac99eadd9de702f9e4db8bbd98d03fc05d
genius.choi <csj4032@gmail.com>
2020-07-07 02:11:26 +0900

    백준 문제 10868 완료

--- baekjoon/src/main/java/p10868/Main.java
+++ baekjoon/src/main/java/p10868/Main.java
@@ -1,6 +1,7 @@
 package p10868;
 
-import java.util.Arrays;
+import java.io.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -10,43 +11,189 @@ import java.util.Scanner;
  */
 public class Main {
 
-    static final int max = 1000000007;
-    static int[] tree;
-    static int[] arr;
+	static int[] tree;
+	static int[] arr;
+	static int n;
+	static int m;
+	static int k;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		k = (int)Math.ceil(Math.log(n) / Math.log(2));
+		arr = new int[n];
+		tree = new int[1 << (k + 1)];
+		for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+		init(0, n - 1, 1);
+		for (int i = 0; i < m; i++) {
+			int l = sc.nextInt();
+			int r = sc.nextInt();
+			int min = query(0, n - 1, l - 1, r - 1, 1);
+			System.out.println(min);
+		}
+	}
 
-        arr = new int[n];
-        tree = new int[1 << ((int) Math.ceil(Math.log(n) / Math.log(2)) + 1)];
+	private static int init(int s, int e, int node) {
+		if (s == e) return tree[node] = arr[s];
+		var mid = (s + e) / 2;
+		return tree[node] = Math.min(init(s, mid, node * 2), init(mid + 1, e, node * 2 + 1));
+	}
 
-        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+	private static int query(int s, int e, int l, int r, int node) {
+		if (s > r || e < l) return Integer.MAX_VALUE;
+		if (s >= l && e <= r) return tree[node];
+		var mid = (s + e) / 2;
+		return Math.min(query(s, mid, l, r, node * 2), query(mid + 1, e, l, r, node * 2 + 1));
+	}
+}
 
-        segment(1, 0, n - 1);
+class InputReader {
+	private InputStream stream;
+	private byte[] buf = new byte[1024];
+	private int curChar;
+	private int numChars;
 
-        for (int i = 0; i < m; i++)
-            sb.append(query(1, 0, n - 1, sc.nextInt() - 1, sc.nextInt() - 1)).append("\n");
+	public InputReader(InputStream stream) {
+		this.stream = stream;
+	}
 
-        System.out.println(sb.toString());
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(tree));
-    }
+	public int read() {
+		if (numChars == -1) {
+			throw new InputMismatchException();
+		} else {
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException var2) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0) {
+					return -1;
+				}
+			}
+			return buf[curChar++];
+		}
+	}
 
-    private static int segment(int node, int left, int right) {
-        if (left == right) return tree[node] = arr[left];
-        int mid = (left + right) / 2;
-        return tree[node] = Math.min(segment(node * 2, left, mid), segment(node * 2 + 1, mid + 1, right));
-    }
+	public boolean isSpace(int c) {
+		return c == 10 || c == 13 || c == -1 || c == 9 || c == 32;
+	}
 
-    private static int query(int i, int left, int right, int start, int end) {
-        if (right < start || end < left) return max;
-        if (start <= left && right <= end) {
-            return tree[i];
-        }
-        int mid = (left + right) / 2;
-        return Math.min(query(i * 2, left, mid, start, end), query(i * 2 + 1, mid + 1, right, start, end));
-    }
+	private boolean isEnd(int c) {
+		return c == 10 || c == 13 || c == -1;
+	}
+
+	public String next() {
+		int c = this.read();
+		while (isSpace(c)) {
+			c = this.read();
+		}
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+		} while (!isSpace(c = this.read()));
+		return res.toString();
+	}
+
+	public int nextInt() {
+		int c = this.read();
+		while (isSpace(c)) {
+			c = this.read();
+		}
+		byte sgn = 1;
+		if (c == 45) {
+			sgn = -1;
+			c = this.read();
+		}
+		int res = 0;
+		while (c >= 48 && c <= 57) {
+			res *= 10;
+			res += c - 48;
+			c = this.read();
+			if (isSpace(c)) {
+				return res * sgn;
+			}
+		}
+		throw new InputMismatchException();
+	}
+
+	public long nextLong() {
+		int c = this.read();
+		while (isSpace(c)) {
+			c = this.read();
+		}
+		byte sgn = 1;
+		if (c == 45) {
+			sgn = -1;
+			c = this.read();
+		}
+		long res = 0;
+		while (c >= 48 && c <= 57) {
+			res *= 10;
+			res += c - 48;
+			c = this.read();
+			if (isSpace(c)) {
+				return res * sgn;
+			}
+		}
+		throw new InputMismatchException();
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isEnd(c)) {
+			c = this.read();
+		}
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+		} while (!isEnd(c = this.read()));
+		return res.toString();
+	}
+
+}
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(char c) {
+		writer.print(c);
+	}
+
+	public void print(int i) {
+		writer.print(i);
+	}
+
+	public void print(long l) {
+		writer.print(l);
+	}
+
+	public void print(double d) {
+		writer.print(d);
+	}
+
+	public void print(Object... objects) {
+		int len = objects.length;
+		for (int i = 0; i < len; i++) {
+			writer.print(objects[i]);
+		}
+	}
+
+	public void println() {
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
 }
