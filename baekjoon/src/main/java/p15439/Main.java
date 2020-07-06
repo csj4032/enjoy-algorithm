c8cac2a0d41e5e41c84c040bdd383574ede42ca9
genius.choi <csj4032@gmail.com>
2020-07-07 03:10:35 +0900

    백준 문제 11256 완료

---
+++ baekjoon/src/main/java/p15439/Main.java
@@ -0,0 +1,11 @@
+package p15439;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var n = sc.nextInt();
+		System.out.println(n * (n - 1));
+	}
+}
