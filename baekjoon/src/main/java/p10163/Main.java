cf1ea17c435fedb4f68d5a97f1b4605e58c69c49
genius.choi <csj4032@gmail.com>
2020-07-11 19:17:49 +0900

    백준 문제 10163  완료

---
+++ baekjoon/src/main/java/p10163/Main.java
@@ -0,0 +1,42 @@
+package p10163;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static int[][] board = new int[101][101];
+
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var n = sc.nextInt();
+		var sb = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			int w = sc.nextInt();
+			int h = sc.nextInt();
+			color(x, y, w, h, i);
+		}
+
+		for (int k = 1; k <= n; k++) {
+			var t = 0;
+			for (int i = 0; i <= 100; i++) {
+				for (int j = 0; j <= 100; j++) {
+					if (board[i][j] == k) {
+						t++;
+					}
+				}
+			}
+			sb.append(t + "\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static void color(int x, int y, int w, int h, int i) {
+		for (int j = y; j < y + h; j++) {
+			for (int k = x; k < x + w; k++) {
+				board[j][k] = i;
+			}
+		}
+	}
+}
--- baekjoon/src/main/java/p1753/Main.java
+++ baekjoon/src/main/java/p1753/Main.java
@@ -1,37 +1,84 @@
 package p1753;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	private static int inf = 10000000;
-	private static int[][] ve;
-	private static int[] dist;
+	static Queue<Tuple> queue = new PriorityQueue<>(2000001);
+	static List<List<Tuple>> graph = new ArrayList<>(2000001);
+	static int[] distance;
+	static boolean[][] processed;
+	static int max = 2000001;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int v = sc.nextInt();
-		int e = sc.nextInt();
-		int s = sc.nextInt();
-		ve = new int[v + 1][v + 1];
-		dist = new int[v + 1];
-
-		Arrays.fill(dist, inf);
-
-		for (int i = 0; i < e; i++) {
-			int ss = sc.nextInt();
-			int ee = sc.nextInt();
-			int vv = sc.nextInt();
-			ve[ss][ee] = vv;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] nn = br.readLine().split(" ");
+		int v = Integer.parseInt(nn[0]);
+		int e = Integer.parseInt(nn[1]);
+		int s = Integer.parseInt(br.readLine());
+		distance = new int[v + 1];
+		processed = new boolean[v + 1][11];
+		for (int i = 0; i <= v; i++) {
+			distance[i] = max;
+			graph.add(new ArrayList<>());
 		}
-
+		for (int i = 0; i < e; i++) {
+			String[] nnn = br.readLine().split(" ");
+			var u = Integer.parseInt(nnn[0]);
+			var vv = Integer.parseInt(nnn[1]);
+			var w = Integer.parseInt(nnn[2]);
+			graph.get(u).add(new Tuple(vv, w));
+		}
 		dijkstra(s);
 
-		System.out.println(1313374%1000);
+		for (int i = 1; i <= v; i++) {
+			if (distance[i] == max) {
+				bw.write("INF\n");
+			} else {
+				bw.write(distance[i] + "\n");
+			}
+		}
+		bw.close();
 	}
 
 	private static void dijkstra(int s) {
-		dist[s] = 0;
+		distance[s] = 0;
+		queue.add(new Tuple(s, 0));
+		while (!queue.isEmpty()) {
+			var cur = queue.peek();
+			queue.poll();
+			if(distance[cur.v] < cur.w) continue;
+			for (Tuple next : graph.get(cur.v)) {
+				if ((distance[cur.v] + next.w) < distance[next.v]) {
+					distance[next.v] = distance[cur.v] + next.w;
+					queue.offer(new Tuple(next.v, distance[next.v]));
+				}
+			}
+		}
+	}
+}
+
+class Tuple implements Comparable<Tuple> {
+	int v;
+	int w;
+
+	public Tuple(int v, int w) {
+		this.v = v;
+		this.w = w;
+	}
+
+	@Override
+	public int compareTo(Tuple o) {
+		return (this.w < o.w) ? -1 : ((this.w == o.w) ? 0 : 1);
+	}
+
+	@Override
+	public String toString() {
+		return "Tuple{" +
+				"v=" + v +
+				", w=" + w +
+				'}';
 	}
 }
