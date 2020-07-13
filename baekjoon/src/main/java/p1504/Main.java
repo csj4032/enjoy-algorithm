153f6187d6e91430e1e003e70c72add7df990a65
genius.choi <csj4032@gmail.com>
2020-07-13 15:34:35 +0900

    백문 문제 11800 완료

--- baekjoon/src/main/java/p1504/Main.java
+++ baekjoon/src/main/java/p1504/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	static PriorityQueue<Tuple> queue = new PriorityQueue<>();
 	static List<List<Tuple>> graph = new ArrayList<>();
+	static Map<Integer, Tuple> path = new HashMap();
 	static int[] distance;
 	static int max = 20000000;
 
@@ -13,33 +14,41 @@ public class Main {
 		var sc = new Scanner(System.in);
 		var node = sc.nextInt();
 		var edge = sc.nextInt();
+		var s = 1;
 		distance = new int[node + 1];
 		Arrays.fill(distance, max);
-		for (int i = 0; i <= node; i++) graph.add(new ArrayList());
+		for (int i = 0; i <= node; i++) {
+			graph.add(new ArrayList());
+		}
 		for (int i = 0; i < edge; i++) {
 			var a = sc.nextInt();
 			var b = sc.nextInt();
 			var c = sc.nextInt();
 			graph.get(a).add(new Tuple(b, c));
 		}
-		System.out.println(graph);
-		dijkstra(1);
-		System.out.println(Arrays.toString(distance));
+		//System.out.println(graph);
+		dijkstra(s);
+		//System.out.println(Arrays.toString(distance));
+		//System.out.println(path);
 	}
 
 	private static void dijkstra(int s) {
 		var first = new Tuple(s, 0);
 		distance[first.v] = first.w;
 		queue.offer(first);
+		path.put(1, first);
 		while (!queue.isEmpty()) {
 			var cur = queue.peek();
 			queue.poll();
+			if(distance[cur.v] < cur.w) continue;
 			for (Tuple next : graph.get(cur.v)) {
-				if (distance[next.v] > distance[cur.v] + next.w) {
+				//if (distance[next.v] > distance[cur.v] + next.w) {
 					distance[next.v] = distance[cur.v] + next.w;
 					queue.offer(new Tuple(next.v, distance[next.v]));
-				}
+					path.put(next.v, new Tuple(cur.v, distance[next.v]));
+				//}
 			}
+			System.out.println(queue);
 		}
 	}
 }
@@ -63,3 +72,13 @@ class Tuple implements Comparable<Tuple> {
 		return this.w.compareTo(o.w);
 	}
 }
+//5 8
+//5
+//5 2 4
+//5 4 2
+//4 2 1
+//4 3 1
+//2 1 3
+//1 4 3
+//1 3 6
+//3 4 2
