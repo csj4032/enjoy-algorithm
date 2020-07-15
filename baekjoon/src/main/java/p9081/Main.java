fc948b1d7b9035980fe37d2d4bc67ae9087d8c48
genius.choi <csj4032@gmail.com>
2020-07-16 03:25:52 +0900

    백준 문제 9081 완료

--- baekjoon/src/main/java/p9081/Main.java
+++ baekjoon/src/main/java/p9081/Main.java
@@ -1,49 +1,58 @@
 package p9081;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 제목 : 단어 맞추기
  * 링크 : https://www.acmicpc.net/problem/9081
  * 분류 : Next Permutation
  * 참고 : https://tttuer.tistory.com/212?category=776472
+ *
+ * 1. 앞에서 부터 배열을 보면서 arr[i] < arr[i+1]을 만족하는 가장 마지막 i를 찾는다.
+ * 1.1 맨 뒤에서 부터 배열을 보면서 arr[i-1] >= arr[i] 인 마지막 i를 찾는다.
+ * 2. 만약 그러한 i가 없다면 이미 마지막 순열이다.
+ * 3. 뒤에서 부터 시작 arr[j] > arr[i] 인 첫번째 j를 찾는다.
+ * 4. swap(i,j)한다.
+ * 5. 그 이후에 i+1부터 끝까지 정렬을 해준다.
+ *
+ *
+ * 출처: https://tttuer.tistory.com/212?category=776472 [여우와펭귄의 블로그]
  */
 public class Main {
 
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
-		var t = sc.nextInt();
 		var sb = new StringBuilder();
-		var set = new HashSet<String>();
+		var t = sc.nextInt();
 		for (int i = 0; i < t; i++) {
-			var s1 = sc.next();
-			set.clear();
-			dfs(s1.length(), s1.length(), 0, s1.split(""), set);
-			List<String> ll = new ArrayList<>(set);
-			Collections.sort(ll);
-			var idx = ll.indexOf(s1);
-			if (ll.size() == idx + 1) {
-				sb.append(ll.get(ll.size() - 1));
-			} else {
-				sb.append(ll.get(idx + 1));
-			}
-			sb.append("\n");
+			sb.append(nextPermutation(sc.next()) + "\n");
 		}
 		System.out.println(sb.toString());
 	}
 
-	private static void dfs(int n, int m, int d, String[] str, HashSet<String> set) {
-		if (d == m) {
-			var ss = "";
-			for (int i = 0; i < str.length; i++) ss += str[i];
-			set.add(ss);
-			return;
+	private static String nextPermutation(String word) {
+		var i = word.length() - 1;
+		while (i > 0 && word.charAt(i - 1) >= word.charAt(i)) {
+			i--;
 		}
-		for (int i = 0; i < n; i++) {
-			swap(i, d, str);
-			dfs(n, m, d + 1, str, set);
-			swap(i, d, str);
+		if (i <= 0) return word;
+
+		var j = word.length() - 1;
+		while (word.charAt(j) <= word.charAt(i - 1)) {
+			j--;
 		}
+		var arr = word.split("");
+		swap(i - 1, j, arr);
+
+		j = word.length() - 1;
+		while (i < j) {
+			swap(i, j, arr);
+			i++;
+			j--;
+		}
+
+		return Arrays.stream(arr).collect(Collectors.joining());
 	}
 
 	private static void swap(int i, int m, String[] str) {
