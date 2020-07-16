5f12abcec302d3a42a4a6f656d36ff03314090e1
genius.choi <csj4032@gmail.com>
2020-07-17 02:53:10 +0900

    백준 문제 9082 완료

---
+++ baekjoon/src/main/java/p9082/Main.java
@@ -0,0 +1,23 @@
+package p9082;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 제목 : 지뢰찾기
+ * 링크 : https://www.acmicpc.net/problem/9082
+ * 분류 : 조합, 이진 (O,X)
+ */
+public class Main {
+	static int k = 0;
+
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			var n = sc.nextInt();
+			var m = sc.next().split("");
+			var s = sc.next();
+		}
+	}
+}
