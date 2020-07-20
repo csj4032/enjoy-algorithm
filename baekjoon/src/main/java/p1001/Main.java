40d8ac5840b6aaca7c629bd6b81368103508cd6b
genius.choi <csj4032@gmail.com>
2020-07-21 00:39:49 +0900

    백준 문제 5525 진행

--- baekjoon/src/main/java/p1001/Main.java
+++ baekjoon/src/main/java/p1001/Main.java
@@ -2,6 +2,11 @@ package p1001;
 
 import java.util.Scanner;
 
+/**
+ * 제목 : A-B
+ * 링크 : https://www.acmicpc.net/problem/1001
+ * 분류 : 사칙연산, 수학
+ */
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
