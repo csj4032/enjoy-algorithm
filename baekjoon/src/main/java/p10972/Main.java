d8ae90d4ec03fd06b83ac738eb5c0602c30d306a
genius.choi <csj4032@gmail.com>
2020-08-21 16:49:14 +0900

    백문 문제 10972 완료

--- baekjoon/src/main/java/p10972/Main.java
+++ baekjoon/src/main/java/p10972/Main.java
@@ -1,49 +1,75 @@
 package p10972;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * 제목 : 다음 순열
  * 링크 : https://www.acmicpc.net/problem/10972
- * 분류 : 순
+ * 분류 : 수학
+ * <p>
+ * 1 2 3 4 (A > idx : 3, B > idx : 2)
+ * 1 2 4 3
+ * 1 3 2 4
+ * 1 3 4 2
+ * 1 4 3 2
+ * 1 4 2 3
+ * <p>
+ * 앞 -> 뒤 방향으로 각각 비교 중 뒤 숫자가 큰 경우가 가장 뒤 발생 위치 (작은 숫자의 위치 A, 뒤쪽이 큰 경우가 없다면 종료 지점)
+ * 뒤 -> A 방향으로 A 위치의 숫자 보다 큰 숫자들 중 가장 뒤에 위치 (큰 숫자 위치 B)
+ * A 와 B Swap
+ * A 뒤 부터 B 까지 단계 Swap
  */
 public class Main {
 
-	private static int[] m;
-	private static int[] x;
-	private static int[] a;
-	private static int n;
-	private static boolean[] v;
-	private static boolean c = false;
-	private static boolean e = false;
-	private static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
-		n = sc.nextInt();
-		m = new int[n];
-		x = new int[n];
-		a = new int[n];
-		v = new boolean[n];
-		for (int i = 0; i < n; i++) x[i] = sc.nextInt();
-		recursive(0, 0);
+		var n = sc.nextInt();
+		var m = new int[n];
+		var sb = new StringBuilder();
+		for (int i = 0; i < n; i++) m[i] = sc.nextInt();
+		var k = nextPermute(m);
+		if (k == null) {
+			System.out.println("-1");
+		} else {
+			for (int i = 0; i < n; i++) sb.append(m[i] + " ");
+			System.out.println(sb.toString().trim());
+		}
 	}
 
-	private static void recursive(int d, int k) {
-		System.out.println("recursive : " + d);
-		if (d == n) {
-			System.out.println(Arrays.toString(x));
-			return;
-		}
-		for (int i = 0; i < n; i++) {
-			System.out.println("d : " + d + " i : " + i + " v : " + Arrays.toString(v));
-			if (v[i]) continue;
-			v[i] = true;
-			x[i] = d + 1;
-			recursive(d + 1, k + 1);
-			v[i] = false;
-		}
+	private static int[] nextPermute(int[] m) {
+		var i = m.length - 1;
+		var j = m.length - 1;
+		while (i > 0 && m[i - 1] >= m[i]) --i;
+		if (i <= 0) return null;
+		while (m[i - 1] > m[j]) --j;
+		swap(i - 1, j, m);
+		j = m.length - 1;
+		for (; i < j; ++i, --j) swap(i, j, m);
+		return m;
+	}
+
+	private static void swap(int i, int j, int[] m) {
+		var t = m[i];
+		m[i] = m[j];
+		m[j] = t;
 	}
 }
+// 5
+// 2 5 1 9 7
+// 2 5 7 1 9
+
+// 4
+// 1 2 3 4
+// 1 2 4 3
+
+// 3
+// 2 3 1
+// 3 1 2
+
+// 5
+// 1 2 3 4 5
+// 1 2 3 5 4
+// 1 2 4 3 5
+// 1 2 4 5 3
+// 1 2 5 3 4
+// 1 2 5 4 3 >> 1 3 5 4 2 >> 1 3 2 4 5
