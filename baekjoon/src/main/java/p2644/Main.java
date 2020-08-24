2f6f929feb06e8a737f4842dc53513ab282e89ea
genius.choi <csj4032@gmail.com>
2020-08-24 19:12:42 +0900

    백문 문제 행 진

---
+++ baekjoon/src/main/java/p2644/Main.java
@@ -0,0 +1,82 @@
+package p2644;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	private static List<List<Integer>> graph = new ArrayList();
+	private static boolean[] visitor;
+	private static int x;
+	private static int y;
+	private static int k = -1;
+
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var n = sc.nextInt();
+		x = sc.nextInt();
+		y = sc.nextInt();
+		var m = sc.nextInt();
+		visitor = new boolean[n];
+
+		for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+
+		for (int i = 0; i < m; i++) {
+			var a = sc.nextInt() - 1;
+			var b = sc.nextInt() - 1;
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+		}
+
+		dfs(x - 1, 0);
+		System.out.println(k);
+	}
+
+	private static void dfs(int idx, int node) {
+		for (int i = 0; i < graph.get(idx).size(); i++) {
+			var next = graph.get(idx).get(i);
+			if (!visitor[next]) {
+				if (next == (y - 1)) {
+					k = node;
+				}
+				visitor[next] = true;
+				node = node + 1;
+				dfs(next, node);
+				node = node - 1;
+			}
+		}
+	}
+}
+//9
+//7 3
+//7
+//1 2
+//1 4
+//2 7
+//2 8
+//2 9
+//4 3
+//4 6
+
+//9
+//5 3
+//7
+//1 2
+//1 3
+//2 7
+//2 8
+//2 9
+//4 5
+//4 6
+
+//9
+//7 3
+//7
+//1 2
+//1 4
+//2 7
+//2 8
+//2 9
+//7 3
+//4 6
