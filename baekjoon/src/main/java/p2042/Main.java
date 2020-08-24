b010062d99f5112f180e492de122fb9a7c031c2d
genius.choi <csj4032@gmail.com>
2020-08-25 01:26:24 +0900

    백준 문제 2042 완료

--- baekjoon/src/main/java/p2042/Main.java
+++ baekjoon/src/main/java/p2042/Main.java
@@ -1,43 +1,62 @@
 package p2042;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-	private static int nn[];
-	private static int node[];
-	private int k;
+	private static long nn[];
+	private static long tree[];
 
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
 		var n = sc.nextInt();
 		var m = sc.nextInt();
 		var k = sc.nextInt();
-		nn = new int[n];
-		node = new int[n * n];
+		m += k;
+		nn = new long[n];
+		tree = new long[n * n];
 		for (int i = 0; i < n; i++) nn[i] = sc.nextInt();
 
-		init(nn, node, 1, 0, n - 1);
+		init(nn, tree, 1, 0, n - 1);
 
-		System.out.println(Arrays.toString(node));
-
-		for (int i = 0; i < m + k; i++) {
+		while (m-- > 0) {
 			var a = sc.nextInt();
 			var b = sc.nextInt();
-			var c = sc.nextInt();
 			if (a == 1) {
-
-			} else {
-
+				var c = sc.nextLong();
+				var diff = c - nn[b - 1];
+				nn[b - 1] = c;
+				update(tree, 1, 0, n - 1, b - 1, diff);
+			} else if (a == 2) {
+				var c = sc.nextInt();
+				System.out.println(sum(tree, 1, 0, n - 1, b - 1, c - 1));
 			}
 		}
 	}
 
-	private static int init(int[] nn, int[] node, int idx, int start, int end) {
+	private static long init(long[] nn, long[] tree, int node, int start, int end) {
 		if (start == end) {
-			return node[idx] = nn[start];
+			return tree[node] = nn[start];
 		}
-		return node[idx] = init(nn, node, idx * 2, start, (start + end) / 2) + init(nn, node, idx * 2 + 1, (start + end) / 2 + 1, end);
+		return tree[node] = init(nn, tree, node * 2, start, (start + end) / 2) + init(nn, tree, node * 2 + 1, (start + end) / 2 + 1, end);
+	}
+
+	private static long sum(long[] tree, int node, int start, int end, int left, int right) {
+		if (end < left || start > right) {
+			return 0;
+		}
+		if (left <= start && end <= right) {
+			return tree[node];
+		}
+		return sum(tree, node * 2, start, (start + end) / 2, left, right) + sum(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+	}
+
+	private static void update(long[] tree, int node, int start, int end, int index, long diff) {
+		if (index < start || index > end) return;
+		tree[node] = tree[node] + diff;
+		if (start != end) {
+			update(tree, node * 2, start, (start + end) / 2, index, diff);
+			update(tree, node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
+		}
 	}
 }
