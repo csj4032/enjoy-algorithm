6610f62f70d58551fa0150a14f37edc4e4f1af62
genius.choi <csj4032@gmail.com>
2020-08-25 00:31:06 +0900

    백준 문제 1271 완료

---
+++ baekjoon/src/main/java/p8370/Main.java
@@ -0,0 +1,14 @@
+package p8370;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var n1 = sc.nextInt();
+		var k1 = sc.nextInt();
+		var n2 = sc.nextInt();
+		var k2 = sc.nextInt();
+		System.out.println((n1 * k1) + (n2 * k2));
+	}
+}
