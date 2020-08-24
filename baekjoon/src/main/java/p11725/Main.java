b185654392a74be834314c5f57c00e625278f59e
genius.choi <csj4032@gmail.com>
2020-08-24 17:41:29 +0900

    백문 문제 11725 완료

--- baekjoon/src/main/java/p11725/Main.java
+++ baekjoon/src/main/java/p11725/Main.java
@@ -1,16 +1,46 @@
 package p11725;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	private static List<List<Integer>> graph = new ArrayList();
+	private static int[] parent;
+	private static boolean[] visitor;
 
-		for (int i = 0; i < n; i++) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		var n = Integer.parseInt(br.readLine());
+		parent = new int[n];
+		visitor = new boolean[n];
+		visitor[0] = true;
 
+		for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+
+		for (int i = 1; i <= n - 1; i++) {
+			var c = br.readLine().split(" ");
+			var a = Integer.parseInt(c[0]) - 1;
+			var b = Integer.parseInt(c[1]) - 1;
+			graph.get(a).add(b);
+			graph.get(b).add(a);
 		}
 
+		dfs(0);
+		for (int i = 1; i < n; i++) System.out.println(parent[i]);
+	}
+
+	private static void dfs(int idx) {
+		for (int i = 0; i < graph.get(idx).size(); i++) {
+			var next = graph.get(idx).get(i);
+			if (!visitor[next]) {
+				parent[next] = idx + 1;
+				visitor[next] = true;
+				dfs(next);
+			}
+		}
 	}
 }
