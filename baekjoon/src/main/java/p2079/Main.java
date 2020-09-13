12422a04423d7b420f690aae06010c1395105408
genius.choi <csj4032@gmail.com>
2020-09-13 19:14:41 +0900

    백문 문제 18415 완료

--- baekjoon/src/main/java/p2079/Main.java
+++ baekjoon/src/main/java/p2079/Main.java
@@ -1,37 +1,32 @@
 package p2079;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	static String[] m;
 	static int[] dp;
-	static int t;
+	static int n;
 
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
 		m = sc.next().split("");
-		t = m.length;
-		dp = new int[20];
-		dp[0] = 1;
+		n = m.length;
+		dp = new int[n + 1];
 
-		for (int i = 2; i < t; i++) {
-			if (palindrome(i - 2, i)) {
-				dp[i] = dp[i - 2];
-			} else {
-				if (palindrome(i - 1, i)) {
-					dp[i] = dp[i - 1];
-				} else {
-					dp[i] = dp[i - 1] + 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (palindrome(j - 1, i - 1)) {
+					if (dp[i] == 0 || dp[i] > dp[j - 1] + 1) {
+						dp[i] = dp[j - 1] + 1;
+					}
 				}
 			}
 		}
-		System.out.println(Arrays.toString(dp));
+		System.out.println(dp[n]);
 	}
 
 	private static boolean palindrome(int s, int e) {
-		if (t <= e) return false;
 		while (s < e) {
 			if (!m[s++].equals(m[e--])) {
 				return false;
