c6c6d3e244bc13e705cc67cbb38451f5bca1b9ed
genius.choi <csj4032@gmail.com>
2020-09-16 23:08:03 +0900

    gitigonre 수정

--- algorithm-puzzles/q43_2.py
+++ algorithm-puzzles/q43_2.py
@@ -2,12 +2,12 @@ N = 12345
 
 
 def trit(n):
-    if (n == 0):
-        return 1;
+    if n == 0:
+        return 1
     a = 0
     while 3 ** (a + 1) <= n:
         a += 1
     return 2 ** 2 + trit(min([n - 3 ** a, 3 ** a - 1]))
 
 
-print(trit(1));
+print(trit(1))
