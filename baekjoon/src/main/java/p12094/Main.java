e0e9994ddf238854da3d1cb49162b085b5be5210
genius.choi <csj4032@gmail.com>
2020-09-25 21:32:12 +0900

    백문 문제 12094 완료

--- baekjoon/src/main/java/p12094/Main.java
+++ baekjoon/src/main/java/p12094/Main.java
@@ -1,193 +1,170 @@
 package p12094;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
 	static int n;
-	static int m = 0;
 	static int max;
-	static int limit;
-	static int sum;
 	static int[][] map;
-	static int[] dy = new int[]{-1, 0, 1, 0};
-	static int[] dx = new int[]{0, 1, 0, -1};
-	static boolean[][] lock;
-	static boolean[][] visitor;
-	static Map<Integer, Integer> counter = new HashMap();
+	static int[] depth_max;
 
-	public static void main(String[] args) {
-		var sc = new Scanner(System.in);
-		n = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		var br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
 		map = new int[n][n];
-		visitor = new boolean[10][4];
-		lock = new boolean[n][n];
+		depth_max = new int[11];
 		for (int i = 0; i < n; i++) {
+			var st = br.readLine().split(" ");
 			for (int j = 0; j < n; j++) {
-				var cell = sc.nextInt();
-				map[i][j] = cell;
-				if (counter.containsKey(cell)) {
-					counter.put(cell, counter.get(cell) + 1);
-				} else {
-					counter.put(cell, 1);
-				}
+				map[i][j] = Integer.parseInt(st[j]);
 			}
 		}
-		dfs(0, map);
+		dfs(0);
 		System.out.println(max);
 	}
 
-	private static void dfs(int depth, int[][] map) {
-		if (depth > 9) return;
+	private static void dfs(int depth) {
+		var d_max = max();
+		if (depth_max[depth] >= d_max) {
+			return;
+		}
+		if (depth == 10) {
+			max = Math.max(d_max, max);
+			var temp = max;
+			while (depth > 0) {
+				depth_max[depth--] = temp;
+				temp /= 2;
+			}
+			return;
+		}
+		var tempMap = copyMap(map);
 		for (int i = 0; i < 4; i++) {
-			if (!visitor[depth][i]) {
-				visitor[depth][i] = true;
-				var copyMap = copyMap(map);
-				move(i, copyMap);
-				max = Math.max(max, max(copyMap));
-				dfs(depth + 1, copyMap);
-				visitor[depth][i] = false;
-			}
+			move(i);
+			max = Math.max(max, max());
+			if (isSameMap(map, tempMap)) continue;
+			dfs(depth + 1);
+			map = copyMap(tempMap);
 		}
 	}
 
-	private static boolean isAllStay(int[][] map, int direct) {
-		var stay = true;
-		if (direct == 0) {
-			outer1:
-			for (int i = 1; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if ((map[i][j] == map[i - 1][j] && map[i][j] != 0) || (map[i][j] != map[i - 1][j] && map[i - 1][j] == 0)) {
-						stay = false;
-						break outer1;
-					}
-				}
-			}
-		} else if (direct == 1) {
-			outer2:
-			for (int i = n - 1; i >= 1; i--) {
-				for (int j = 0; j < n; j++) {
-					if ((map[j][i] == map[j][i - 1] && map[i][j] != 0) || (map[j][i] != map[j][i - 1] && map[j][i - 1] == 0)) {
-						stay = false;
-						break outer2;
-					}
-				}
-			}
-		} else if (direct == 2) {
-			outer3:
-			for (int i = n - 1; i >= 1; i--) {
-				for (int j = 0; j < n; j++) {
-					if ((map[i][j] == map[i - 1][j] && map[i][j] != 0) || (map[i][j] != map[i - 1][j] && map[i - 1][j] == 0)) {
-						stay = false;
-						break outer3;
-					}
-				}
-			}
-		} else if (direct == 3) {
-			outer4:
-			for (int i = 0; i < n - 1; i++) {
-				for (int j = 0; j < n; j++) {
-					if ((map[j][i] == map[j][i + 1] && map[i][j] != 0) || (map[j][i] != map[j][i + 1] && map[i][j] == 0)) {
-						stay = false;
-						break outer4;
-					}
-				}
+	private static boolean isSameMap(int[][] map, int[][] copyMap) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] != copyMap[i][j]) return false;
 			}
 		}
-		return stay;
+		return true;
 	}
 
-	private static void move(int direct, int[][] map) {
-		var c = 0;
-		lock = new boolean[n][n];
-		while (true) {
-			if (c == n) break;
-			if (direct == 0) {
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < n; j++) {
-						top(map, i, j);
-					}
-				}
-			} else if (direct == 1) {
-				for (int i = 0; i < n; i++) {
-					for (int j = n - 1; j >= 0; j--) {
-						right(map, i, j);
-					}
-				}
-			} else if (direct == 2) {
-				for (int i = n - 1; i >= 0; i--) {
-					for (int j = 0; j < n; j++) {
-						bottom(map, i, j);
-					}
-				}
-			} else if (direct == 3) {
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < n; j++) {
-						left(map, i, j);
-					}
-				}
-			}
-			c++;
-		}
-	}
-
-	private static void left(int[][] map, int i, int j) {
-		var ddx = j + dx[3];
-		if (ddx >= m && map[i][j] != 0) copyCol(map, i, j, ddx);
-	}
-
-	private static void bottom(int[][] map, int i, int j) {
-		var ddy = i + dy[2];
-		if (ddy < n && map[i][j] != 0) copyRow(map, i, j, ddy);
-	}
-
-	private static void right(int[][] map, int i, int j) {
-		var ddx = j + dx[1];
-		if (ddx < n && map[i][j] != 0) copyCol(map, i, j, ddx);
-	}
-
-	private static void top(int[][] map, int i, int j) {
-		var ddy = i + dy[0];
-		if (ddy >= m && map[i][j] != 0) copyRow(map, i, j, ddy);
-	}
-
-	private static void copyCol(int[][] map, int i, int j, int ddx) {
-		copyCell(map, i, j, ddx, map[i], lock[i]);
-	}
-
-	private static void copyRow(int[][] map, int i, int j, int ddy) {
-		copyCell(map, ddy, j, j, map[i], lock[i]);
-	}
-
-	private static void copyCell(int[][] map, int i, int j, int ddx, int[] ints, boolean[] booleans) {
-		if (map[i][ddx] == ints[j] && !lock[i][ddx] && !booleans[j]) {
-			lock[i][ddx] = true;
-			map[i][ddx] <<= 1;
-			ints[j] = 0;
-		} else {
-			if (map[i][ddx] == 0) {
-				map[i][ddx] = ints[j];
-				ints[j] = 0;
-			}
-		}
-	}
-
-	private static int max(int[][] map) {
+	private static int max() {
 		var max = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				max = max < map[i][j] ? map[i][j] : max;
+				max = max > map[i][j] ? max : map[i][j];
 			}
 		}
 		return max;
 	}
 
+	private static void move(int direct) {
+		var queue = new LinkedList<Integer>();
+		if (direct == 0) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (map[j][i] > 0) {
+						queue.addLast(map[j][i]);
+						map[j][i] = 0;
+					}
+				}
+				var idx = 0;
+				while (!queue.isEmpty()) {
+					var num = queue.peek();
+					queue.pop();
+					if (map[idx][i] == 0) {
+						map[idx][i] = num;
+					} else if (map[idx][i] == num) {
+						map[idx++][i] += num;
+					} else {
+						map[++idx][i] = num;
+					}
+				}
+			}
+		} else if (direct == 1) {
+			for (int i = 0; i < n; i++) {
+				for (int j = n - 1; j >= 0; j--) {
+					if (map[i][j] > 0) {
+						queue.addLast(map[i][j]);
+						map[i][j] = 0;
+					}
+				}
+				var idx = n - 1;
+				while (!queue.isEmpty()) {
+					var num = queue.peek();
+					queue.pop();
+					if (map[i][idx] == 0) {
+						map[i][idx] = num;
+					} else if (map[i][idx] == num) {
+						map[i][idx--] += num;
+					} else {
+						map[i][--idx] = num;
+					}
+				}
+			}
+		} else if (direct == 2) {
+			for (int i = 0; i < n; i++) {
+				for (int j = n - 1; j >= 0; j--) {
+					if (map[j][i] > 0) {
+						queue.addLast(map[j][i]);
+						map[j][i] = 0;
+					}
+				}
+				var idx = n - 1;
+				while (!queue.isEmpty()) {
+					var num = queue.peek();
+					queue.pop();
+					if (map[idx][i] == 0) {
+						map[idx][i] = num;
+					} else if (map[idx][i] == num) {
+						map[idx--][i] += num;
+					} else {
+						map[--idx][i] = num;
+					}
+				}
+			}
+		} else if (direct == 3) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (map[i][j] > 0) {
+						queue.addLast(map[i][j]);
+						map[i][j] = 0;
+					}
+				}
+				var idx = 0;
+				while (!queue.isEmpty()) {
+					var num = queue.peek();
+					queue.pop();
+					if (map[i][idx] == 0) {
+						map[i][idx] = num;
+					} else if (map[i][idx] == num) {
+						map[i][idx++] += num;
+					} else {
+						map[i][++idx] = num;
+					}
+				}
+			}
+		}
+	}
+
 	private static int[][] copyMap(int[][] src) {
-		var target = new int[n][n];
-		for (int i = 0; i < n; i++) System.arraycopy(src[i], 0, target[i], 0, src[i].length);
-		return target;
+		var dest = new int[n][n];
+		for (int i = 0; i < src.length; i++) {
+			System.arraycopy(src[i], 0, dest[i], 0, src[0].length);
+		}
+		return dest;
 	}
 
 	private static void display(int[][] m) {
@@ -200,28 +177,33 @@ public class Main {
 	}
 }
 /*
-20
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 8
 
+3
+2 2 2
+2 2 2
+2 2 2
+
+20
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
+1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024 1024
 
 16
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
@@ -240,6 +222,23 @@ public class Main {
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+
+8
+2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2
+
+4
+1024 0 0 1024
+1024 0 0 1024
+1024 0 0 1024
+1024 0 0 1024
+
  * <p>
  * <p>
  * 2 4
