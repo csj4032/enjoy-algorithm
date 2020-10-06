b78f9f58fe4cee3e5766423c131e5ee1f06aba50
genius.choi <csj4032@gmail.com>
2020-10-06 10:10:38 +0900

    백문 문제 11758 완료

--- baekjoon/src/main/java/p14890/Main.java
+++ baekjoon/src/main/java/p14890/Main.java
@@ -1,16 +1,19 @@
 package p14890;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * 제목 : 경사로
+ * 링크 : https://www.acmicpc.net/problem/14890
+ * 분류 : 구현
+ * 기출 : 삼성 SW 역량 테스트 기출 문제
+ */
 public class Main {
 
 	static int n;
 	static int l;
 	static int k;
 	static Info[][] map;
-	static Info[] v;
-	static Info[] h;
 
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
@@ -23,43 +26,52 @@ public class Main {
 			}
 		}
 
-		outer:
 		for (int i = 0; i < n; i++) {
-			h = copyRow(map, i);
-			var ww = true;
-			var wc = true;
-			var prev = h[0];
-			for (int j = 1; j < n; j++) {
-				var current = h[j];
-				if (prev.value != current.value && !check(h, l, j, current, prev)) {
-					ww = false;
-					break;
-				}
-				prev = current;
-			}
-
-			for (int j = n - 1; j >= 0; j--) {
-
-			}
-			if (ww && wc) k++;
+			if (isSlope(true, copyRow(map, i))) k++;
 		}
 
-		for (
-				int i = 0;
-				i < n; i++) {
-			v = copyCol(map, i);
+		for (int i = 0; i < n; i++) {
+			if (isSlope(true, copyCol(map, i))) k++;
 		}
 
 		System.out.println(k);
 	}
 
+	private static boolean isSlope(boolean ww, Info[] h) {
+		var prev = h[0];
+		for (int j = 1; j < n; j++) {
+			var current = h[j];
+			if (prev.value != current.value && !check(h, l, j, current, prev)) {
+				ww = false;
+				break;
+			}
+			prev = current;
+		}
+		return ww;
+	}
+
 	private static boolean check(Info[] row, int size, int distance, Info current, Info prev) {
 		var count = size * Math.abs(current.value - prev.value);
-		if (count > distance) return false;
-		while (count-- > 0) {
-			var tt = row[--distance].value;
-			if (tt != prev.value) {
-				return false;
+		if (current.value < prev.value) {
+			if (count + distance > row.length) return false;
+			while (count-- > 0) {
+				var tt = row[distance];
+				if (tt.value != current.value || tt.slope >= 1) {
+					return false;
+				} else {
+					row[distance].slope = count + 1;
+				}
+				distance++;
+			}
+		} else {
+			if (count > distance) return false;
+			while (count-- > 0) {
+				var tt = row[--distance];
+				if (tt.value != prev.value || tt.slope >= 1) {
+					return false;
+				} else {
+					row[distance].slope = count + 1;
+				}
 			}
 		}
 		return true;
@@ -68,7 +80,7 @@ public class Main {
 	private static Info[] copyRow(Info[][] map, int idx) {
 		var newRow = new Info[n];
 		for (int i = 0; i < n; i++) {
-			newRow[i] = map[idx][i];
+			newRow[i] = new Info(map[idx][i].value, map[idx][i].slope);
 		}
 		return newRow;
 	}
@@ -76,7 +88,7 @@ public class Main {
 	private static Info[] copyCol(Info[][] map, int idx) {
 		var newCol = new Info[n];
 		for (int i = 0; i < n; i++) {
-			newCol[i] = map[i][idx];
+			newCol[i] = new Info(map[i][idx].value, map[i][idx].slope);
 		}
 		return newCol;
 	}
@@ -99,3 +111,13 @@ class Info {
 				'}';
 	}
 }
+
+/*
+6 2
+3 3 3 3 3 3
+2 3 3 3 3 3
+2 3 3 3 3 3
+1 3 3 3 3 3
+1 3 3 3 3 3
+1 3 3 3 3 3
+ */
