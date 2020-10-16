06c6c50014b299c9d3d8544717af12e4308fc0db
genius.choi <csj4032@gmail.com>
2020-10-16 19:06:14 +0900

    백준 문제 10993 완료

--- baekjoon/src/main/java/p10993/Main.java
+++ baekjoon/src/main/java/p10993/Main.java
@@ -4,86 +4,96 @@ import java.util.Scanner;
 
 public class Main {
 
-	static char m[][];
+	static String m[][];
+	static StringBuilder sb;
+	static int n;
+	static int h = 1;
+	static int w = 1;
+	static int hh = 1;
+	static int ww = 2;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int w = 1;
-		int f = 4;
-		int h = 1;
-		int t = 2;
-		for (int i = 0; i < n - 1; i++) {
-			w += f;
-			f *= 2;
-			h += t;
-			t *= 2;
+		n = sc.nextInt();
+		sb = new StringBuilder();
+		for (int i = 1; i < n; i++) {
+			hh = hh * 2;
+			h = hh + h;
+			ww = ww * 2;
+			w = ww + w;
 		}
-		m = new char[w][f];
+		m = new String[h][w];
+		init(m);
+		recursive(0, 0, h, w, n);
+		display(m);
+	}
 
+	private static void init(String[][] m) {
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
-				m[i][j] = ' ';
+				m[i][j] = " ";
 			}
 		}
+	}
 
-		display(0, 0, h, w, n);
-
-		StringBuilder sb = new StringBuilder();
+	private static void display(String[][] m) {
+		var ww = w;
+		var wm = w / 2;
 		for (int i = 0; i < h; i++) {
+			ww = ww - 1;
+			wm = wm + 1;
 			for (int j = 0; j < w; j++) {
-				sb.append(m[i][j]);
+				if ((ww < j && (n % 2) == 0) || (wm <= j && (n % 2) == 1)) {
+					sb.append("");
+				} else {
+					sb.append(m[i][j]);
+				}
 			}
 			sb.append("\n");
 		}
-
 		System.out.println(sb.toString());
 	}
 
-	private static void display(int s, int e, int h, int w, int k) {
-		if (k <= 0) {
-			return;
-		}
-		int mid = (s + s + w) / 2;
-		int l = s;
-		int r = s + w;
+	private static void recursive(int hs, int ws, int h, int w, int k) {
+		if (k == 0) return;
+
+		var wm = (ws + ws + w) / 2;
 
 		if (k % 2 == 1) {
-			int ll = mid;
-			int rr = mid;
-			for (int i = e; i < e + h; i++) {
-				for (int j = ll; j <= rr; j++) {
-					if (i == e || i == e + h - 1 || j == ll || j == rr) {
-						m[i][j] = '*';
+			var wws = wm;
+			var wwe = wm;
+			for (int i = hs; i < hs + h; i++) {
+				for (int j = wws; j <= wwe; j++) {
+					if (j == wws || j == wwe || i == hs + h - 1 || i == hs) {
+						m[i][j] = "*";
 					} else {
-						m[i][j] = ' ';
+						m[i][j] = " ";
 					}
 				}
-				ll--;
-				rr++;
+				wws--;
+				wwe++;
 			}
 		} else {
-			int ll = l;
-			int rr = r;
-			for (int i = e; i < e + h; i++) {
-				for (int j = ll; j < rr; j++) {
-					if (i == e || i == e + h - 1 || j == ll || j == rr - 1) {
-						m[i][j] = '*';
+			var wws = ws;
+			var wwe = ws + w;
+			for (int i = hs; i < hs + h; i++) {
+				for (int j = wws; j < wwe; j++) {
+					if (i == hs + h - 1 || i == hs || j == wws || j == wwe - 1) {
+						m[i][j] = "*";
 					} else {
-						m[i][j] = ' ';
+						m[i][j] = " ";
 					}
 				}
-				ll++;
-				rr--;
+				wws++;
+				wwe--;
 			}
 		}
 
-		int mr = s + (int) Math.pow(2, k - 1);
-		//System.out.println(mr + " " + k);
+		ws = ws + (int) Math.pow(2, k - 1);
 		if (k % 2 == 1) {
-			display(mr, e + h / 2, h / 2, w - (int) Math.pow(2, k), k - 1);
+			recursive(hs + h / 2, ws, h / 2, w - (int) Math.pow(2, k), k - 1);
 		} else {
-			display(mr, e + 1, h / 2, w - (int) Math.pow(2, k), k - 1);
+			recursive(hs + 1, ws, h / 2, w - (int) Math.pow(2, k), k - 1);
 		}
 	}
 }
