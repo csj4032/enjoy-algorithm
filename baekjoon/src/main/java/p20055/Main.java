7cce2da62bd31aed4a4661611efbb5dfdf548297
genius.choi <csj4032@gmail.com>
2020-11-16 18:50:17 +0900

    백문 문제 20055 진행

--- baekjoon/src/main/java/p20055/Main.java
+++ baekjoon/src/main/java/p20055/Main.java
@@ -1,12 +1,22 @@
 package p20055;
 
+import java.util.Scanner;
+
 /**
  * 제목 : 컨베이어 벨트 위의 로봇
  * 링크 : https://www.acmicpc.net/problem/20055
  */
 public class Main {
 
-	public static void main(String[] args) {
+	private static int[] l;
 
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		var n = sc.nextInt();
+		var k = sc.nextInt();
+		l = new int[n * 2];
+		for (int i = 0; i < n * 2; i++) {
+			l[i] = sc.nextInt();
+		}
 	}
 }
