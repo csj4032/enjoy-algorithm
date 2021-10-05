514714f6394f3c852f999a2fcf1d338a40635b68
choisj2 <choisj2@brandi.co.kr>
2021-10-05 17:03:04 +0900

    백준 문제 20056 진행

---
+++ baekjoon/src/main/java/p20056/Main.java
@@ -0,0 +1,12 @@
+package p20056;
+
+/**
+ * 제목 : 마법사 상어와 파이어볼
+ * 링크 : https://www.acmicpc.net/problem/20056
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("!");
+	}
+}
