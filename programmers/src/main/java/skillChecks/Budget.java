02218a257f905edece7273b20c27526353346281
genius.choi <csj4032@gmail.com>
2021-12-16 04:14:40 +0900

    프로그래머스 레벨 2 진행 중

--- programmers/src/main/java/skillChecks/Budget.java
+++ programmers/src/main/java/skillChecks/Budget.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 
 public class Budget {
 
-    public int solution(int[] d, int budget) {
-        Arrays.sort(d);
-        int sum = 0;
-        for (int i = 1; i < d.length + 1; i++) {
-            sum += d[i - 1];
-            if (sum > budget) {
-                return i - 1;
-            } else if (sum == budget) {
-                return i;
-            }
-        }
-        if (sum < budget) {
-            return d.length;
-        } else {
-            return 0;
-        }
-    }
+	public int solution(int[] d, int budget) {
+		Arrays.sort(d);
+		int sum = 0;
+		for (int i = 1; i < d.length + 1; i++) {
+			sum += d[i - 1];
+			if (sum > budget) {
+				return i - 1;
+			} else if (sum == budget) {
+				return i;
+			}
+		}
+		if (sum < budget) {
+			return d.length;
+		} else {
+			return 0;
+		}
+	}
 }
