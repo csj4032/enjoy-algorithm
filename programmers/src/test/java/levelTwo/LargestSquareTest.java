67e0c679473a3c91912940e0797a010ee002fa91
genius.choi <csj4032@gmail.com>
2022-01-07 08:41:17 +0900

    프로그래머스 레벨 2 진행 중

---
+++ programmers/src/test/java/levelTwo/LargestSquareTest.java
@@ -0,0 +1,19 @@
+package levelTwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LargestSquareTest {
+
+	public int solution(int[][] board) {
+		LargestSquare largestSquare = new LargestSquare();
+		return largestSquare.area(board);
+	}
+
+	@Test
+	public void test() {
+		Assertions.assertEquals(9, solution(new int[][]{{0, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {0, 0, 1, 0}}));
+		Assertions.assertEquals(4, solution(new int[][]{{0, 0, 1, 1}, {1, 1, 1, 1}}));
+		Assertions.assertEquals(1, solution(new int[][]{{1, 0}, {0, 0}}));
+	}
+}
