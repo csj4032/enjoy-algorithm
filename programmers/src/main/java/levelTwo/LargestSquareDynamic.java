27b9037e6bb452ece201b5228ef6f62ddc476161
genius.choi <csj4032@gmail.com>
2022-01-08 00:49:57 +0900

    프로그래머스 레벨 2 진행 중

--- programmers/src/main/java/levelTwo/LargestSquareDynamic.java
+++ programmers/src/main/java/levelTwo/LargestSquareDynamic.java
@@ -3,6 +3,18 @@ package levelTwo;
 public class LargestSquareDynamic {
 
 	public int area(int[][] board) {
-		return 0;
+		int rows = board.length;
+		int cols = board[0].length;
+		int[][] dp = new int[rows + 1][cols + 1];
+		int max = 0;
+		for (int i = 1; i < rows + 1; i++) {
+			for (int j = 1; j < cols + 1; j++) {
+				if (board[i - 1][j - 1] == 1) {
+					dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+					max = Math.max(dp[i][j], max);
+				}
+			}
+		}
+		return max * max;
 	}
 }
---
+++ programmers/src/test/java/levelTwo/LargestSquareDynamicTest.java
@@ -0,0 +1,20 @@
+package levelTwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LargestSquareDynamicTest {
+
+	public int solution(int[][] board) {
+		LargestSquareDynamic largestSquare = new LargestSquareDynamic();
+		return largestSquare.area(board);
+	}
+
+	@Test
+	public void test() {
+		Assertions.assertEquals(9, solution(new int[][]{{0, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {0, 0, 1, 0}}));
+//		Assertions.assertEquals(4, solution(new int[][]{{0, 0, 1, 1}, {1, 1, 1, 1}}));
+//		Assertions.assertEquals(1, solution(new int[][]{{1, 0}, {0, 0}}));
+	}
+
+}
