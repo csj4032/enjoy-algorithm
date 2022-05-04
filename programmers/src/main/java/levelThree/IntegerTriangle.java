0d408addb0a2e4bbb59c271d12bd9de97f5aed54
choisj2 <choisj2@brandi.co.kr>
2022-05-04 18:34:37 +0900

    프로그래머스 IntegerTriangle

--- programmers/src/main/java/levelThree/IntegerTriangle.java
+++ programmers/src/main/java/levelThree/IntegerTriangle.java
@@ -5,19 +5,37 @@ import java.util.Arrays;
 public class IntegerTriangle {
 
 	public int solution(int[][] triangle) {
-		System.out.println(Arrays.deepToString(triangle));
-		System.out.println(triangle.length);
-		int answer = 0;
-		return answer;
+		int max = 0;
+		int length = triangle.length + 1;
+		int[][] square = new int[length][length + 1];
+		for (int i = 0; i < length; i++) {
+			Arrays.fill(square[i], 0);
+			if (i <= 0) continue;
+			int row = triangle[i - 1].length;
+			int[] rows = triangle[i - 1];
+			for (int j = 0; j < row; j++) {
+				square[i][j + 1] = rows[j];
+			}
+		}
+		for (int i = 1; i < length; i++) {
+			for (int j = 1; j < length; j++) {
+				int result1 = square[i][j] + square[i - 1][j];
+				int result2 = square[i][j] + square[i - 1][j - 1];
+				square[i][j] = Math.max(result1, result2);
+				max = Math.max(max, square[i][j]);
+			}
+		}
+		return max;
 	}
 
 	public static void main(String[] args) {
-		int[][] triangle = new int[5][];
-		triangle[0] = new int[]{7};
-		triangle[1] = new int[]{3, 8};
-		triangle[2] = new int[]{8, 1, 0};
-		triangle[3] = new int[]{8, 1, 0};
-		triangle[3] = new int[]{8, 1, 0};
+		int[][] triangle = new int[][]{
+				new int[]{7},
+				new int[]{3, 8},
+				new int[]{8, 1, 0},
+				new int[]{2, 7, 4, 4},
+				new int[]{4, 5, 2, 6, 5}
+		};
 		IntegerTriangle integerTriangle = new IntegerTriangle();
 		System.out.println(integerTriangle.solution(triangle));
 	}
