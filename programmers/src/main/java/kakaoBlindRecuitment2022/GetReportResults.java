dbef729651a84d6657fb60c2c6049e9dfc47e0ff
genius.choi <csj4032@gmail.com>
2022-05-05 00:19:27 +0900

    프로그래머스 GetReportResults

--- programmers/src/main/java/kakaoBlindRecuitment2022/GetReportResults.java
+++ programmers/src/main/java/kakaoBlindRecuitment2022/GetReportResults.java
@@ -1,8 +1,38 @@
 package kakaoBlindRecuitment2022;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class GetReportResults {
 
-	public int[] report(String[] id_list, String[] report, int k) {
-		return null;
+	public int[] solution(String[] id_list, String[] report, int k) {
+		Map<String, Set<String>> reports = new LinkedHashMap<>();
+		Map<String, Set<String>> reportedCounts = new HashMap<>();
+		Integer idListLength = id_list.length;
+		Integer reportLength = report.length;
+		for (int i = 0; i < idListLength; i++) {
+			reports.put(id_list[i], new HashSet<>());
+			reportedCounts.put(id_list[i], new HashSet<>());
+		}
+		for (int i = 0; i < reportLength; i++) {
+			String id = report[i].split(" ")[0];
+			String reported = report[i].split(" ")[1];
+			Set<String> reportedIds = reports.get(id);
+			reportedIds.add(reported);
+			Set<String> uniqueReportedCounts = reportedCounts.getOrDefault(reported, new HashSet<>());
+			uniqueReportedCounts.add(id);
+		}
+		for (int i = 0; i < idListLength; i++) {
+			String id = id_list[i];
+			Set<String> reportIds = reports.get(id);
+			Set<String> filterIds = reportIds.stream().filter(e -> reportedCounts.get(e).size() >= k).collect(Collectors.toSet());
+			reports.put(id, filterIds);
+		}
+		int[] answer = new int[idListLength];
+		for (int i = 0; i < idListLength; i++) {
+			String id = id_list[i];
+			answer[i] = reports.get(id).size();
+		}
+		return answer;
 	}
 }
--- programmers/src/test/java/kakaoBlindRecuitment2022/GetReportResultsTest.java
+++ programmers/src/test/java/kakaoBlindRecuitment2022/GetReportResultsTest.java
@@ -7,11 +7,15 @@ class GetReportResultsTest {
 
 	public int[] solution(String[] id_list, String[] report, int k) {
 		GetReportResults getReportResults = new GetReportResults();
-		return getReportResults.report(id_list, report, k);
+		return getReportResults.solution(id_list, report, k);
 	}
 
 	@Test
 	public void test() {
-		Assertions.assertEquals(7, solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, 2));
+		Assertions.assertArrayEquals(new int[]{2,1,1,0},
+				solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, 2));
+
+		Assertions.assertArrayEquals(new int[]{0,0},
+				solution(new String[]{"con", "ryan"}, new String[]{"ryan con", "ryan con", "ryan con", "ryan con"}, 3));
 	}
 }
