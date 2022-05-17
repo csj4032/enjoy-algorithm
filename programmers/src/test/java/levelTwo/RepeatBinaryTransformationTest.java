3839b7a8986bcf05a360de8fd003dc9e757acbbf
genius.choi <csj4032@gmail.com>
2022-05-17 13:51:48 +0900

    프로그래머스 이진 변환 반복하기 완료

--- programmers/src/test/java/levelTwo/RepeatBinaryTransformationTest.java
+++ programmers/src/test/java/levelTwo/RepeatBinaryTransformationTest.java
@@ -1,7 +1,15 @@
 package levelTwo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class RepeatBinaryTransformationTest {
 
+	@Test
+	public void test() {
+		RepeatBinaryTransformation repeatBinaryTransformation = new RepeatBinaryTransformation();
+		Assertions.assertArrayEquals(new int[]{3, 8}, repeatBinaryTransformation.solution("110010101001"));
+		Assertions.assertArrayEquals(new int[]{3, 3}, repeatBinaryTransformation.solution("01110"));
+		Assertions.assertArrayEquals(new int[]{4, 1}, repeatBinaryTransformation.solution("1111111"));
+	}
 }
