71b70668fbbbff59d4d749e0db8861d2ff71b92b
choisj2 <choisj2@brandi.co.kr>
2022-05-17 13:51:48 +0900

    프로그래머스 이진 변환 반복하기 완료

--- programmers/src/main/java/levelTwo/RepeatBinaryTransformation.java
+++ programmers/src/main/java/levelTwo/RepeatBinaryTransformation.java
@@ -1,9 +1,25 @@
 package levelTwo;
 
+import java.util.Arrays;
+
 public class RepeatBinaryTransformation {
 
 	public int[] solution(String s) {
-		int[] answer = {};
+		int[] answer = new int[]{0, 0};
+		while (true) {
+			answer[0]++;
+			String ss[] = s.split("");
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < ss.length; i++) {
+				if (ss[i].equals("0")) {
+					answer[1]++;
+					continue;
+				}
+				sb.append(ss[i]);
+			}
+			if (sb.toString().equals("1")) break;
+			s = Integer.toBinaryString(sb.length());
+		}
 		return answer;
 	}
 }
