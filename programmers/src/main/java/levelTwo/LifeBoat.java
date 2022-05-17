a7cbab57a5e3a411709e3bfada62368d5a07b614
choisj2 <choisj2@brandi.co.kr>
2022-05-17 13:38:52 +0900

    프로그래머스 이진 변환 반복하기 진행 중

--- programmers/src/main/java/levelTwo/LifeBoat.java
+++ programmers/src/main/java/levelTwo/LifeBoat.java
@@ -7,12 +7,24 @@ public class LifeBoat {
 	public int solution(int[] people, int limit) {
 		int answer = 0;
 		Arrays.sort(people);
-		int boat = people[0];
-		for (int i = people.length - 1; i > 0; i++) {
-			if (people[i] == 0) continue;
-			if (people[i] + boat == limit) people[i] = 0;
-			answer++;
-
+		int i = 0;
+		int j = people.length - 1;
+		while (i <= j) {
+			int from = people[i];
+			int to = people[j];
+			if (from + to <= limit) {
+				answer++;
+				people[i] = 0;
+				people[j] = 0;
+				i = i + 1;
+				j = j - 1;
+			} else {
+				j = j - 1;
+			}
+			if (i > j) break;
+		}
+		for (int k = 0; k < people.length; k++) {
+			if (people[k] > 0) answer++;
 		}
 		return answer;
 	}
---
+++ programmers/src/test/java/levelTwo/RepeatBinaryTransformationTest.java
@@ -0,0 +1,7 @@
+package levelTwo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepeatBinaryTransformationTest {
+
+}
