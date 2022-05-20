a044443dc0939c5185943c6242766809cd84a590
choisj2 <choisj2@brandi.co.kr>
2022-05-20 11:23:12 +0900

    프로그래머스 삼각 달팽이 완료

--- programmers/src/main/java/levelTwo/TriangularSnail.java
+++ programmers/src/main/java/levelTwo/TriangularSnail.java
@@ -1,20 +1,44 @@
 package levelTwo;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TriangularSnail {
 
 	public int[] solution(int n) {
-		int length = IntStream.range(0, n).reduce(0, (a, b) -> a + b);
-		int[] answer = new int[length];
-		int number = 1;
+		List<Integer> list = new ArrayList<>();
+		int[][] grid = new int[n][n];
+		int[] dx = new int[]{0, 1, -1};
+		int[] dy = new int[]{1, 0, -1};
+		int x = 0;
+		int y = 0;
+		int k = 0;
+		int t = 1;
+		for (int i = n; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				x = x + dx[k % 3];
+				y = y + dy[k % 3];
+				grid[y - 1][x] = t++;
+			}
+			k++;
+		}
+		display(n, grid);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				answer[i] = number++;
+				if (grid[i][j] > 0) {
+					list.add(grid[i][j]);
+				}
 			}
 		}
-		System.out.println(Arrays.toString(answer));
-		return answer;
+		return list.stream().mapToInt(e -> e.intValue()).toArray();
+	}
+
+	private void display(int n, int[][] grid) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(grid[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
