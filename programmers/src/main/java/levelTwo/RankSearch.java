6d17bce6d18c52ae50869c195eb132ed326d79dd
genius.choi <csj4032@gmail.com>
2022-05-21 20:45:47 +0900

    프로그래머 순위 검색 완료

--- programmers/src/main/java/levelTwo/RankSearch.java
+++ programmers/src/main/java/levelTwo/RankSearch.java
@@ -1,55 +1,73 @@
 package levelTwo;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class RankSearch {
 
 	public int[] solution(String[] info, String[] query) {
 		int m = info.length;
-		int n = 5;
-		String[][] table = new String[m][n];
-
-		for (int i = 0; i < m; i++) table[i] = info[i].split(" ");
-
 		int l = query.length;
 		int[] answer = new int[l];
-		String[][] queryTable = new String[l][n];
+		Map<String, List<Integer>> informationMap = new LinkedHashMap<>();
+		String[] language = new String[]{"cpp", "java", "python", "-"};
+		String[] group = new String[]{"backend", "frontend", "-"};
+		String[] career = new String[]{"junior", "senior", "-"};
+		String[] food = new String[]{"chicken", "pizza", "-"};
 
-		for (int i = 0; i < l; i++) {
-			String[] queries = query[i].split(" ");
-			queryTable[i][0] = queries[0];
-			queryTable[i][1] = queries[2];
-			queryTable[i][2] = queries[4];
-			queryTable[i][3] = queries[6];
-			queryTable[i][4] = queries[7];
-			String[] conditions = queryTable[i];
-			boolean[][] checked = new boolean[m][n];
-			for (int j = 0; j < 5; j++) {
-				String condition = conditions[j];
-				if (j == 4) {
-					int condition1 = Integer.parseInt(conditions[4]);
-					for (int jj = 0; jj < m; jj++) {
-						int point = Integer.parseInt(table[jj][4]);
-						if (condition1 <= point) checked[jj][4] = true;
-						int kk = 0;
-						for (int t = 0; t < n; t++) {
-							if (checked[jj][t]) kk++;
-						}
-						if (kk == n) answer[i]++;
+		for (int i = 0; i < language.length; i++) {
+			for (int j = 0; j < group.length; j++) {
+				for (int k = 0; k < career.length; k++) {
+					for (int n = 0; n < food.length; n++) {
+						String key = language[i] + group[j] + career[k] + food[n];
+						informationMap.put(key, new ArrayList<>());
 					}
-				} else {
-					for (int k = 0; k < m; k++) {
-						if (table[k][j].equals(condition) || condition.equals("-")) {
-							checked[k][j] = true;
+				}
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			String[] infos = info[i].split(" ");
+			String[] infosLanguage = new String[]{infos[0], "-"};
+			String[] infosGroup = new String[]{infos[1], "-"};
+			String[] infosCareer = new String[]{infos[2], "-"};
+			String[] infosFood = new String[]{infos[3], "-"};
+			for (int j = 0; j < infosLanguage.length; j++) {
+				for (int k = 0; k < infosGroup.length; k++) {
+					for (int n = 0; n < infosCareer.length; n++) {
+						for (int o = 0; o < infosFood.length; o++) {
+							String key = infosLanguage[j] + infosGroup[k] + infosCareer[n] + infosFood[o];
+							informationMap.get(key).add(Integer.valueOf(infos[4]));
 						}
 					}
 				}
 			}
-
-
-			//System.out.println(Arrays.deepToString(checked));
 		}
-		//System.out.println(Arrays.toString(answer));
+
+		informationMap.entrySet().stream().forEach(e -> Collections.sort(e.getValue(), Comparator.naturalOrder()));
+
+		for (int i = 0; i < l; i++) {
+			String[] queries = query[i].replaceAll(" and ", "").split(" ");
+			String key = queries[0];
+			Integer point = Integer.valueOf(queries[1]);
+			List<Integer> points = informationMap.get(key);
+			int lowBound = lowBound(points, point);
+			answer[i] = points.size() - lowBound;
+		}
+
 		return answer;
+	}
+
+	private int lowBound(List<Integer> array, int target) {
+		int start = 0;
+		int end = array.size();
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (array.get(mid) >= target) {
+				end = mid;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return end;
 	}
 }
