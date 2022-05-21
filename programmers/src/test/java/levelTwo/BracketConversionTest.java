8e1dc463a0f16ec7ec151f29eabceda88e3360ad
genius.choi <csj4032@gmail.com>
2022-05-22 04:32:33 +0900

    프로그래머스 괄호 변환 진행 중

---
+++ programmers/src/test/java/levelTwo/BracketConversionTest.java
@@ -0,0 +1,18 @@
+package levelTwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BracketConversionTest {
+
+	@Test
+	public void test() {
+		BracketConversion bracketConversion = new BracketConversion();
+		Assertions.assertEquals("()", bracketConversion.solution("()((())()))("));
+		//Assertions.assertEquals("(()())()", bracketConversion.solution("(()())()"));
+		Assertions.assertEquals("((()))()", bracketConversion.solution(")))(((()"));
+		Assertions.assertEquals("(((())))", bracketConversion.solution("))))(((("));
+		Assertions.assertEquals("()(())", bracketConversion.solution("()))(("));
+		Assertions.assertEquals("()(())()", bracketConversion.solution("()))((()"));
+	}
+}
