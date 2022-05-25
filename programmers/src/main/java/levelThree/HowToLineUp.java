b6a0c67b21c9576c86af6bb5ae49b9bef7eff37c
choisj2 <choisj2@brandi.co.kr>
2022-05-25 18:37:22 +0900

    프로그래머스 줄 서는 방법 진행 중

--- programmers/src/main/java/levelThree/HowToLineUp.java
+++ programmers/src/main/java/levelThree/HowToLineUp.java
@@ -5,34 +5,17 @@ import java.util.LinkedList;
 
 public class HowToLineUp {
 
-	static int mm = 0;
-	static long kk = 0;
-	static long tt = 0;
-	static long qq = 1;
-	static int[] numbers;
-	static int[] permutations;
-	static int[] answer;
-	static boolean[] visited;
 
 	public int[] solution(int n, long k) {
-		numbers = new int[n];
-		permutations = new int[n];
-		answer = new int[n];
-		visited = new boolean[n];
-		kk = k;
-		qq = factorial(n - 1);
-		mm = (int) (k / qq);
-		tt = (int) (k / qq) * qq;
+		LinkedList list = new LinkedList<>();
+		for (int i = 1; i <= n; i++) {
+			list.add(i);
+		}
 
-		System.out.println(qq + " " + mm + " " + tt);
+		long f = factorial(n - 1);
 
-		LinkedList<Integer> numberList = new LinkedList<>();
-		for (int i = 1; i <= n; i++) numberList.add(i);
-		numberList.remove(mm);
-		numbers = numberList.stream().mapToInt(e -> e.intValue()).toArray();
-		permutation(0, n-1);
-		System.out.println(Arrays.toString(answer));
-		return answer;
+
+		return null;
 	}
 
 	public static long factorial(int number) {
@@ -41,21 +24,5 @@ public class HowToLineUp {
 			result *= factor;
 		}
 		return result;
-	}
-
-	private static void permutation(int idx, int n) {
-		if (idx == n) {
-			tt++;
-			if (kk == tt) answer = permutations;
-			return;
-		}
-
-		for (int i = 0; i < n; i++) {
-			if (visited[i]) continue;
-			permutations[idx] = numbers[i];
-			visited[i] = true;
-			permutation(idx + 1, n);
-			visited[i] = false;
-		}
 	}
 }
