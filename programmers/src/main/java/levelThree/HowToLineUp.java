e86f1dae0ffc8856fa5c3c566e393daf57dedb88
genius.choi <csj4032@gmail.com>
2022-05-25 18:57:21 +0900

    프로그래머스 줄 서는 방법 진행 중

--- programmers/src/main/java/levelThree/HowToLineUp.java
+++ programmers/src/main/java/levelThree/HowToLineUp.java
@@ -7,15 +7,32 @@ public class HowToLineUp {
 
 
 	public int[] solution(int n, long k) {
-		LinkedList list = new LinkedList<>();
+		int[] answer = new int[n];
+		LinkedList<Integer> list = new LinkedList<>();
 		for (int i = 1; i <= n; i++) {
 			list.add(i);
 		}
 
-		long f = factorial(n - 1);
+		int i = 0;
+		while (true) {
+			long f = factorial(n - 1);
+			int kk = (int) (k / f);
+			int kkk = (int) (k % f);
+			if (n == 0) {
+				//answer[n] = list.get(0);
+				//answer[n - 1] = list.get(1);
+				break;
+			}
+			k = kkk;
+			n = n - 1;
+			answer[i] = list.get(kk);
+			list.remove(kk);
+			i++;
+			System.out.println(kk + " " + kkk);
+			System.out.println(Arrays.toString(answer));
+		}
 
-
-		return null;
+		return answer;
 	}
 
 	public static long factorial(int number) {
