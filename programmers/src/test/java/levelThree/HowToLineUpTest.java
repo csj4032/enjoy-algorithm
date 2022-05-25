2290a63cece6e530a5684412bf82b8fd8a2b0098
genius.choi <csj4032@gmail.com>
2022-05-25 23:26:42 +0900

    프로그래머스 줄 서는 방법 완료

--- programmers/src/test/java/levelThree/HowToLineUpTest.java
+++ programmers/src/test/java/levelThree/HowToLineUpTest.java
@@ -8,6 +8,10 @@ class HowToLineUpTest {
 	@Test
 	public void test() {
 		HowToLineUp howToLineUp = new HowToLineUp();
+		Assertions.assertArrayEquals(new int[]{1}, howToLineUp.solution(1, 1));
 		Assertions.assertArrayEquals(new int[]{3, 1, 2}, howToLineUp.solution(3, 5));
+		Assertions.assertArrayEquals(new int[]{1, 4, 2, 3}, howToLineUp.solution(4, 5));
+		Assertions.assertArrayEquals(new int[]{2, 3, 4, 1}, howToLineUp.solution(4, 10));
+		Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, howToLineUp.solution(20, 1));
 	}
 }
