d661bfc8726887693fc1126d6a4fa3db260c7c40
genius.choi <csj4032@gmail.com>
2022-05-28 22:42:01 +0900

    프로그래머스 입국심사 진행 중

--- programmers/src/main/java/levelThree/Immigration.java
+++ programmers/src/main/java/levelThree/Immigration.java
@@ -2,66 +2,54 @@ package levelThree;
 
 import java.util.Arrays;
 
+/**
+ * 입국심사를 기다리는 사람 수를 적당히 나워서 심사를 처리
+ * 심사원은 최대한 동일한 입국자를 할당
+ * 심사를 받는데 걸리는 최대 시간에서 최소 시간으로 입국자를 넘김
+ * 최소 시간이 다시 늘어나는 시점에 종료
+ */
 public class Immigration {
 
 	public long solution(int n, int[] times) {
 		long answer = 0;
-		int idx = 0;
+		long temp = Long.MAX_VALUE;
 		int length = times.length;
+		int portion = n / length;
+		int remainder = n % length;
+		long[] imm = new long[length];
 		Arrays.sort(times);
-		int[] imm = new int[length];
-		//int[] imm = Arrays.copyOf(times, length);
+		Arrays.fill(imm, portion);
+		for (int i = 0; i < remainder; i++) {
+			imm[i] = imm[i] + 1;
+		}
+		//System.out.println(Arrays.toString(imm));
 		while (true) {
-			answer++;
-			Arrays.sort(imm);
-			int lower = lowerBound(imm, 0, length);
-			int upper = upperBound(imm, 0, length);
-			System.out.println(lower + " " + upper + " " + Arrays.toString(imm) + " " + answer + " " + n);
-			if (lower != upper) {
-				for (int i = lower; i <= upper; i++) {
-					if (imm[i] > 0) continue;
-					n--;
-					imm[i] = times[idx];
-					idx++;
-					idx = idx % length;
+			long max = Long.MIN_VALUE;
+			int iMax = 0;
+			long min = Long.MAX_VALUE;
+			int iMin = 0;
+			for (int i = 0; i < length; i++) {
+				long time = imm[i] * times[i];
+				if (time == 0) continue;
+				if (max <= time) {
+					max = time;
+					iMax = i;
 				}
-			} else {
-				for (int i = 0; i < length; i++) {
-					imm[i] = imm[i] - 1;
+				if (min >= time) {
+					min = time;
+					iMin = i;
 				}
 			}
-			if (n < 0 && (lower < upper)) break;
+			imm[iMax] = imm[iMax] - 1;
+			imm[iMin] = imm[iMin] + 1;
+			answer = max;
+			//System.out.println(answer + " " + max + " " + temp);
+			if (answer >= temp) {
+				answer = temp;
+				break;
+			}
+			temp = answer;
 		}
 		return answer;
-	}
-
-	private int lowerBound(int[] array, int target, int size) {
-		int mid;
-		int start = 0;
-		int end = size - 1;
-		while (end > start) {
-			mid = (end + start) / 2;
-			if (array[mid] >= target) {
-				end = mid;
-			} else {
-				start = mid + 1;
-			}
-		}
-		return end;
-	}
-
-	private int upperBound(int[] array, int target, int size) {
-		int mid;
-		int start = 0;
-		int end = size - 1;
-		while (end > start) {
-			mid = (start + end) / 2;
-			if (array[mid] > target) {
-				end = mid;
-			} else {
-				start = mid + 1;
-			}
-		}
-		return end;
 	}
 }
