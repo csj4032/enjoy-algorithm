ed2b38d1c52f2a912140c12b6788f76660ae4606
genius.choi <csj4032@gmail.com>
2022-05-28 22:42:01 +0900

    프로그래머스 입국심사 진행 중

--- programmers/src/test/java/levelThree/ImmigrationTest.java
+++ programmers/src/test/java/levelThree/ImmigrationTest.java
@@ -8,6 +8,9 @@ class ImmigrationTest {
 	@Test
 	public void test() {
 		Immigration immigration = new Immigration();
-		Assertions.assertEquals(28, immigration.solution(6, new int[]{7, 10}));
+		//Assertions.assertEquals(28, immigration.solution(6, new int[]{7, 10}));
+		//Assertions.assertEquals(2, immigration.solution(2, new int[]{1, 10}));
+		//Assertions.assertEquals(3, immigration.solution(3, new int[]{1, 99, 99}));
+		Assertions.assertEquals(30, immigration.solution(10, new int[]{6, 8, 10}));
 	}
 }
