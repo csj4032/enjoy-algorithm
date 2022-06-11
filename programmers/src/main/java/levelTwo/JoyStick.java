01d5aa91a587b0b00ad7e6049c9cbe5f87032311
genius.choi <csj4032@gmail.com>
2022-06-11 20:37:39 +0900

    프로그래머 조이스틱 완료

--- programmers/src/main/java/levelTwo/JoyStick.java
+++ programmers/src/main/java/levelTwo/JoyStick.java
@@ -1,51 +1,21 @@
 package levelTwo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class JoyStick {
-
-	static Map<String, Integer> alphabet = new HashMap<>();
-
-	static {
-		alphabet.put("A", 0);
-		alphabet.put("B", 1);
-		alphabet.put("C", 2);
-		alphabet.put("D", 3);
-		alphabet.put("E", 4);
-		alphabet.put("F", 5);
-		alphabet.put("G", 6);
-		alphabet.put("H", 7);
-		alphabet.put("I", 8);
-		alphabet.put("J", 9);
-		alphabet.put("K", 10);
-		alphabet.put("L", 11);
-		alphabet.put("M", 12);
-		alphabet.put("N", 13);
-		alphabet.put("O", 12);
-		alphabet.put("P", 11);
-		alphabet.put("Q", 10);
-		alphabet.put("R", 9);
-		alphabet.put("S", 8);
-		alphabet.put("T", 7);
-		alphabet.put("U", 6);
-		alphabet.put("V", 5);
-		alphabet.put("W", 4);
-		alphabet.put("X", 3);
-		alphabet.put("Y", 2);
-		alphabet.put("Z", 1);
-	}
 
 	public int solution(String name) {
 		int answer = 0;
-		System.out.println(recursive(0, name.length()));
-		return answer;
-	}
-
-	private int recursive(int i, int n) {
-		if (i > n) return i;
-		return recursive(i + 1, n) + recursive(i + 2, n);
+		int length = name.length();
+		int move = length - 1;
+		int index;
+		for (int i = 0; i < length; i++) {
+			answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+			index = i + 1;
+			while (index < length && name.charAt(index) == 'A') {
+				index++;
+			}
+			move = Math.min(move, i * 2 + length - index);
+			move = Math.min(move, (length - index) * 2 + i);
+		}
+		return answer + move;
 	}
 }
-
-
