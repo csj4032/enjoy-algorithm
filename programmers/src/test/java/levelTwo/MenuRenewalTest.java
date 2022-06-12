facff1c1c58e612722a5aa753c08384230e904a7
genius.choi <csj4032@gmail.com>
2022-06-12 23:49:34 +0900

    프로그래머스 메뉴 리뉴얼 완료

--- programmers/src/test/java/levelTwo/MenuRenewalTest.java
+++ programmers/src/test/java/levelTwo/MenuRenewalTest.java
@@ -8,9 +8,8 @@ class MenuRenewalTest {
 	@Test
 	void test() {
 		MenuRenewal menuRenewal = new MenuRenewal();
-//		Assertions.assertArrayEquals(new String[]{"AC", "ACDE", "BCFG", "CDE"}, menuRenewal.solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4}));
-//		Assertions.assertArrayEquals(new String[]{"ACD", "AD", "ADE", "CD", "XYZ"}, menuRenewal.solution(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}));
+		//Assertions.assertArrayEquals(new String[]{"AC", "ACDE", "BCFG", "CDE"}, menuRenewal.solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4}));
+		//Assertions.assertArrayEquals(new String[]{"ACD", "AD", "ADE", "CD", "XYZ"}, menuRenewal.solution(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}));
 		Assertions.assertArrayEquals(new String[]{"WX", "XY"}, menuRenewal.solution(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2, 3, 4}));
 	}
 }
-// A W X Y Z
