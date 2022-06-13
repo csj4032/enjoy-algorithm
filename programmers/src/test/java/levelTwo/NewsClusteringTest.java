ba539bd9fb6206cfb238f4b6e276705fe8434815
genius.choi <csj4032@gmail.com>
2022-06-13 19:01:48 +0900

    프로그래머스 뉴스 클러스터링 진행 중

---
+++ programmers/src/test/java/levelTwo/NewsClusteringTest.java
@@ -0,0 +1,17 @@
+package levelTwo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class NewsClusteringTest {
+
+	@Test
+	void test() {
+		NewsClustering newsClustering = new NewsClustering();
+//		Assertions.assertEquals(16384, newsClustering.solution("FRANCE", "french"));
+//		Assertions.assertEquals(65536, newsClustering.solution("handshake", "shake hands"));
+		Assertions.assertEquals(16384, newsClustering.solution("abc", "abbb"));
+//		Assertions.assertEquals(43690, newsClustering.solution("aa1+aa2", "AAAA12"));
+//		Assertions.assertEquals(65536, newsClustering.solution("E=M*C^2", "e=m*c^2"));
+	}
+}
