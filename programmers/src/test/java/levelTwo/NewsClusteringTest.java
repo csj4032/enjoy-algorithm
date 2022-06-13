c1f0599033b75248d6cb2444eb39ae1873218a76
genius.choi <csj4032@gmail.com>
2022-06-13 23:40:38 +0900

    프로그래머스 뉴스 클러스터링 완료

--- programmers/src/test/java/levelTwo/NewsClusteringTest.java
+++ programmers/src/test/java/levelTwo/NewsClusteringTest.java
@@ -8,10 +8,10 @@ class NewsClusteringTest {
 	@Test
 	void test() {
 		NewsClustering newsClustering = new NewsClustering();
-//		Assertions.assertEquals(16384, newsClustering.solution("FRANCE", "french"));
-//		Assertions.assertEquals(65536, newsClustering.solution("handshake", "shake hands"));
+		Assertions.assertEquals(16384, newsClustering.solution("FRANCE", "french"));
+		Assertions.assertEquals(65536, newsClustering.solution("handshake", "shake hands"));
 		Assertions.assertEquals(16384, newsClustering.solution("abc", "abbb"));
-//		Assertions.assertEquals(43690, newsClustering.solution("aa1+aa2", "AAAA12"));
-//		Assertions.assertEquals(65536, newsClustering.solution("E=M*C^2", "e=m*c^2"));
+		Assertions.assertEquals(43690, newsClustering.solution("aa1+aa2", "AAAA12"));
+		Assertions.assertEquals(65536, newsClustering.solution("E=M*C^2", "e=m*c^2"));
 	}
 }
