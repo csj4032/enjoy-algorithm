9eb1676768334c72de85fc56707e9cf8d0d92cd5
genius.choi <csj4032@gmail.com>
2024-01-18 19:15:36 +0900

    백준 1806 문제 완료

--- baekjoon/src/main/python/p1806.py
+++ baekjoon/src/main/python/p1806.py
@@ -1,3 +1,25 @@
 ns = list(map(int, (input().split())))
-n = ns[0]
-s = ns[1]
+numbers = list(map(int, (input().split())))
+
+n: int = ns[0]
+s: int = ns[1]
+i: int = 0
+j: int = 0
+k: int = 0
+l: int = n + 1
+
+while True:
+    if k >= s:
+        l = min(l, j - i)
+        k -= numbers[i]
+        i = i + 1
+    elif j == n:
+        break
+    else:
+        k += numbers[j]
+        j = j + 1
+
+if l == n + 1:
+    l = 0
+
+print(l)
