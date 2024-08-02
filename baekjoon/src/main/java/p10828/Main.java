d40f10b24e484d056b3611a76fa1787740192fa8
genius.choi <csj4032@gmail.com>
2024-08-02 10:15:55 +0900

    백준 10828, 10845 문제 완료

--- baekjoon/src/main/java/p10828/Main.java
+++ baekjoon/src/main/java/p10828/Main.java
@@ -1,95 +1,97 @@
 package p10828;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		Stack stack = new Stack();
-		for (int i = 0; i < n; i++) {
-			String m = sc.next();
-			if (m.equals("push")) {
-				stack.push(sc.nextInt());
-			}
-			if (m.equals("pop")) {
-				System.out.println(stack.pop());
-			}
-			if (m.equals("size")) {
-				System.out.println(stack.size());
-			}
-			if (m.equals("empty")) {
-				System.out.println(stack.isEmpty());
-			}
-			if (m.equals("top")) {
-				System.out.println(stack.top());
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack stack = new Stack();
+        for (int i = 0; i < n; i++) {
+            String[] command = br.readLine().split(" ");
+            if (command[0].equals("push")) {
+                stack.push(Integer.parseInt(command[1]));
+            }
+            if (command[0].equals("pop")) {
+                System.out.println(stack.pop());
+            }
+            if (command[0].equals("size")) {
+                System.out.println(stack.size());
+            }
+            if (command[0].equals("empty")) {
+                System.out.println(stack.isEmpty());
+            }
+            if (command[0].equals("top")) {
+                System.out.println(stack.top());
+            }
+        }
+    }
 }
 
 class Stack {
 
-	private int size;
-	private Node current;
+    private int size;
+    private Node current;
 
-	public void push(int n) {
-		if (isEmpty() == 1) {
-			current = new Node(n);
-		} else {
-			Node node = new Node(n);
-			node.setPrevNode(current);
-			current = node;
-		}
-		size++;
-	}
+    public void push(int n) {
+        if (isEmpty() == 1) {
+            current = new Node(n);
+        } else {
+            Node node = new Node(n);
+            node.setPrevNode(current);
+            current = node;
+        }
+        size++;
+    }
 
-	public int pop() {
-		if (current == null) {
-			return -1;
-		} else {
-			int value = current.getValue();
-			current = current.getPrevNode();
-			size--;
-			return value;
-		}
-	}
+    public int pop() {
+        if (current == null) {
+            return -1;
+        } else {
+            int value = current.getValue();
+            current = current.getPrevNode();
+            size--;
+            return value;
+        }
+    }
 
-	public int size() {
-		return size;
-	}
+    public int size() {
+        return size;
+    }
 
-	public int isEmpty() {
-		return size == 0 ? 1 : 0;
-	}
+    public int isEmpty() {
+        return size == 0 ? 1 : 0;
+    }
 
-	public int top() {
-		if (current == null) {
-			return -1;
-		} else {
-			return current.getValue();
-		}
-	}
+    public int top() {
+        if (current == null) {
+            return -1;
+        } else {
+            return current.getValue();
+        }
+    }
 }
 
 class Node {
-	private Node prevNode;
-	private int value;
+    private Node prevNode;
+    private final int value;
 
-	public Node(int value) {
-		this.value = value;
-	}
+    public Node(int value) {
+        this.value = value;
+    }
 
-	public Node getPrevNode() {
-		return prevNode;
-	}
+    public Node getPrevNode() {
+        return prevNode;
+    }
 
-	public Node setPrevNode(Node node) {
-		return prevNode = node;
-	}
+    public void setPrevNode(Node node) {
+        prevNode = node;
+    }
 
-	public int getValue() {
-		return value;
-	}
+    public int getValue() {
+        return value;
+    }
 }
--- baekjoon/src/main/java/p10845/Main.java
+++ baekjoon/src/main/java/p10845/Main.java
@@ -1,31 +1,33 @@
 package p10845;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		Queue queue = new Queue();
 		for (int i = 0; i < n; i++) {
-			String str = sc.next();
-			if (str.equals("push")) {
-				queue.push(sc.nextInt());
+			String[] command = br.readLine().split(" ");
+			if (command[0].equals("push")) {
+				queue.push(Integer.parseInt(command[1]));
 			}
-			if (str.equals("front")) {
+			if (command[0].equals("front")) {
 				System.out.println(queue.front());
 			}
-			if (str.equals("back")) {
+			if (command[0].equals("back")) {
 				System.out.println(queue.back());
 			}
-			if (str.equals("size")) {
+			if (command[0].equals("size")) {
 				System.out.println(queue.size());
 			}
-			if (str.equals("pop")) {
+			if (command[0].equals("pop")) {
 				System.out.println(queue.pop());
 			}
-			if (str.equals("empty")) {
+			if (command[0].equals("empty")) {
 				System.out.println(queue.isEmpty());
 			}
 		}
