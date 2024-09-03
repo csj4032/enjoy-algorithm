c556e17f95d7d5ab374978d474b86d2a5d3eadd4
csj4032 <csj4032@gmail.com>
2024-09-03 14:16:03 +0900

    백준 2023 문제 완료

--- baekjoon/src/main/java/p2566/Main.java
+++ baekjoon/src/main/java/p2566/Main.java
@@ -1,26 +1,29 @@
 package p2566;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int max = 0;
-		int maxRow = 0;
-		int maxCol = 0;
-		for (int i = 1; i < 10; i++) {
-			for (int j = 1; j < 10; j++) {
-				int n = sc.nextInt();
-				if (max < n) {
-					max = n;
-					maxRow = i;
-					maxCol = j;
-				}
-			}
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int maxRow = 0;
+        int maxCol = 0;
+        for (int i = 1; i < 10; i++) {
+            String[] s = br.readLine().split(" ");
+            for (int j = 1; j < 10; j++) {
+                int n = Integer.parseInt(s[j - 1]);
+                if (max <= n) {
+                    max = n;
+                    maxRow = i;
+                    maxCol = j;
+                }
+            }
+        }
 
-		System.out.println(max);
-		System.out.println(maxRow + " " + maxCol);
-	}
+        System.out.println(max);
+        System.out.println(maxRow + " " + maxCol);
+    }
 }
