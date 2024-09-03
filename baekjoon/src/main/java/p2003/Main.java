c73b429d8ce8b6828b47b7b76b0c1320a2e3c2f9
genius.choi <csj4032@gmail.com>
2024-09-03 14:16:03 +0900

    백준 2023 문제 완료

--- baekjoon/src/main/java/p2003/Main.java
+++ baekjoon/src/main/java/p2003/Main.java
@@ -7,32 +7,34 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int t = sc.nextInt();
-		int[] m = new int[10001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int[] m = new int[10001];
 
-		for (int i = 0; i < n; i++) {
-			m[i] = sc.nextInt();
-		}
+        for (int i = 0; i < n; i++) m[i] = sc.nextInt();
 
-		int start = 0;
-		int end = 0;
-		int sum = 0;
-		int count = 0;
+        int left = 0;
+        int right = 0;
+        int sum = m[0];
+        int count = 0;
 
-		while (end <= n) {
+        while (true) {
+            if (sum < t) {
+                if (right < n) {
+                    sum += m[++right];
+                } else {
+                    break;
+                }
+            } else if (sum == t) {
+                count++;
+                sum -= m[left++];
+            } else {
+                sum -= m[left++];
+            }
+        }
 
-			if (sum < t) sum += m[end++];
-			if (sum > t) sum -= m[start++];
-
-			if (sum == t) {
-				sum -= m[start++];
-				count++;
-			}
-		}
-
-		System.out.println(count);
-	}
+        System.out.println(count);
+    }
 }
