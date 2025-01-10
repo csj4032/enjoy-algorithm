2d5f46be48d4f158ddaefc1cf2c8af5aa3a654b7
genius.choi <csj4032@gmail.com>
2025-01-10 18:00:26 +0900

    프로그래머스 공던지기 완료

--- programmers/src/main/java/levelZero/ThrowingBall.java
+++ programmers/src/main/java/levelZero/ThrowingBall.java
@@ -1,10 +1,11 @@
 package levelZero;
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/120843
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/120843">공 던지기</a>
+ **/
 public class ThrowingBall {
 
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        return answer;
+        return (k - 1) * 2 % numbers.length + 1;
     }
 }
