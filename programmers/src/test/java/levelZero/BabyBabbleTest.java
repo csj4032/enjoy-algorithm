9c1b5c58bcb27a2974e89979a41e41fc0c8129e3
genius.choi <csj4032@gmail.com>
2025-01-10 23:23:09 +0900

    프로그래머스 안전지대 문제 완료

---
+++ programmers/src/test/java/levelZero/BabyBabbleTest.java
@@ -0,0 +1,14 @@
+package levelZero;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BabyBabbleTest {
+
+    @Test
+    public void test() {
+        BabyBabble babyBabble = new BabyBabble();
+        Assertions.assertEquals(1, babyBabble.solution(new String[]{"aya", "yee", "u", "maa", "wyeoo"}));
+        Assertions.assertEquals(3, babyBabble.solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"}));
+    }
+}
