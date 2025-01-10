4d5efca4e9ddd71c4f2b1d85cef8406c4e8a72ce
genius.choi <csj4032@gmail.com>
2025-01-10 18:00:26 +0900

    프로그래머스 공던지기 완료

--- programmers/src/test/java/levelZero/ThrowingBallTest.java
+++ programmers/src/test/java/levelZero/ThrowingBallTest.java
@@ -3,13 +3,13 @@ package levelZero;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ThrowingBallTest {
 
     @Test
-    public void test(){
+    public void test() {
         ThrowingBall throwingBall = new ThrowingBall();
-        Assertions.assertEquals(1, throwingBall.solution(new int[]{1, 2, 3, 4}, 2));
+        Assertions.assertEquals(3, throwingBall.solution(new int[]{1, 2, 3, 4}, 2));
+        Assertions.assertEquals(3, throwingBall.solution(new int[]{1, 2, 3, 4, 5, 6}, 5));
+        Assertions.assertEquals(3, throwingBall.solution(new int[]{1, 2, 3}, 2));
     }
 }
