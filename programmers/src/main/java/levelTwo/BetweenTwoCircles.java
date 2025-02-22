8d1d3ebf58c03ff76602b7e98afd41e7f52be0a5
genius.choi <csj4032@gmail.com>
2025-02-22 20:08:01 +0900

    프로그래머스 숫자 블록 완료

--- programmers/src/main/java/levelTwo/BetweenTwoCircles.java
+++ programmers/src/main/java/levelTwo/BetweenTwoCircles.java
@@ -1,25 +1,23 @@
 package levelTwo;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class BetweenTwoCircles {
 
     public long solution(int r1, int r2) {
-        long innerCirclePoints = countGridPoints(r1);
-        long outerCirclePoints = countGridPoints(r2);
-        return outerCirclePoints - innerCirclePoints + 4L;
+        return countGridPoints(r1, r2);
     }
 
-    public static long countGridPoints(int r) {
-        long count = 0;
-        long rSquared = (long) r * r;
+    public static long countGridPoints(int r1, int r2) {
+        long count = 0L;
+        long r1Squared = (long) r1 * r1;
+        long r2Squared = (long) r2 * r2;
 
-        for (int x = 0; x <= r; x++) {
-            long maxYSquared = rSquared - (long) x * x;
+        for (int x = 0; x <= r2; x++) {
+            long maxYSquared = r2Squared - (long) x * x;
             long maxY = (long) Math.sqrt(maxYSquared);
-            count += maxY + 1;
+            long minYSquared = r1Squared - (long) x * x;
+            long minY = minYSquared > 0 ? (long) Math.ceil(Math.sqrt(minYSquared)) : 0;
+            count += (maxY - minY + 1);
         }
-        return 4 * count - 4L * r - 3;
+        return count * 4 - ((r2 - r1 + 1) * 4);
     }
 }
--- programmers/src/test/java/levelTwo/BetweenTwoCirclesTest.java
+++ programmers/src/test/java/levelTwo/BetweenTwoCirclesTest.java
@@ -11,5 +11,6 @@ public class BetweenTwoCirclesTest {
         Assertions.assertEquals(12, betweenTwoCircles.solution(1, 2));
         Assertions.assertEquals(28, betweenTwoCircles.solution(1, 3));
         Assertions.assertEquals(20, betweenTwoCircles.solution(2, 3));
+        //Assertions.assertEquals(20, betweenTwoCircles.solution(100, 101));
     }
 }
