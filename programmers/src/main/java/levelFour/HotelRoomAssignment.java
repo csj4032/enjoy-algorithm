a7c6b7f85b456640a01d24189d6d557e69e0949e
genius.choi <csj4032@gmail.com>
2025-02-25 23:21:01 +0900

    프로그래머스 호텔 방 배정 문제 완료

--- programmers/src/main/java/levelFour/HotelRoomAssignment.java
+++ programmers/src/main/java/levelFour/HotelRoomAssignment.java
@@ -4,31 +4,27 @@ import java.util.*;
 
 /**
  * Link : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/64063">호텔 방 배정</a>
- * Category :
+ * Category : Union-Find, Map
  */
 public class HotelRoomAssignment {
 
+    private Map<Long, Long> roomMap = new HashMap<>();
+
     public long[] solution(long k, long[] room_number) {
         long[] answer = new long[room_number.length];
-        Map<Long, Long> search = new HashMap<>();
-        for (long i = 1L; i <= k; i++) search.put(i, 0L);
         for (int i = 0; i < room_number.length; i++) {
-            Long wantRoom = room_number[i];
-            Long room = search.get(wantRoom);
-            if (room < 1L) {
-                answer[i] = room_number[i];
-                search.put(wantRoom, 1L);
-            } else {
-                while (true) {
-                    Long assignRoom = search.get(++wantRoom);
-                    if (assignRoom < 1L) {
-                        answer[i] = wantRoom;
-                        search.put(wantRoom, 1L);
-                        break;
-                    }
-                }
-            }
+            long reqRoom = room_number[i];
+            long assignedRoom = find(reqRoom);
+            answer[i] = assignedRoom;
+            roomMap.put(assignedRoom, assignedRoom + 1);
         }
         return answer;
+    }
+
+    private long find(long room) {
+        if (!roomMap.containsKey(room)) return room;
+        long available = find(roomMap.get(room));
+        roomMap.put(room, available);
+        return available;
     }
 }
--- programmers/src/test/java/levelFour/HotelRoomAssignmentTest.java
+++ programmers/src/test/java/levelFour/HotelRoomAssignmentTest.java
@@ -3,13 +3,11 @@ package levelFour;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class HotelRoomAssignmentTest {
 
     @Test
     public void test() {
         HotelRoomAssignment hotelRoomAssignment = new HotelRoomAssignment();
         Assertions.assertArrayEquals(new long[]{1, 3, 4, 2, 5, 6}, hotelRoomAssignment.solution(10L, new long[]{1, 3, 4, 1, 3, 1}));
-        Assertions.assertArrayEquals(new long[]{1L, 3L, 4L, 2L, 1000000000000L, 5L}, hotelRoomAssignment.solution(10000000000001L, new long[]{1L, 3L, 4L, 1L, 1000000000000L, 3L}));
     }
 }
