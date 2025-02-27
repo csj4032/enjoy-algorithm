c8123a6c13eecb1b887eb688c3429f1457a74761
genius.choi <csj4032@gmail.com>
2025-02-27 22:37:23 +0900

    프로그래머스 미로탈출 DFS 진행 중

--- programmers/src/test/java/levelTwo/MazeEscapeTest.java
+++ programmers/src/test/java/levelTwo/MazeEscapeTest.java
@@ -9,10 +9,12 @@ public class MazeEscapeTest {
     @Test
     public void test() {
         MazeEscape mazeEscape = new MazeEscape();
-//        assertEquals(16, mazeEscape.solution(new String[]{"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"}));
-//        assertEquals(-1, mazeEscape.solution(new String[]{"LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"}));
-//        assertEquals(8, mazeEscape.solution(new String[]{"LOOOS","OOOOO","OOOOO","OOOOO","EOOOO"}));
-        assertEquals(8, mazeEscape.solution(new String[]{"OOOOS","OOOOO","OOOOO","OOOOO","EOOOL"}));
-//      assertEquals(2, mazeEscape.solution(new String[]{"OOELS","OOOOO","OOOOO","OOOOO","OOOOO"}));
+        assertEquals(16, mazeEscape.solution(new String[]{"SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"}));
+        assertEquals(-1, mazeEscape.solution(new String[]{"LOOXS", "OOOOX", "OOOOO", "OOOOO", "EOOOO"}));
+        assertEquals(8, mazeEscape.solution(new String[]{"LOOOS", "OOOOO", "OOOOO", "OOOOO", "EOOOO"}));
+        assertEquals(8, mazeEscape.solution(new String[]{"OOOOS", "OOOOO", "OOOOO", "OOOOO", "EOOOL"}));
+        assertEquals(2, mazeEscape.solution(new String[]{"OOELS", "OOOOO", "OOOOO", "OOOOO", "OOOOO"}));
+        assertEquals(6, mazeEscape.solution(new String[]{"SOEOL", "OOOOO", "OOOOO", "OOOOO", "OOOOO"}));
+//        assertEquals(6, mazeEscape.solution(new String[]{"SOEOOOOOOL", "OOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO"}));
     }
 }
