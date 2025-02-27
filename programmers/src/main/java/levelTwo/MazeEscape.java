11de2d409b7bdf69ced329ff39d9ce3ee4958fb1
genius.choi <csj4032@gmail.com>
2025-02-28 00:35:05 +0900

    프로그래머스 미로탈출 문제 완료

--- programmers/src/main/java/levelTwo/MazeEscape.java
+++ programmers/src/main/java/levelTwo/MazeEscape.java
@@ -1,57 +1,59 @@
 package levelTwo;
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MazeEscape {
 
     private static final int[] dy = {0, 0, -1, 1};
     private static final int[] dx = {-1, 1, 0, 0};
-    private static int rows;
-    private static int cols;
-    private static int min;
-    private static String[][] maze;
-    private static boolean[][] visited;
 
     public int solution(String[] maps) {
-        rows = maps.length;
-        cols = maps[0].length();
-        min = Integer.MAX_VALUE;
-        maze = new String[rows][cols];
-        visited = new boolean[rows][cols];
+        int rows = maps.length;
+        int cols = maps[0].length();
+        char[][] maze = new char[rows][cols];
         int[] start = new int[2];
+        int[] lever = new int[2];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                maze[i][j] = String.valueOf(maps[i].charAt(j));
-                if (maze[i][j].equals("S")) {
-                    start[0] = i;
-                    start[1] = j;
+                maze[i][j] = maps[i].charAt(j);
+                if (maze[i][j] == 'S') {
+                    start = new int[]{i, j};
+                } else if (maze[i][j] == 'L') {
+                    lever = new int[]{i, j};
                 }
             }
         }
-        maze(start[0], start[1], 0, false);
-        return min == Integer.MAX_VALUE ? -1 : min;
+
+        int toLever = bfs(maze, start, 'L');
+        if (toLever == -1) return -1;
+        int toEnd = bfs(maze, lever, 'E');
+        if (toEnd == -1) return -1;
+        return toLever + toEnd;
     }
 
-    private void maze(int row, int col, int count, boolean lever) {
-        if (maze[row][col].equals("E") && lever) {
-            min = Math.min(min, count);
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            int ny = row + dy[i];
-            int nx = col + dx[i];
-            if (min < count) continue;
-            if (nx < 0 || ny < 0 || ny >= rows || nx >= cols) continue;
-            if (maze[ny][nx].equals("X") || visited[ny][nx]) continue;
-            if (maze[ny][nx].equals("L")) {
-                lever = true;
-                for (int j = 0; j < visited.length; j++) {
-                    Arrays.fill(visited[j], false);
-                }
+    public static int bfs(char[][] maze, int[] start, char target) {
+        int rows = maze.length;
+        int cols = maze[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start[0], start[1], 0});
+        visited[start[0]][start[1]] = true;
+        while (!queue.isEmpty()) {
+            int[] point = queue.poll();
+            int row = point[0];
+            int col = point[1];
+            int step = point[2];
+            if (maze[row][col] == target) return step;
+            for (int i = 0; i < 4; i++) {
+                int ny = row + dy[i];
+                int nx = col + dx[i];
+                int nc = step + 1;
+                if (nx < 0 || ny < 0 || ny >= rows || nx >= cols || maze[ny][nx] == 'X' || visited[ny][nx]) continue;
+                visited[ny][nx] = true;
+                queue.add(new int[]{ny, nx, nc});
             }
-            visited[ny][nx] = true;
-            maze(ny, nx, count + 1, lever);
-            visited[ny][nx] = false;
         }
+        return -1;
     }
 }
--- programmers/src/test/java/levelTwo/MazeEscapeTest.java
+++ programmers/src/test/java/levelTwo/MazeEscapeTest.java
@@ -9,12 +9,16 @@ public class MazeEscapeTest {
     @Test
     public void test() {
         MazeEscape mazeEscape = new MazeEscape();
-        assertEquals(16, mazeEscape.solution(new String[]{"SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"}));
-        assertEquals(-1, mazeEscape.solution(new String[]{"LOOXS", "OOOOX", "OOOOO", "OOOOO", "EOOOO"}));
-        assertEquals(8, mazeEscape.solution(new String[]{"LOOOS", "OOOOO", "OOOOO", "OOOOO", "EOOOO"}));
-        assertEquals(8, mazeEscape.solution(new String[]{"OOOOS", "OOOOO", "OOOOO", "OOOOO", "EOOOL"}));
-        assertEquals(2, mazeEscape.solution(new String[]{"OOELS", "OOOOO", "OOOOO", "OOOOO", "OOOOO"}));
-        assertEquals(6, mazeEscape.solution(new String[]{"SOEOL", "OOOOO", "OOOOO", "OOOOO", "OOOOO"}));
-//        assertEquals(6, mazeEscape.solution(new String[]{"SOEOOOOOOL", "OOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO"}));
+//        assertEquals(16, mazeEscape.solution(new String[]{"SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"}));
+//        assertEquals(-1, mazeEscape.solution(new String[]{"LOOXS", "OOOOX", "OOOOO", "OOOOO", "EOOOO"}));
+//        assertEquals(-1, mazeEscape.solution(new String[]{"SOOXL", "OOOOX", "OOOOO", "OOOOO", "EOOOO"}));
+        assertEquals(12, mazeEscape.solution(new String[]{"SOXLX", "OOOOX", "OOOOO", "OOOOO", "EOOOO"}));
+//        assertEquals(8, mazeEscape.solution(new String[]{"LOOOS", "OOOOO", "OOOOO", "OOOOO", "EOOOO"}));
+//        assertEquals(8, mazeEscape.solution(new String[]{"OOOOS", "OOOOO", "OOOOO", "OOOOO", "EOOOL"}));
+//        assertEquals(2, mazeEscape.solution(new String[]{"OOELS", "OOOOO", "OOOOO", "OOOOO", "OOOOO"}));
+//        assertEquals(6, mazeEscape.solution(new String[]{"SOEOL", "OOOOO", "OOOOO", "OOOOO", "OOOOO"}));
+//        assertEquals(16, mazeEscape.solution(new String[]{"SOEOOOOOOL", "OOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO"}));
+//        assertEquals(2, mazeEscape.solution(new String[]{"SO0OOOOOO0", "LOOOOOOOOO", "EOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO"}));
+//        assertEquals(3, mazeEscape.solution(new String[]{"SO0OOOOOO0", "EOOOOOOOOO", "LOOOOOOOOO", "OOOOOOOOOO", "OOOOOOOOOO"}));
     }
 }
