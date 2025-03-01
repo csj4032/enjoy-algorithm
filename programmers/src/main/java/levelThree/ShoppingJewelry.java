f754aa95017564cf07d873a0c0840fc17722fd35
genius.choi <csj4032@gmail.com>
2025-03-01 14:58:01 +0900

    프로그래머스 보석 쇼핑 문제 완료

--- programmers/src/main/java/levelThree/ShoppingJewelry.java
+++ programmers/src/main/java/levelThree/ShoppingJewelry.java
@@ -1,15 +1,40 @@
 package levelThree;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
+/**
+ * Link : https://school.programmers.co.kr/learn/courses/30/lessons/67258
+ * Category : Sliding Window, Two Pointers
+ */
 public class ShoppingJewelry {
 
     public int[] solution(String[] gems) {
-        Set<String> unique = new HashSet<>(Arrays.stream(gems).toList());
-        int length = unique.size();
-        int[] answer = {};
-        return answer;
+        Set<String> unique = new HashSet<>(Arrays.stream(gems).collect(Collectors.toList()));
+        int uniqueSize = unique.size();
+        Map<String, Integer> map = new HashMap<>();
+        for (String gem : unique) map.put(gem, 0);
+        int right = 0;
+        int left = 0;
+        int match = 0;
+        int minLength = gems.length;
+        int[] minRange = new int[]{0, gems.length};
+        while (right < gems.length) {
+            String add = gems[right];
+            map.merge(add, 1, Integer::sum);
+            if (map.get(add) == 1) match++;
+            while (match == uniqueSize) {
+                if (right - left - 1 < minLength) {
+                    minLength = right - left - 1;
+                    minRange = new int[]{left + 1, right + 1};
+                }
+                String remove = gems[left];
+                map.merge(remove, -1, Integer::sum);
+                if (map.get(remove) == 0) match--;
+                left++;
+            }
+            right++;
+        }
+        return minRange;
     }
 }
--- programmers/src/test/java/levelThree/ShoppingJewelryTest.java
+++ programmers/src/test/java/levelThree/ShoppingJewelryTest.java
@@ -9,5 +9,12 @@ public class ShoppingJewelryTest {
     public void test() {
         ShoppingJewelry shoppingJewelry = new ShoppingJewelry();
         Assertions.assertArrayEquals(new int[]{3, 7}, shoppingJewelry.solution(new String[]{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"}));
+        Assertions.assertArrayEquals(new int[]{1, 3}, shoppingJewelry.solution(new String[]{"AA", "AB", "AC", "AA", "AC"}));
+        Assertions.assertArrayEquals(new int[]{1, 1}, shoppingJewelry.solution(new String[]{"XYZ", "XYZ", "XYZ"}));
+        Assertions.assertArrayEquals(new int[]{1, 5}, shoppingJewelry.solution(new String[]{"ZZZ", "YYY", "NNNN", "YYY", "BBB"}));
+        Assertions.assertArrayEquals(new int[]{1, 4}, shoppingJewelry.solution(new String[]{"A", "B", "B", "C"}));
+        Assertions.assertArrayEquals(new int[]{1, 1}, shoppingJewelry.solution(new String[]{"A"}));
+        Assertions.assertArrayEquals(new int[]{1, 3}, shoppingJewelry.solution(new String[]{"A", "B", "C"}));
+        Assertions.assertArrayEquals(new int[]{3, 6}, shoppingJewelry.solution(new String[]{"A", "B", "B", "A", "E", "F"}));
     }
 }
