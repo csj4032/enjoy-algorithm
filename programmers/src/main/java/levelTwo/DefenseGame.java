9a4ae76a1b53544e17f566115172f5119e5e69f2
genius.choi <csj4032@gmail.com>
2025-03-05 19:10:22 +0900

    프로그래머스 디펜스 게임 진행 중

--- programmers/src/main/java/levelTwo/DefenseGame.java
+++ programmers/src/main/java/levelTwo/DefenseGame.java
@@ -1,8 +1,6 @@
 package levelTwo;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class DefenseGame {
 
@@ -25,19 +23,38 @@ public class DefenseGame {
                     ", index=" + index +
                     '}';
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            State state = (State) o;
+            return n == state.n && k == state.k && index == state.index;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(n, k, index);
+        }
     }
 
     public int solution(int n, int k, int[] enemy) {
+        long sum = 0L;
+        for (int j : enemy) sum += j;
+        if (n >= sum) return enemy.length;
         if (k >= enemy.length) return enemy.length;
-        return bfs(n, k, 0, enemy);
+        return bfs(n, k, enemy);
     }
 
-    private int bfs(int n, int k, int index, int[] enemy) {
+    private int bfs(int n, int k, int[] enemy) {
+        Map<State, Integer> map = new HashMap<>();
         Queue<State> queue = new LinkedList<>();
-        queue.add(new State(n, k, index));
+        queue.add(new State(n, k, 0));
         int result = 0;
         while (!queue.isEmpty()) {
             State state = queue.poll();
+            if (map.containsKey(state)) continue;
+            map.put(state, state.index);
             result = Math.max(result, state.index);
             if (enemy.length > state.index) {
                 if (state.n - enemy[state.index] >= 0) {
