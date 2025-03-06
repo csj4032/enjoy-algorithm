1b7c4cfdaa07fa44b79622dc33f874f6f23149ca
genius.choi <csj4032@gmail.com>
2025-03-06 20:09:18 +0900

    프로그래머스 디펜스 게임 진행 중

--- programmers/src/main/java/levelTwo/DefenseGame.java
+++ programmers/src/main/java/levelTwo/DefenseGame.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class DefenseGame {
 
-    public static class State {
+    public static class State implements Comparable<State> {
         int n;
         int k;
         int index;
@@ -36,20 +36,49 @@ public class DefenseGame {
         public int hashCode() {
             return Objects.hash(n, k, index);
         }
+
+        @Override
+        public int compareTo(State o) {
+            int result = Integer.compare(o.index, index);
+            if (result == 0) {
+                result = Integer.compare(k, o.k);
+                if (result == 0) {
+                    result = Integer.compare(n, o.n);
+                }
+            }
+            return result;
+        }
     }
 
     public int solution(int n, int k, int[] enemy) {
-        long sum = 0L;
-        for (int j : enemy) sum += j;
-        if (n >= sum) return enemy.length;
         if (k >= enemy.length) return enemy.length;
-        return bfs(n, k, enemy);
+        int result = 0;
+        for (int i = 0; i <= k; i++) {
+            int tempN = n;
+            int tempK = k - i;
+            int index = i;
+            while (enemy.length > index) {
+                if (enemy[index] >= tempN && tempK > 0) {
+                    tempK--;
+                } else {
+                    tempN -= enemy[index];
+                }
+                if (tempK <= 0 && tempN < 0) {
+                    break;
+                }
+                System.out.println("start : " + i + " current : " + index + " K : " + tempK + " N : " + tempN);
+                index++;
+            }
+            result = Math.max(result, index);
+        }
+        System.out.println();
+        return result;
     }
 
-    private int bfs(int n, int k, int[] enemy) {
+    private int bfs(State first, int[] enemy) {
         Map<State, Integer> map = new HashMap<>();
-        Queue<State> queue = new LinkedList<>();
-        queue.add(new State(n, k, 0));
+        Queue<State> queue = new PriorityQueue<>();
+        queue.add(first);
         int result = 0;
         while (!queue.isEmpty()) {
             State state = queue.poll();
@@ -64,16 +93,17 @@ public class DefenseGame {
                     queue.add(new State(state.n, state.k - 1, state.index + 1));
                 }
             }
+            System.out.println(state + " " + queue);
         }
+        System.out.println();
         return result;
     }
 
-    private int dfs(int n, int k, int index, int[] enemy) {
-        if (index >= enemy.length) return index;
-        if (n < 0) return index;
-        if (k <= 0) return index;
-        int max1 = dfs(n - enemy[index], k, index + 1, enemy);
-        int max2 = dfs(n, k - 1, index + 1, enemy);
-        return Math.max(max1, max2);
+    private int dfs(State state, int[] enemy, Map<State, Integer> map) {
+        if (map.containsKey(state)) return state.index;
+        if (state.index >= enemy.length) return enemy.length;
+        if (state.n < 0) return state.index;
+        if (state.k <= 0) return state.index;
+        return Math.max(dfs(new State(state.n - enemy[state.index], state.k, state.index + 1), enemy, map), dfs(new State(state.n, state.k - 1, state.index + 1), enemy, map));
     }
 }
