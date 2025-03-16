39df91178e997187881c83f6f00c633009f657c6
genius.choi <csj4032@gmail.com>
2025-03-16 16:41:56 +0900

    프로그래머스 경주로 건설 완료

--- programmers/src/main/java/levelThree/RaceTrack.java
+++ programmers/src/main/java/levelThree/RaceTrack.java
@@ -1,36 +1,42 @@
 package levelThree;
 
+import java.util.*;
+
 /**
  * Link : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/67259">경주로 건설</a>
  * Category : DFS/BFS
  */
 public class RaceTrack {
 
-    private static final int[] dx = {0, 1, 0, -1};
-    private static final int[] dy = {-1, 0, 1, 0};
-    private static int min;
+    public static int[] dy = {1, 0, -1, 0};
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[][][] dp;
 
     public int solution(int[][] board) {
-        min = Integer.MAX_VALUE;
-        dfs(board, new boolean[board.length][board.length], 0, 0, 0, 0);
-        return min;
+        dp = new int[board.length][board.length][4];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                Arrays.fill(dp[i][j], Integer.MAX_VALUE);
+            }
+        }
+        int answer = dfs(board, 0, 0, -1, 0);
+        return answer;
     }
 
-    public void dfs(int[][] board, boolean[][] visited, int x, int y, int cost, int direction) {
-        if (x == board.length - 1 && y == board.length - 1) {
-            min = Math.min(min, cost - 500);
-            return;
-        }
-
-        visited[y][x] = true;
+    public static int dfs(int[][] board, int sy, int sx, int dir, int cost) {
+        if (sy == board.length - 1 && sx == board.length - 1) return cost;
+        int minCost = Integer.MAX_VALUE;
         for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (nx < 0 || ny < 0 || nx >= board.length || ny >= board.length || visited[ny][nx] || board[ny][nx] == 1) continue;
-            cost = cost + 100;
-            if (direction != i) cost = cost + 500;
-            dfs(board, visited, nx, ny, cost, i);
+            int ny = sy + dy[i];
+            int nx = sx + dx[i];
+            if (nx >= 0 && ny >= 0 && nx < board.length && ny < board.length && board[ny][nx] == 0) {
+                int costToNext = (dir == i || dir == -1) ? cost + 100 : cost + 600;
+                if (costToNext < dp[ny][nx][i]) {
+                    dp[ny][nx][i] = costToNext;
+                    minCost = Math.min(minCost, dfs(board, ny, nx, i, costToNext));
+                }
+            }
         }
-        visited[y][x] = false;
+        return minCost;
     }
 }
