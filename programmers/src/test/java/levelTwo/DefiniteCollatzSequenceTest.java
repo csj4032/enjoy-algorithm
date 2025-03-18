821ef8ff5d7d97173011980e3f362bd8e80bde54
genius.choi <csj4032@gmail.com>
2025-03-18 17:34:44 +0900

    프로그래머스 가장 긴 팰린드롬 진행 중

--- programmers/src/test/java/levelTwo/DefiniteCollatzSequenceTest.java
+++ programmers/src/test/java/levelTwo/DefiniteCollatzSequenceTest.java
@@ -3,6 +3,8 @@ package levelTwo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class DefiniteCollatzSequenceTest {
 
     @Test
@@ -10,5 +12,8 @@ public class DefiniteCollatzSequenceTest {
         DefiniteCollatzSequence definiteCollatzSequence = new DefiniteCollatzSequence();
         Assertions.assertArrayEquals(new double[]{33.0, 31.5, 0.0, -1}, definiteCollatzSequence.solution(5, new int[][]{{0, 0}, {0, -1}, {2, -3}, {3, -3}}));
         Assertions.assertArrayEquals(new double[]{47.0, 36.0, 12.0}, definiteCollatzSequence.solution(3, new int[][]{{0, 0}, {1, -2}, {3, -3}}));
+
+        List<Integer> a = List.of(1, 2, 3, 4);
+        System.out.println(a.indexOf(3));
     }
 }
