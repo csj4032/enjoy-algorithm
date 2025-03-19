86e0b380609cc7a5444553d4664a52407478f166
genius.choi <csj4032@gmail.com>
2025-03-19 22:39:54 +0900

    프로그래머스 당구 연습 진행 중

---
+++ programmers/src/main/java/levelTwo/BilliardsPractice.java
@@ -0,0 +1,9 @@
+package levelTwo;
+
+public class BilliardsPractice {
+
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+        return answer;
+    }
+}
