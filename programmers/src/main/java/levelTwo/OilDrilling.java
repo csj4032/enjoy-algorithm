f42a2e1dad42fa3c956999de346619c9c4352724
genius.choi <csj4032@gmail.com>
2025-03-22 21:22:11 +0900

    프로그래머스 석유 시추 문제 진행 중

---
+++ programmers/src/main/java/levelTwo/OilDrilling.java
@@ -0,0 +1,13 @@
+package levelTwo;
+
+/**
+ * Link : https://school.programmers.co.kr/learn/courses/30/lessons/250136
+ * Category : DFS/BFS
+ */
+public class OilDrilling {
+
+    public int solution(int[][] land) {
+        int answer = 0;
+        return answer;
+    }
+}
