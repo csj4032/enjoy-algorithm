185b82741261c336f3f22a3b6a2cf84b6f580f8c
genius.choi <csj4032@gmail.com>
2025-03-22 21:22:11 +0900

    프로그래머스 석유 시추 문제 진행 중

---
+++ programmers/src/test/java/levelTwo/OilDrillingTest.java
@@ -0,0 +1,19 @@
+package levelTwo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OilDrillingTest {
+
+    @Test
+    public void test() {
+        OilDrilling oilDrilling = new OilDrilling();
+        assertEquals(9, oilDrilling.solution(new int[][]{
+                {0, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 1, 1, 0, 0},
+                {1, 1, 0, 0, 0, 1, 1, 0},
+                {1, 1, 1, 0, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0, 1, 1}}));
+    }
+}
