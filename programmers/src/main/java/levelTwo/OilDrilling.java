4dd8e41e42860f4840471c18fa668dfb6d82c26f
genius.choi <csj4032@gmail.com>
2025-03-22 23:07:53 +0900

    프로그래머스 석유 시추 문제 진행 중

--- programmers/src/main/java/levelTwo/OilDrilling.java
+++ programmers/src/main/java/levelTwo/OilDrilling.java
@@ -6,8 +6,42 @@ package levelTwo;
  */
 public class OilDrilling {
 
+    private int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     public int solution(int[][] land) {
         int answer = 0;
+        int col = land[0].length;
+        int row = land.length;
+        for (int i = 5; i < col; i++) {
+            int count = 0;
+            boolean[][] visited = new boolean[row][col];
+            for (int j = 0; j < row; j++) {
+                if (land[j][i] == 0) continue;
+                if (visited[j][i]) continue;
+                visited[j][i] = true;
+                drilling(land, j, i, visited);
+                System.out.println();
+            }
+            for (int j = 0; j < row; j++) {
+                for (int k = 0; k < col; k++) {
+                    if (!visited[j][k]) continue;
+                    count++;
+                }
+            }
+            answer = Math.max(answer, count);
+        }
         return answer;
+    }
+
+    private void drilling(int[][] land, int row, int col, boolean[][] visited) {
+        for (int i = 0; i < directions.length; i++) {
+            int nextRow = row + directions[i][1];
+            int nextCol = col + directions[i][0];
+            if (nextRow < 0 || nextRow >= land.length || nextCol < 0 || nextCol >= land[0].length || land[nextRow][nextCol] == 0 || visited[nextRow][nextCol]) {
+                continue;
+            }
+            visited[nextRow][nextCol] = true;
+            drilling(land, nextRow, nextCol, visited);
+        }
     }
 }
