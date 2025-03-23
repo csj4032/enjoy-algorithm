e7a87b1c7b185944fc63fe788f3922e9e46ba03f
genius.choi <csj4032@gmail.com>
2025-03-24 00:36:36 +0900

    프로그래머스 석유 시추 문제 완료

--- programmers/src/main/java/levelTwo/OilDrilling.java
+++ programmers/src/main/java/levelTwo/OilDrilling.java
@@ -1,47 +1,75 @@
 package levelTwo;
 
+import java.util.*;
+
 /**
  * Link : https://school.programmers.co.kr/learn/courses/30/lessons/250136
  * Category : DFS/BFS
  */
 public class OilDrilling {
 
-    private int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    class UnionFind {
+        public int[] parent;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public int union(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x != y) parent[y] = x;
+            return x;
+        }
+    }
+
+    private int[][] directions = {{0, 1}, {1, 0}};
 
     public int solution(int[][] land) {
         int answer = 0;
-        int col = land[0].length;
         int row = land.length;
-        for (int i = 5; i < col; i++) {
-            int count = 0;
-            boolean[][] visited = new boolean[row][col];
+        int col = land[0].length;
+        UnionFind uf = new UnionFind(row * col);
+        int[] checker = new int[row * col];
+
+        for (int i = 0; i < col; i++) {
             for (int j = 0; j < row; j++) {
                 if (land[j][i] == 0) continue;
-                if (visited[j][i]) continue;
-                visited[j][i] = true;
-                drilling(land, j, i, visited);
-                System.out.println();
-            }
-            for (int j = 0; j < row; j++) {
-                for (int k = 0; k < col; k++) {
-                    if (!visited[j][k]) continue;
-                    count++;
+                int current = j * col + i;
+                checker[current] = 1;
+                for (int k = 0; k < 2; k++) {
+                    int ny = j + directions[k][0];
+                    int nx = i + directions[k][1];
+                    if (ny < row && nx < col && land[ny][nx] == 1) {
+                        int next = ny * col + nx;
+                        int rootCurrent = uf.find(current);
+                        int rootNext = uf.find(next);
+                        if (rootCurrent != rootNext) {
+                            int root = uf.union(rootCurrent, rootNext);
+                            checker[root] = checker[rootCurrent] + (checker[rootNext] == 0 ? 1 : checker[rootNext]);
+                        }
+                    }
                 }
             }
+        }
+
+        for (int i = 0; i < col; i++) {
+            int count = 0;
+            Set<Integer> chunkSet = new HashSet<>();
+            for (int j = 0; j < row; j++) {
+                if (land[j][i] == 0) continue;
+                int current = j * col + i;
+                chunkSet.add(uf.find(current));
+            }
+            for (Integer chuck : chunkSet) count += checker[chuck];
             answer = Math.max(answer, count);
         }
         return answer;
-    }
-
-    private void drilling(int[][] land, int row, int col, boolean[][] visited) {
-        for (int i = 0; i < directions.length; i++) {
-            int nextRow = row + directions[i][1];
-            int nextCol = col + directions[i][0];
-            if (nextRow < 0 || nextRow >= land.length || nextCol < 0 || nextCol >= land[0].length || land[nextRow][nextCol] == 0 || visited[nextRow][nextCol]) {
-                continue;
-            }
-            visited[nextRow][nextCol] = true;
-            drilling(land, nextRow, nextCol, visited);
-        }
     }
 }
--- programmers/src/test/java/levelTwo/OilDrillingTest.java
+++ programmers/src/test/java/levelTwo/OilDrillingTest.java
@@ -9,21 +9,16 @@ public class OilDrillingTest {
     @Test
     public void test() {
         OilDrilling oilDrilling = new OilDrilling();
-        assertEquals(9, oilDrilling.solution(new int[][]{
-                {0, 0, 0, 1, 1, 1, 0, 0},
-                {0, 0, 0, 0, 1, 1, 0, 0},
-                {1, 1, 0, 0, 0, 1, 1, 0},
-                {1, 1, 1, 0, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0, 0, 1, 1}})
+        assertEquals(40, oilDrilling.solution(new int[][]{
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1}})
         );
-//        assertEquals(16, oilDrilling.solution(new int[][]{
-//                {1, 0, 1, 0, 1, 1},
-//                {1, 0, 1, 0, 0, 0},
-//                {1, 0, 1, 0, 0, 1},
-//                {1, 0, 0, 1, 0, 0},
-//                {1, 0, 0, 1, 0, 1},
-//                {1, 0, 0, 0, 0, 0},
-//                {1, 1, 1, 1, 1, 1}})
-//        );
+        assertEquals(7, oilDrilling.solution(new int[][]{
+                        {1, 1, 1, 1, 1, 1, 1}
+                })
+        );
     }
 }
