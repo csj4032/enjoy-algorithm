0db060da411c89ef01acdc7d27f8fb3051a85d9e
genius.choi <csj4032@gmail.com>
2025-03-28 01:19:24 +0900

    프로그래머스 당구 게임 완료

--- programmers/src/main/java/levelTwo/BilliardsPractice.java
+++ programmers/src/main/java/levelTwo/BilliardsPractice.java
@@ -12,20 +12,32 @@ public class BilliardsPractice {
     public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
         int[] answer = new int[balls.length];
         for (int i = 0; i < balls.length; i++) {
-            int x = balls[i][0];
-            int y = balls[i][1];
-            int dx = x - startX;
-            int dy = y - startY;
-            int gcd = gcd(Math.abs(dx), Math.abs(dy));
-            
-
+            answer[i] = calculate(m, n, startX, startY, balls[i][0], balls[i][1]);
         }
-        System.out.println("answer = " + Arrays.toString(answer));
         return answer;
     }
 
-    public static int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+    private int calculate(int m, int n, int sx, int sy, int tx, int ty) {
+        if (sy == ty) return minDistanceOneCoordinateSame(sx, tx, sy, m, n);
+        if (sx == tx) return minDistanceOneCoordinateSame(sy, ty, sx, n, m);
+        int dx = Math.abs(sx - tx);
+        int dy = Math.abs(sy - ty);
+        int distance1 = square(sx + tx) + square(dy);
+        int distance2 = square(dx) + square(sy + ty);
+        int distance3 = square((m - sx + m - tx)) + square(dy);
+        int distance4 = square(dx) + square((n - sy + n - ty));
+        return Math.min(Math.min(distance1, distance2), Math.min(distance3, distance4));
+    }
+
+    private int minDistanceOneCoordinateSame(int a1, int a2, int fixed, int boundaryA, int boundaryFixed) {
+        int diff = Math.abs(a1 - a2);
+        int option1 = square(diff) + square((boundaryFixed - fixed) * 2);
+        int option2 = square(diff) + square(fixed * 2);
+        int option3 = (a1 > a2) ? square((boundaryA - a1 + boundaryA - a2)) : square((a1 + a2));
+        return Math.min(option1, Math.min(option2, option3));
+    }
+
+    private int square(int x) {
+        return x * x;
     }
 }
