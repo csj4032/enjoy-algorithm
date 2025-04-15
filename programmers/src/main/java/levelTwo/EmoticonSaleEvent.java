c40fdb071539d3494e04cebaffbcd7de5d87739f
genius.choi <csj4032@gmail.com>
2025-04-15 14:43:32 +0900

    프로그래머스 이모티콘 할인행사 진행 중

---
+++ programmers/src/main/java/levelTwo/EmoticonSaleEvent.java
@@ -0,0 +1,12 @@
+package levelTwo;
+
+/**
+ * @link : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/150368">이모티콘 할인행사</a>
+ */
+public class EmoticonSaleEvent {
+
+    public int[] solution(int[][] users, int[] emoticons) {
+        int[] answer = {};
+        return answer;
+    }
+}
