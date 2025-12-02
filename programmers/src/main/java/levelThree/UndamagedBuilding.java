49a3def29312feb845769e4ed77bbc2a320e63d0
genius.choi <csj4032@gmail.com>
2025-12-02 11:15:43 +0900

    Add UndamagedBuilding class and tests; implement initial solution method and test cases

---
+++ programmers/src/main/java/levelThree/UndamagedBuilding.java
@@ -0,0 +1,15 @@
+package levelThree;
+
+
+/**
+ * Link : <a href="https://school.programmers.co.kr/learn/courses/30/lessons/92344">파괴되지 않은 건물</a>
+ * Category :
+ * Idea :
+ */
+public class UndamagedBuilding {
+
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ programmers/src/test/java/levelThree/UndamagedBuildingTest.java
@@ -0,0 +1,18 @@
+package levelThree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UndamagedBuildingTest {
+
+    @Test
+    void test() {
+        UndamagedBuilding undamagedBuilding = new UndamagedBuilding();
+        Assertions.assertEquals(10, undamagedBuilding.solution(
+                new int[][]{{5, 5, 5, 5, 5}, {5, 5, 5, 5, 5}, {5, 5, 5, 5, 5}, {5, 5, 5, 5, 5}},
+                new int[][]{{1, 0, 0, 3, 4, 4}, {1, 2, 0, 2, 3, 2}, {2, 1, 0, 3, 1, 2}, {1, 0, 1, 3, 3, 1}}));
+        Assertions.assertEquals(6, undamagedBuilding.solution(
+                new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+                new int[][]{{1, 1, 1, 2, 2, 4}, {1, 0, 0, 1, 1, 2}, {2, 2, 0, 2, 0, 100}}));
+    }
+}
